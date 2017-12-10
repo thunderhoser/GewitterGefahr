5bbba3f3a81ae49dbb2fbcf96cc9505295cd9475
thunderhoser <ryan.lagerquist@ou.edu>
2017-12-09 20:36:28 -0600

    Fixed another bug in 'best_tracks.write_output_storm_objects'.

--- gewittergefahr/gg_utils/best_tracks.py
+++ gewittergefahr/gg_utils/best_tracks.py
@@ -1318,6 +1318,8 @@ def write_output_storm_objects(
         input_columns_to_drop = set(list(storm_object_table))
         for this_column in COLUMNS_TO_MERGE_ON:
             input_columns_to_drop.remove(this_column)
+        input_columns_to_drop = list(input_columns_to_drop.intersection(
+            set(list(this_input_table))))
         this_input_table.drop(input_columns_to_drop, axis=1, inplace=True)
 
         this_output_table = storm_object_table.loc[
