e3f771a968481fdc37c2e47cc5e3a22ba80ef999
thunderhoser <ryan.lagerquist@ou.edu>
2017-12-17 18:29:27 -0600

    myrorss_io.unzip_1day_tar_file now allows for missing azimuthal shear.

--- gewittergefahr/gg_io/myrorss_io.py
+++ gewittergefahr/gg_io/myrorss_io.py
@@ -5,9 +5,13 @@
 MYRORSS = Multi-year Reanalysis of Remotely Sensed Storms
 """
 
+import numpy
 from gewittergefahr.gg_io import radar_io
 from gewittergefahr.gg_utils import unzipping
 from gewittergefahr.gg_utils import error_checking
+
+IGNORABLE_RADAR_FIELD_NAMES = [
+    radar_io.LOW_LEVEL_SHEAR_NAME, radar_io.MID_LEVEL_SHEAR_NAME]
 
 
 def unzip_1day_tar_file(
@@ -25,6 +29,22 @@ def unzip_1day_tar_file(
         above ground level).
     :return: target_directory_name: Path to output directory.
     """
+
+    error_checking.assert_is_string_list(field_names)
+    error_checking.assert_is_numpy_array(
+        numpy.asarray(field_names), num_dimensions=1)
+
+    # Put ignorable radar fields (ones that are allowed to be missing) at the
+    # end.  This way, if the tar command errors out due to missing data, it will
+    # do so after unzipping all the non-missing data.
+    field_names_removed = []
+    for this_field_name in IGNORABLE_RADAR_FIELD_NAMES:
+        if this_field_name in field_names:
+            field_names.remove(this_field_name)
+            field_names_removed.append(this_field_name)
+
+    for this_field_name in field_names_removed:
+        field_names.append(this_field_name)
 
     field_to_heights_dict_m_agl = radar_io.field_and_height_arrays_to_dict(
         field_names, refl_heights_m_agl=refl_heights_m_agl,
@@ -46,8 +66,9 @@ def unzip_1day_tar_file(
                     field_name=this_field_name, height_m_agl=this_height_m_agl,
                     data_source=radar_io.MYRORSS_SOURCE_ID))
 
-    unzipping.unzip_tar(tar_file_name,
-                        target_directory_name=target_directory_name,
-                        file_and_dir_names_to_unzip=directory_names_to_unzip)
+    unzipping.unzip_tar(
+        tar_file_name,
+        target_directory_name=target_directory_name,
+        file_and_dir_names_to_unzip=directory_names_to_unzip)
 
     return target_directory_name
