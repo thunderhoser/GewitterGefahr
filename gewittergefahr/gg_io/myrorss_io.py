07e63c234e01d874facf120ceca4fffa566703d4
thunderhoser <ryan.lagerquist@ou.edu>
2017-12-23 20:42:32 -0600

    Made 'myrorss_utils.get_echo_tops' faster.

--- gewittergefahr/gg_io/myrorss_io.py
+++ gewittergefahr/gg_io/myrorss_io.py
@@ -16,6 +16,17 @@ from gewittergefahr.gg_utils import error_checking
 IGNORABLE_RADAR_FIELD_NAMES = [
     radar_io.LOW_LEVEL_SHEAR_NAME, radar_io.MID_LEVEL_SHEAR_NAME]
 
+DEFAULT_FIELDS_TO_REMOVE = [
+    radar_io.ECHO_TOP_18DBZ_NAME, radar_io.ECHO_TOP_50DBZ_NAME,
+    radar_io.LOW_LEVEL_SHEAR_NAME, radar_io.MID_LEVEL_SHEAR_NAME,
+    radar_io.REFL_NAME, radar_io.REFL_COLUMN_MAX_NAME, radar_io.MESH_NAME,
+    radar_io.REFL_0CELSIUS_NAME, radar_io.REFL_M10CELSIUS_NAME,
+    radar_io.REFL_M20CELSIUS_NAME, radar_io.REFL_LOWEST_ALTITUDE_NAME,
+    radar_io.SHI_NAME, radar_io.VIL_NAME]
+
+DEFAULT_REFL_HEIGHTS_TO_REMOVE_M_AGL = radar_io.get_valid_heights_for_field(
+    radar_io.REFL_NAME, data_source=radar_io.MYRORSS_SOURCE_ID)
+
 
 def unzip_1day_tar_file(
         tar_file_name, field_names, spc_date_string, top_target_directory_name,
@@ -28,8 +39,8 @@ def unzip_1day_tar_file(
     :param top_target_directory_name: Name of top-level directory for unzipped
         MYRORSS files.  This method will create a subdirectory therein for the
         SPC date.
-    :param refl_heights_m_agl: 1-D numpy array of reflectivity heights (metres
-        above ground level).
+    :param refl_heights_m_agl: 1-D integer numpy array of reflectivity heights
+        (metres above ground level).
     :return: target_directory_name: Path to output directory.
     """
 
@@ -76,38 +87,61 @@ def unzip_1day_tar_file(
     return target_directory_name
 
 
-def remove_unzipped_data_1day(spc_date_string, top_directory_name):
+def remove_unzipped_data_1day(
+        spc_date_string, top_directory_name,
+        field_names=DEFAULT_FIELDS_TO_REMOVE,
+        refl_heights_m_agl=DEFAULT_REFL_HEIGHTS_TO_REMOVE_M_AGL):
     """Removes unzipped MYRORSS data for one SPC date.
 
     Basically, this method cleans up after unzip_1day_tar_file.
 
     :param spc_date_string: SPC date (format "yyyymmdd").
-    :param top_directory_name: Name of top-level directory for unzipped MYRORSS
-        files.  This method will find the subdirectory therein for the SPC date.
+    :param top_directory_name: Name of top-level directory with unzipped MYRORSS
+        files.  This method will find the subdirectory in `top_directory_name`
+        for the given SPC date.
+    :param field_names: 1-D list with names of radar fields.  Only these will be
+        deleted.
+    :param refl_heights_m_agl: 1-D integer numpy array of reflectivity heights
+        (metres above ground level).
     """
 
     spc_date_unix_sec = time_conversion.spc_date_string_to_unix_sec(
         spc_date_string)
 
-    field_names = radar_io.RADAR_FIELD_NAMES
-    field_names.remove(radar_io.STORM_ID_NAME)
     field_to_heights_dict_m_agl = radar_io.field_and_height_arrays_to_dict(
-        field_names,
-        refl_heights_m_agl=numpy.array([radar_io.DEFAULT_HEIGHT_MYRORSS_M_AGL]),
+        field_names, refl_heights_m_agl=refl_heights_m_agl,
         data_source=radar_io.MYRORSS_SOURCE_ID)
 
     for this_field_name in field_to_heights_dict_m_agl.keys():
-        example_file_name = radar_io.find_raw_file(
-            unix_time_sec=spc_date_unix_sec,
-            spc_date_unix_sec=spc_date_unix_sec, field_name=this_field_name,
-            height_m_agl=field_to_heights_dict_m_agl[this_field_name][0],
-            data_source=radar_io.MYRORSS_SOURCE_ID,
-            top_directory_name=top_directory_name, raise_error_if_missing=False)
+        these_heights_m_agl = field_to_heights_dict_m_agl[this_field_name]
 
-        field_and_height_dir_name, _ = os.path.split(example_file_name)
-        directory_name_parts = field_and_height_dir_name.split('/')
-        field_directory_name = '/'.join(directory_name_parts[:-1])
+        for this_height_m_agl in these_heights_m_agl:
+            example_file_name = radar_io.find_raw_file(
+                unix_time_sec=spc_date_unix_sec,
+                spc_date_unix_sec=spc_date_unix_sec, field_name=this_field_name,
+                height_m_agl=this_height_m_agl,
+                data_source=radar_io.MYRORSS_SOURCE_ID,
+                top_directory_name=top_directory_name,
+                raise_error_if_missing=False)
 
-        if os.path.isdir(field_directory_name):
-            print 'Removing directory "{0:s}"...'.format(field_directory_name)
-            shutil.rmtree(field_directory_name, ignore_errors=True)
+            example_directory_name, _ = os.path.split(example_file_name)
+            directory_name_parts = example_directory_name.split('/')
+            remove_all_heights = False
+
+            if this_field_name == radar_io.REFL_NAME:
+                if (set(these_heights_m_agl) ==
+                        set(DEFAULT_REFL_HEIGHTS_TO_REMOVE_M_AGL)):
+                    remove_all_heights = True
+                    dir_name_to_remove = '/'.join(directory_name_parts[:-1])
+                else:
+                    dir_name_to_remove = '/'.join(directory_name_parts)
+
+            else:
+                dir_name_to_remove = '/'.join(directory_name_parts[:-1])
+
+            if os.path.isdir(dir_name_to_remove):
+                print 'Removing directory "{0:s}"...'.format(dir_name_to_remove)
+                shutil.rmtree(dir_name_to_remove, ignore_errors=True)
+
+            if remove_all_heights:
+                break
--- gewittergefahr/gg_utils/myrorss_utils.py
+++ gewittergefahr/gg_utils/myrorss_utils.py
@@ -12,7 +12,7 @@ Ortega, K., and Coauthors, 2012: "The multi-year reanalysis of remotely sensed
 import numpy
 from gewittergefahr.gg_io import radar_io
 from gewittergefahr.gg_utils import time_conversion
-from gewittergefahr.gg_utils import gridrad_utils
+from gewittergefahr.gg_utils import radar_utils
 from gewittergefahr.gg_utils import radar_sparse_to_full as radar_s2f
 from gewittergefahr.gg_utils import error_checking
 
@@ -21,7 +21,8 @@ DEFAULT_TOP_INPUT_HEIGHT_FOR_ECHO_TOPS_M_ASL = 20000
 
 def get_echo_tops(unix_time_sec, spc_date_string, top_directory_name,
                   critical_reflectivity_dbz, top_height_to_consider_m_asl=
-                  DEFAULT_TOP_INPUT_HEIGHT_FOR_ECHO_TOPS_M_ASL):
+                  DEFAULT_TOP_INPUT_HEIGHT_FOR_ECHO_TOPS_M_ASL,
+                  lowest_refl_to_consider_dbz=None):
     """Finds echo top at each horizontal location.
 
     "Echo top" is max height with reflectivity >= critical reflectivity.
@@ -36,18 +37,32 @@ def get_echo_tops(unix_time_sec, spc_date_string, top_directory_name,
         top).
     :param top_height_to_consider_m_asl: Top height level to consider (metres
         above sea level).
+    :param lowest_refl_to_consider_dbz: Lowest reflectivity to consider in echo
+        top calculations.  If None, will consider all reflectivities.
     :return: echo_top_matrix_m_asl: M-by-N matrix of echo tops (metres above sea
-        level).
+        level).  Latitude increases down each column, and longitude increases to
+        the right along each row.
+    :return: grid_point_latitudes_deg: length-M numpy array with latitudes
+        (deg N) of grid points, sorted in ascending order.
+    :return: grid_point_longitudes_deg: length-N numpy array with longitudes
+        (deg E) of grid points, sorted in ascending order.
+    :return: metadata_dict: Dictionary created by
+        `radar_io.read_metadata_from_raw_file` for column-max reflectivity.
     """
 
     error_checking.assert_is_greater(critical_reflectivity_dbz, 0.)
     error_checking.assert_is_integer(top_height_to_consider_m_asl)
     error_checking.assert_is_greater(top_height_to_consider_m_asl, 0)
 
-    unique_grid_point_heights_m_asl = radar_io.get_valid_heights_for_field(
+    if lowest_refl_to_consider_dbz is None:
+        lowest_refl_to_consider_dbz = 0.
+    error_checking.assert_is_less_than(
+        lowest_refl_to_consider_dbz, critical_reflectivity_dbz)
+
+    grid_point_heights_m_asl = radar_io.get_valid_heights_for_field(
         radar_io.REFL_NAME, data_source=radar_io.MYRORSS_SOURCE_ID)
-    unique_grid_point_heights_m_asl = unique_grid_point_heights_m_asl[
-        unique_grid_point_heights_m_asl <= top_height_to_consider_m_asl]
+    grid_point_heights_m_asl = grid_point_heights_m_asl[
+        grid_point_heights_m_asl <= top_height_to_consider_m_asl]
     spc_date_unix_sec = time_conversion.spc_date_string_to_unix_sec(
         spc_date_string)
 
@@ -57,34 +72,34 @@ def get_echo_tops(unix_time_sec, spc_date_string, top_directory_name,
         data_source=radar_io.MYRORSS_SOURCE_ID,
         top_directory_name=top_directory_name)
 
-    num_grid_heights = len(unique_grid_point_heights_m_asl)
+    num_grid_heights = len(grid_point_heights_m_asl)
     single_height_refl_file_names = [''] * num_grid_heights
     for k in range(num_grid_heights):
         single_height_refl_file_names[k] = radar_io.find_raw_file(
             unix_time_sec=unix_time_sec, spc_date_unix_sec=spc_date_unix_sec,
             field_name=radar_io.REFL_NAME,
-            height_m_agl=unique_grid_point_heights_m_asl[k],
+            height_m_agl=grid_point_heights_m_asl[k],
             data_source=radar_io.MYRORSS_SOURCE_ID,
             top_directory_name=top_directory_name)
 
     print 'Reading "{0:s}" for echo-top calculation...'.format(
         column_max_refl_file_name)
 
-    this_metadata_dict = radar_io.read_metadata_from_raw_file(
+    metadata_dict = radar_io.read_metadata_from_raw_file(
         column_max_refl_file_name, data_source=radar_io.MYRORSS_SOURCE_ID)
     this_sparse_grid_table = radar_io.read_data_from_sparse_grid_file(
         column_max_refl_file_name,
-        field_name_orig=this_metadata_dict[radar_io.FIELD_NAME_COLUMN_ORIG],
+        field_name_orig=metadata_dict[radar_io.FIELD_NAME_COLUMN_ORIG],
         data_source=radar_io.MYRORSS_SOURCE_ID,
-        sentinel_values=this_metadata_dict[radar_io.SENTINEL_VALUE_COLUMN])
+        sentinel_values=metadata_dict[radar_io.SENTINEL_VALUE_COLUMN])
 
     (column_max_refl_matrix_dbz,
-     unique_grid_point_lats_deg,
-     unique_grid_point_lngs_deg) = radar_s2f.sparse_to_full_grid(
-         this_sparse_grid_table, this_metadata_dict)
+     grid_point_latitudes_deg,
+     grid_point_longitudes_deg) = radar_s2f.sparse_to_full_grid(
+         this_sparse_grid_table, metadata_dict)
 
-    num_grid_rows = len(unique_grid_point_lats_deg)
-    num_grid_columns = len(unique_grid_point_lngs_deg)
+    num_grid_rows = len(grid_point_latitudes_deg)
+    num_grid_columns = len(grid_point_longitudes_deg)
     linear_indices_to_consider = numpy.where(numpy.reshape(
         column_max_refl_matrix_dbz, num_grid_rows * num_grid_columns) >=
                                              critical_reflectivity_dbz)[0]
@@ -118,7 +133,8 @@ def get_echo_tops(unix_time_sec, spc_date_string, top_directory_name,
             sentinel_values=this_metadata_dict[radar_io.SENTINEL_VALUE_COLUMN])
 
         this_reflectivity_matrix_dbz, _, _ = radar_s2f.sparse_to_full_grid(
-            this_sparse_grid_table, this_metadata_dict)
+            this_sparse_grid_table, this_metadata_dict,
+            ignore_if_below=lowest_refl_to_consider_dbz)
         reflectivity_matrix_dbz[k, :] = this_reflectivity_matrix_dbz[
             grid_rows_to_consider, grid_columns_to_consider]
 
@@ -128,9 +144,10 @@ def get_echo_tops(unix_time_sec, spc_date_string, top_directory_name,
     for i in range(num_horiz_points_to_consider):
         echo_top_matrix_m_asl[
             grid_rows_to_consider[i], grid_columns_to_consider[i]] = (
-                gridrad_utils.get_echo_top_single_column(
+                radar_utils.get_echo_top_single_column(
                     reflectivities_dbz=reflectivity_matrix_dbz[:, i],
-                    heights_m_asl=unique_grid_point_heights_m_asl,
+                    heights_m_asl=grid_point_heights_m_asl,
                     critical_reflectivity_dbz=critical_reflectivity_dbz))
 
-    return echo_top_matrix_m_asl
+    return (numpy.flipud(echo_top_matrix_m_asl), grid_point_latitudes_deg[::-1],
+            grid_point_longitudes_deg, metadata_dict)
