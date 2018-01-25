1c8e4ee7c67d302bc0cfdfbf334ca443345447b0
thunderhoser <ryan.lagerquist@ou.edu>
2018-01-24 21:24:28 -0600

    Added another debugging print statement to gridrad_tracking.py.

--- gewittergefahr/gg_utils/gridrad_tracking.py
+++ gewittergefahr/gg_utils/gridrad_tracking.py
@@ -405,6 +405,10 @@ def _find_input_radar_files(
             keep_time_indices = numpy.where(
                 these_times_unix_sec >= start_time_unix_sec)[0]
             these_times_unix_sec = these_times_unix_sec[keep_time_indices]
+            print keep_time_indices
+            print type(keep_time_indices)
+            print these_file_names
+            print type(these_file_names)
             these_file_names = these_file_names[keep_time_indices]
 
         if i == num_spc_dates - 1:
