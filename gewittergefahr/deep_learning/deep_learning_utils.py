a5b043e9f6c66e1abf3b601dc30504e6b558b521
thunderhoser <ryan.lagerquist@ou.edu>
2018-04-25 17:50:56 -0500

    Added normalization code to deep_learning_utils.py.

--- gewittergefahr/deep_learning/deep_learning_utils.py
+++ gewittergefahr/deep_learning/deep_learning_utils.py
@@ -10,7 +10,34 @@ P = number of channels (predictor variables) per image
 """
 
 import numpy
+from gewittergefahr.gg_utils import radar_utils
 from gewittergefahr.gg_utils import error_checking
+
+DEFAULT_PERCENTILE_OFFSET_FOR_NORMALIZATION = 1.
+MAX_PERCENTILE_OFFSET_FOR_NORMALIZATION = 5.
+
+DEFAULT_NORMALIZATION_DICT = {
+    radar_utils.ECHO_TOP_18DBZ_NAME: numpy.array([0., 15.]),  # km
+    radar_utils.ECHO_TOP_40DBZ_NAME: numpy.array([0., 15.]),  # km
+    radar_utils.ECHO_TOP_50DBZ_NAME: numpy.array([0., 15.]),  # km
+    radar_utils.LOW_LEVEL_SHEAR_NAME: numpy.array([-0.01, 0.02]),  # s^-1
+    radar_utils.MID_LEVEL_SHEAR_NAME: numpy.array([-0.01, 0.02]),  # s^-1
+    radar_utils.MESH_NAME: numpy.array([0., 90.]),  # mm
+    radar_utils.REFL_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.REFL_COLUMN_MAX_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.REFL_0CELSIUS_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.REFL_M10CELSIUS_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.REFL_M20CELSIUS_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.REFL_LOWEST_ALTITUDE_NAME: numpy.array([0., 75.]),  # dBZ
+    radar_utils.SHI_NAME: numpy.array([0., 450.]),  # unitless
+    radar_utils.VIL_NAME: numpy.array([0., 75.]),  # mm
+    radar_utils.DIFFERENTIAL_REFL_NAME: numpy.array([-1., 4.]),  # dB
+    radar_utils.SPEC_DIFF_PHASE_NAME: numpy.array([-1., 4.]),  # deg km^-1
+    radar_utils.CORRELATION_COEFF_NAME: numpy.array([0.7, 1.]),  # unitless
+    radar_utils.SPECTRUM_WIDTH_NAME: numpy.array([0., 10.]),  # m s^-1
+    radar_utils.VORTICITY_NAME: numpy.array([-0.0075, 0.0075]),  # s^-1
+    radar_utils.DIVERGENCE_NAME: numpy.array([-0.0075, 0.0075])  # s^-1
+}
 
 
 def _check_predictor_matrix(
@@ -47,5 +74,84 @@ def stack_predictor_variables(tuple_of_3d_predictor_matrices):
     _check_predictor_matrix(
         predictor_matrix=predictor_matrix, min_num_dimensions=4,
         max_num_dimensions=4)
+
+    return predictor_matrix
+
+
+def normalize_predictor_matrix(
+        predictor_matrix, normalize_by_batch=False, predictor_names=None,
+        normalization_dict=DEFAULT_NORMALIZATION_DICT, percentile_offset=1.):
+    """Normalizes predictor matrix.
+
+    Specifically, for each predictor x and each pixel (i, j), the normalization
+    is done as follows.
+
+    x_new(i, j) = [x_old(i, j) - x_min] / [x_max - x_min]
+
+    If normalize_by_batch = True, predictor_matrix is treated as a batch and
+    variables have the following meanings.
+
+    x_min = minimum (or a very small percentile) of all x-values in the batch
+    x_max = max (or a very large percentile) of all x-values in the batch
+
+    If normalize_by_batch = False, variables have the following meanings.
+
+    x_min = climatological minimum for x (taken from normalization_dict)
+    x_max = climatological max for x (taken from normalization_dict)
+
+    :param predictor_matrix: numpy array of predictor images.  Dimensions must
+        be E x M x N x C.
+    :param normalize_by_batch: Boolean flag.  If True, x_min and x_max are based
+        on the batch.  If False, x_min and x_max are based on climatology.
+    :param predictor_names: [used only if normalize_by_batch = False]
+        length-C list of predictor names, indicating the order of predictors in
+        predictor_matrix.
+    :param normalization_dict: [used only if normalize_by_batch = False]
+        Dictionary, where each key is a predictor name and each value is a
+        length-2 numpy array with (x_min, x_max).
+    :param percentile_offset: [used only if normalize_by_batch = True]
+        Offset for x_min and x_max (reduces the effect of outliers).  For
+        example, if percentile_offset = 1, x_min and x_max will actually be the
+        1st and 99th percentiles, respectively.  If you want the strict min and
+        max, set this to 0.
+    :return: predictor_matrix: Normalized version of input.  Dimensions are the
+        same.
+    """
+
+    _check_predictor_matrix(
+        predictor_matrix, min_num_dimensions=4, max_num_dimensions=4)
+    num_predictors = predictor_matrix.shape[-1]
+
+    error_checking.assert_is_boolean(normalize_by_batch)
+
+    if normalize_by_batch:
+        error_checking.assert_is_geq(percentile_offset, 0.)
+        error_checking.assert_is_leq(
+            percentile_offset, MAX_PERCENTILE_OFFSET_FOR_NORMALIZATION)
+
+        num_examples = predictor_matrix.shape[0]
+        for i in range(num_examples):
+            for m in range(num_predictors):
+                this_min_value = numpy.nanpercentile(
+                    predictor_matrix[i, ..., m], percentile_offset)
+                this_max_value = numpy.nanpercentile(
+                    predictor_matrix[i, ..., m], 100. - percentile_offset)
+
+                predictor_matrix[i, ..., m] = (
+                    (predictor_matrix[i, ..., m] - this_min_value) /
+                    (this_max_value - this_min_value))
+
+    else:
+        error_checking.assert_is_string_list(predictor_names)
+        error_checking.assert_is_numpy_array(
+            numpy.array(predictor_names),
+            exact_dimensions=numpy.array([num_predictors]))
+
+        for m in range(num_predictors):
+            this_min_value = normalization_dict[predictor_names[m]][0]
+            this_max_value = normalization_dict[predictor_names[m]][1]
+            predictor_matrix[..., m] = (
+                (predictor_matrix[..., m] - this_min_value) /
+                (this_max_value - this_min_value))
 
     return predictor_matrix
--- gewittergefahr/deep_learning/deep_learning_utils_test.py
+++ gewittergefahr/deep_learning/deep_learning_utils_test.py
@@ -1,7 +1,9 @@
 """Unit tests for deep_learning_utils.py"""
 
+import copy
 import unittest
 import numpy
+from gewittergefahr.gg_utils import radar_utils
 from gewittergefahr.deep_learning import deep_learning_utils as dl_utils
 
 TOLERANCE = 1e-6
@@ -19,6 +21,44 @@ TUPLE_OF_3D_PREDICTOR_MATRICES = (
     PREDICTOR_MATRIX_3D, PREDICTOR_MATRIX_3D, PREDICTOR_MATRIX_3D,
     PREDICTOR_MATRIX_3D, PREDICTOR_MATRIX_3D, PREDICTOR_MATRIX_3D)
 PREDICTOR_MATRIX_4D = numpy.stack(TUPLE_OF_3D_PREDICTOR_MATRICES, axis=-1)
+
+# The following constants are used to test normalize_predictor_matrix.
+PERCENTILE_OFFSET_FOR_NORMALIZATION = 0.
+PREDICTOR_NAMES = [radar_utils.REFL_NAME, radar_utils.DIFFERENTIAL_REFL_NAME]
+PREDICTOR_NORMALIZATION_DICT = {
+    radar_utils.DIFFERENTIAL_REFL_NAME: numpy.array([-10., 10.]),
+    radar_utils.REFL_NAME: numpy.array([1., 10.])
+}
+
+REFL_MATRIX = numpy.array(
+    [[0, 1, 2, 3],
+     [4, 5, 6, 7]], dtype=numpy.float32)
+DIFFERENTIAL_REFL_MATRIX = numpy.array(
+    [[2, 4, 6, 8],
+     [-1, -3, -5, -7]], dtype=numpy.float32)
+
+THIS_SINGLE_EXAMPLE_MATRIX = numpy.stack(
+    (REFL_MATRIX, DIFFERENTIAL_REFL_MATRIX), axis=-1)
+PREDICTOR_MATRIX_UNNORMALIZED = numpy.stack(
+    (THIS_SINGLE_EXAMPLE_MATRIX, THIS_SINGLE_EXAMPLE_MATRIX), axis=0)
+
+REFL_MATRIX_NORMALIZED_BY_BATCH = (REFL_MATRIX + 0) / 7
+DIFF_REFL_MATRIX_NORMALIZED_BY_BATCH = (DIFFERENTIAL_REFL_MATRIX + 7) / 15
+
+THIS_SINGLE_EXAMPLE_MATRIX = numpy.stack(
+    (REFL_MATRIX_NORMALIZED_BY_BATCH, DIFF_REFL_MATRIX_NORMALIZED_BY_BATCH),
+    axis=-1)
+PREDICTOR_MATRIX_NORMALIZED_BY_BATCH = numpy.stack(
+    (THIS_SINGLE_EXAMPLE_MATRIX, THIS_SINGLE_EXAMPLE_MATRIX), axis=0)
+
+REFL_MATRIX_NORMALIZED_BY_CLIMO = (REFL_MATRIX - 1) / 9
+DIFF_REFL_MATRIX_NORMALIZED_BY_CLIMO = (DIFFERENTIAL_REFL_MATRIX + 10) / 20
+
+THIS_SINGLE_EXAMPLE_MATRIX = numpy.stack(
+    (REFL_MATRIX_NORMALIZED_BY_CLIMO, DIFF_REFL_MATRIX_NORMALIZED_BY_CLIMO),
+    axis=-1)
+PREDICTOR_MATRIX_NORMALIZED_BY_CLIMO = numpy.stack(
+    (THIS_SINGLE_EXAMPLE_MATRIX, THIS_SINGLE_EXAMPLE_MATRIX), axis=0)
 
 
 class DeepLearningUtilsTests(unittest.TestCase):
@@ -65,6 +105,37 @@ class DeepLearningUtilsTests(unittest.TestCase):
             TUPLE_OF_3D_PREDICTOR_MATRICES)
         self.assertTrue(numpy.allclose(
             this_matrix, PREDICTOR_MATRIX_4D, atol=TOLERANCE, equal_nan=True))
+
+    def test_normalize_predictor_matrix_by_batch(self):
+        """Ensures correct output from normalize_predictor_matrix.
+
+        In this case, normalize_by_batch = True.
+        """
+
+        this_predictor_matrix = copy.deepcopy(PREDICTOR_MATRIX_UNNORMALIZED)
+        this_predictor_matrix = dl_utils.normalize_predictor_matrix(
+            predictor_matrix=this_predictor_matrix, normalize_by_batch=True,
+            percentile_offset=PERCENTILE_OFFSET_FOR_NORMALIZATION)
+
+        self.assertTrue(numpy.allclose(
+            this_predictor_matrix, PREDICTOR_MATRIX_NORMALIZED_BY_BATCH,
+            atol=TOLERANCE, equal_nan=True))
+
+    def test_normalize_predictor_matrix_by_climo(self):
+        """Ensures correct output from normalize_predictor_matrix.
+
+        In this case, normalize_by_batch = False.
+        """
+
+        this_predictor_matrix = copy.deepcopy(PREDICTOR_MATRIX_UNNORMALIZED)
+        this_predictor_matrix = dl_utils.normalize_predictor_matrix(
+            predictor_matrix=this_predictor_matrix, normalize_by_batch=False,
+            predictor_names=PREDICTOR_NAMES,
+            normalization_dict=PREDICTOR_NORMALIZATION_DICT)
+
+        self.assertTrue(numpy.allclose(
+            this_predictor_matrix, PREDICTOR_MATRIX_NORMALIZED_BY_CLIMO,
+            atol=TOLERANCE, equal_nan=True))
 
 
 if __name__ == '__main__':
