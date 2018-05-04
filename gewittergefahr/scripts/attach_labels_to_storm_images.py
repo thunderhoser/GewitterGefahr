27629e79f1a26dff0359da65b886b8da0d5f5386
thunderhoser <ryan.lagerquist@ou.edu>
2018-05-03 19:19:59 -0500

    Added input arguments to attach_labels_to_storm_images.py.

--- gewittergefahr/scripts/attach_labels_to_storm_images.py
+++ gewittergefahr/scripts/attach_labels_to_storm_images.py
@@ -1,5 +1,6 @@
 """Attaches labels (target variables) to storm-centered radar images."""
 
+import copy
 import argparse
 import numpy
 from gewittergefahr.gg_utils import radar_utils
@@ -12,12 +13,23 @@ from gewittergefahr.deep_learning import storm_images
 
 EMPTY_STRING = 'None'
 
+RADAR_SOURCE_ARG_NAME = 'radar_source'
+RADAR_FIELD_NAMES_ARG_NAME = 'radar_field_names'
+RADAR_HEIGHTS_ARG_NAME = 'radar_heights_m_asl'
 SPC_DATE_ARG_NAME = 'spc_date_string'
 STORM_IMAGE_DIR_ARG_NAME = 'inout_storm_image_dir_name'
 WIND_LABEL_DIR_ARG_NAME = 'input_wind_label_dir_name'
 TORNADO_LABEL_DIR_ARG_NAME = 'input_tornado_label_dir_name'
-RADAR_SOURCE_ARG_NAME = 'radar_source'
 
+RADAR_SOURCE_HELP_STRING = (
+    'Radar source.  Must be a string in the following list:\n{0:s}'
+).format(radar_utils.DATA_SOURCE_IDS)
+RADAR_FIELD_NAMES_HELP_STRING = (
+    'List with names of radar fields.  For more details, see documentation for'
+    ' `{0:s}`.').format(SPC_DATE_ARG_NAME)
+RADAR_HEIGHTS_HELP_STRING = (
+    'List of radar heights (metres above sea level).  For more details, see '
+    'documentation for `{0:s}`.').format(SPC_DATE_ARG_NAME)
 SPC_DATE_HELP_STRING = (
     'SPC (Storm Prediction Center) date in format "yyyymmdd".  Labels will be '
     'added to the file for each time step, radar field, and radar height on '
@@ -35,11 +47,20 @@ TORNADO_LABEL_DIR_HELP_STRING = (
     'Name of top-level directory with files containing tornado labels (one per '
     'SPC date, readable by `labels.read_tornado_labels`).  If empty, this '
     'script will attach only wind-speed labels.')
-RADAR_SOURCE_HELP_STRING = (
-    'Radar source.  Must be a string in the following list:\n{0:s}'
-).format(radar_utils.DATA_SOURCE_IDS)
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + RADAR_SOURCE_ARG_NAME, type=str, required=True,
+    help=RADAR_SOURCE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + RADAR_FIELD_NAMES_ARG_NAME, type=str, nargs='+', required=False,
+    default=[], help=RADAR_FIELD_NAMES_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + RADAR_HEIGHTS_ARG_NAME, type=int, nargs='+', required=False,
+    default=[], help=RADAR_HEIGHTS_HELP_STRING)
+
 INPUT_ARG_PARSER.add_argument(
     '--' + SPC_DATE_ARG_NAME, type=str, required=True,
     help=SPC_DATE_HELP_STRING)
@@ -56,24 +77,22 @@ INPUT_ARG_PARSER.add_argument(
     '--' + TORNADO_LABEL_DIR_ARG_NAME, type=str, required=False,
     default=EMPTY_STRING, help=TORNADO_LABEL_DIR_HELP_STRING)
 
-INPUT_ARG_PARSER.add_argument(
-    '--' + RADAR_SOURCE_ARG_NAME, type=str, required=True,
-    help=RADAR_SOURCE_HELP_STRING)
-
 
 def _run_attach_labels(
-        spc_date_string, top_storm_image_dir_name, top_wind_label_dir_name,
-        top_tornado_label_dir_name, radar_source):
+        radar_source, radar_field_names, radar_heights_m_asl, spc_date_string,
+        top_storm_image_dir_name, top_wind_label_dir_name,
+        top_tornado_label_dir_name):
     """Attaches labels (target variables) to storm-centered radar images.
 
-    :param spc_date_string: See documentation at top of file.
+    :param radar_source: See documentation at top of file.
+    :param radar_field_names: Same.
+    :param radar_heights_m_asl: Same.
     :param top_storm_image_dir_name: Same.
     :param top_wind_label_dir_name: Same.
     :param top_tornado_label_dir_name: Same.
-    :param radar_source: Same.
+    :param spc_date_string: Same.
     """
 
-    radar_utils.check_data_source(radar_source)
     start_time_unix_sec = time_conversion.get_start_of_spc_date(spc_date_string)
     end_time_unix_sec = time_conversion.get_end_of_spc_date(spc_date_string)
     end_time_unix_sec = int(rounder.floor_to_nearest(
@@ -84,14 +103,14 @@ def _run_attach_labels(
             top_directory_name=top_storm_image_dir_name,
             start_time_unix_sec=start_time_unix_sec,
             end_time_unix_sec=end_time_unix_sec,
-            radar_field_names=storm_images.DEFAULT_GRIDRAD_FIELD_NAMES,
-            radar_heights_m_asl=storm_images.DEFAULT_GRIDRAD_HEIGHTS_M_ASL,
+            radar_field_names=radar_field_names,
+            radar_heights_m_asl=radar_heights_m_asl,
             raise_error_if_missing=True)
 
         field_name_by_predictor, _ = (
             gridrad_utils.fields_and_refl_heights_to_pairs(
-                field_names=storm_images.DEFAULT_GRIDRAD_FIELD_NAMES,
-                heights_m_asl=storm_images.DEFAULT_GRIDRAD_HEIGHTS_M_ASL))
+                field_names=radar_field_names,
+                heights_m_asl=radar_heights_m_asl))
 
         num_times = image_file_name_matrix.shape[0]
         num_predictors = len(field_name_by_predictor)
@@ -104,7 +123,8 @@ def _run_attach_labels(
                 top_directory_name=top_storm_image_dir_name,
                 start_time_unix_sec=start_time_unix_sec,
                 end_time_unix_sec=end_time_unix_sec, radar_source=radar_source,
-                radar_field_names=storm_images.DEFAULT_MYRORSS_MRMS_FIELD_NAMES,
+                radar_field_names=radar_field_names,
+                reflectivity_heights_m_asl=radar_heights_m_asl,
                 raise_error_if_missing=True))
 
     if top_wind_label_dir_name == EMPTY_STRING:
@@ -139,12 +159,26 @@ def _run_attach_labels(
 if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
+    RADAR_SOURCE = getattr(INPUT_ARG_OBJECT, RADAR_SOURCE_ARG_NAME)
+    radar_utils.check_data_source(RADAR_SOURCE)
+    RADAR_FIELD_NAMES = getattr(INPUT_ARG_OBJECT, RADAR_FIELD_NAMES_ARG_NAME)
+
+    if not len(RADAR_FIELD_NAMES):
+        if RADAR_SOURCE == radar_utils.GRIDRAD_SOURCE_ID:
+            RADAR_FIELD_NAMES = copy.deepcopy(
+                storm_images.DEFAULT_GRIDRAD_FIELD_NAMES)
+        else:
+            RADAR_FIELD_NAMES = copy.deepcopy(
+                storm_images.DEFAULT_MYRORSS_MRMS_FIELD_NAMES)
+
     _run_attach_labels(
+        radar_source=RADAR_SOURCE, radar_field_names=RADAR_FIELD_NAMES,
+        radar_heights_m_asl=numpy.array(
+            getattr(INPUT_ARG_OBJECT, RADAR_HEIGHTS_ARG_NAME), dtype=int),
         spc_date_string=getattr(INPUT_ARG_OBJECT, SPC_DATE_ARG_NAME),
         top_storm_image_dir_name=getattr(
             INPUT_ARG_OBJECT, STORM_IMAGE_DIR_ARG_NAME),
         top_wind_label_dir_name=getattr(
             INPUT_ARG_OBJECT, WIND_LABEL_DIR_ARG_NAME),
         top_tornado_label_dir_name=getattr(
-            INPUT_ARG_OBJECT, TORNADO_LABEL_DIR_ARG_NAME),
-        radar_source=getattr(INPUT_ARG_OBJECT, RADAR_SOURCE_ARG_NAME))
+            INPUT_ARG_OBJECT, TORNADO_LABEL_DIR_ARG_NAME))
