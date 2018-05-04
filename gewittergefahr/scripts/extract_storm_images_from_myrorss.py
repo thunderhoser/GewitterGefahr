1963b543c430705e3a76c16941b174ab0562bae8
thunderhoser <ryan.lagerquist@ou.edu>
2018-05-03 19:01:20 -0500

    Added input arguments to extract_storm_images_from_myrorss.py.

--- gewittergefahr/scripts/extract_storm_images_from_myrorss.py
+++ gewittergefahr/scripts/extract_storm_images_from_myrorss.py
@@ -11,11 +11,13 @@ from gewittergefahr.deep_learning import storm_images
 
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
-AZIMUTHAL_SHEAR_FIELD_NAMES = [
+ALL_AZ_SHEAR_FIELD_NAMES = [
     radar_utils.LOW_LEVEL_SHEAR_NAME, radar_utils.MID_LEVEL_SHEAR_NAME]
 
 NUM_ROWS_ARG_NAME = 'num_rows_per_image'
 NUM_COLUMNS_ARG_NAME = 'num_columns_per_image'
+RADAR_FIELD_NAMES_ARG_NAME = 'radar_field_names'
+REFL_HEIGHTS_ARG_NAME = 'reflectivity_heights_m_asl'
 SPC_DATE_ARG_NAME = 'spc_date_string'
 TARRED_MYRORSS_DIR_ARG_NAME = 'input_tarred_myrorss_dir_name'
 UNTARRED_MYRORSS_DIR_ARG_NAME = 'input_untarred_myrorss_dir_name'
@@ -27,6 +29,14 @@ NUM_ROWS_HELP_STRING = (
     'Number of pixel rows in each storm-centered radar image.')
 NUM_COLUMNS_HELP_STRING = (
     'Number of pixel columns in each storm-centered radar image.')
+RADAR_FIELD_NAMES_HELP_STRING = (
+    'List with names of radar fields.  For more details, see documentation for'
+    ' `{0:s}`.').format(SPC_DATE_ARG_NAME)
+RADAR_HEIGHTS_HELP_STRING = (
+    'List of radar heights (metres above sea level).  These apply only to the '
+    'field "{0:s}" (all other fields are 2-D).  For more details, see '
+    'documentation for `{1:s}`.').format(radar_utils.REFL_NAME,
+                                         SPC_DATE_ARG_NAME)
 SPC_DATE_HELP_STRING = (
     'SPC (Storm Prediction Center) date in format "yyyymmdd".  An image will be'
     ' created for each field, height, and storm object on this date.  Fields '
@@ -47,8 +57,6 @@ TRACKING_SCALE_HELP_STRING = (
 OUTPUT_DIR_HELP_STRING = (
     'Name of top-level directory for storm-centered radar images.')
 
-DEFAULT_NUM_IMAGE_ROWS = storm_images.DEFAULT_NUM_IMAGE_ROWS
-DEFAULT_NUM_IMAGE_COLUMNS = storm_images.DEFAULT_NUM_IMAGE_COLUMNS
 DEFAULT_TARRED_DIR_NAME = '/condo/swatcommon/common/myrorss'
 DEFAULT_UNTARRED_DIR_NAME = '/condo/swatwork/ralager/myrorss_temp'
 DEFAULT_TRACKING_DIR_NAME = (
@@ -64,11 +72,21 @@ DEFAULT_TRACKING_SCALE_METRES2 = int(numpy.round(
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + NUM_ROWS_ARG_NAME, type=int, required=False,
-    default=DEFAULT_NUM_IMAGE_ROWS, help=NUM_ROWS_HELP_STRING)
+    default=storm_images.DEFAULT_NUM_IMAGE_ROWS, help=NUM_ROWS_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + NUM_COLUMNS_ARG_NAME, type=int, required=False,
-    default=DEFAULT_NUM_IMAGE_COLUMNS, help=NUM_COLUMNS_HELP_STRING)
+    default=storm_images.DEFAULT_NUM_IMAGE_COLUMNS,
+    help=NUM_COLUMNS_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + RADAR_FIELD_NAMES_ARG_NAME, type=str, nargs='+', required=False,
+    default=storm_images.DEFAULT_MYRORSS_MRMS_FIELD_NAMES,
+    help=RADAR_FIELD_NAMES_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + REFL_HEIGHTS_ARG_NAME, type=int, nargs='+', required=False,
+    default=[0], help=RADAR_HEIGHTS_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + SPC_DATE_ARG_NAME, type=str, required=True,
@@ -96,13 +114,16 @@ INPUT_ARG_PARSER.add_argument(
 
 
 def _extract_storm_images(
-        num_image_rows, num_image_columns, spc_date_string,
-        tarred_myrorss_dir_name, untarred_myrorss_dir_name, tracking_dir_name,
-        tracking_scale_metres2, output_dir_name):
+        num_image_rows, num_image_columns, radar_field_names,
+        refl_heights_m_asl, spc_date_string, tarred_myrorss_dir_name,
+        untarred_myrorss_dir_name, tracking_dir_name, tracking_scale_metres2,
+        output_dir_name):
     """Extracts storm-centered radar image for each field, height, storm object.
 
     :param num_image_rows: See documentation at top of file.
     :param num_image_columns: Same.
+    :param radar_field_names: Same.
+    :param refl_heights_m_asl: Same.
     :param spc_date_string: Same.
     :param tarred_myrorss_dir_name: Same.
     :param untarred_myrorss_dir_name: Same.
@@ -112,29 +133,33 @@ def _extract_storm_images(
     """
 
     # Untar files with azimuthal shear.
-    az_shear_tar_file_name = (
-        '{0:s}/{1:s}/azimuthal_shear_only/{2:s}.tar'.format(
-            tarred_myrorss_dir_name, spc_date_string[:4], spc_date_string))
+    az_shear_field_names = list(
+        set(radar_field_names) & set(ALL_AZ_SHEAR_FIELD_NAMES))
 
-    myrorss_io.unzip_1day_tar_file(
-        tar_file_name=az_shear_tar_file_name,
-        field_names=AZIMUTHAL_SHEAR_FIELD_NAMES, spc_date_string=spc_date_string,
-        top_target_directory_name=untarred_myrorss_dir_name)
-    print SEPARATOR_STRING
+    if len(az_shear_field_names):
+        az_shear_tar_file_name = (
+            '{0:s}/{1:s}/azimuthal_shear_only/{2:s}.tar'.format(
+                tarred_myrorss_dir_name, spc_date_string[:4], spc_date_string))
+
+        myrorss_io.unzip_1day_tar_file(
+            tar_file_name=az_shear_tar_file_name,
+            field_names=az_shear_field_names, spc_date_string=spc_date_string,
+            top_target_directory_name=untarred_myrorss_dir_name)
+        print SEPARATOR_STRING
 
     # Untar files with other radar fields.
     non_shear_field_names = list(
-        set(storm_images.DEFAULT_MYRORSS_MRMS_FIELD_NAMES) -
-        set(AZIMUTHAL_SHEAR_FIELD_NAMES))
+        set(radar_field_names) - set(ALL_AZ_SHEAR_FIELD_NAMES))
 
-    non_shear_tar_file_name = '{0:s}/{1:s}/{2:s}.tar'.format(
-        tarred_myrorss_dir_name, spc_date_string[:4], spc_date_string)
+    if len(non_shear_field_names):
+        non_shear_tar_file_name = '{0:s}/{1:s}/{2:s}.tar'.format(
+            tarred_myrorss_dir_name, spc_date_string[:4], spc_date_string)
 
-    myrorss_io.unzip_1day_tar_file(
-        tar_file_name=non_shear_tar_file_name,
-        field_names=non_shear_field_names, spc_date_string=spc_date_string,
-        top_target_directory_name=untarred_myrorss_dir_name)
-    print SEPARATOR_STRING
+        myrorss_io.unzip_1day_tar_file(
+            tar_file_name=non_shear_tar_file_name,
+            field_names=non_shear_field_names, spc_date_string=spc_date_string,
+            top_target_directory_name=untarred_myrorss_dir_name)
+        print SEPARATOR_STRING
 
     # Find storm objects on the given SPC date.
     tracking_file_names = tracking_io.find_processed_files_one_spc_date(
@@ -155,14 +180,16 @@ def _extract_storm_images(
         top_radar_dir_name=untarred_myrorss_dir_name,
         top_output_dir_name=output_dir_name,
         num_storm_image_rows=num_image_rows,
-        num_storm_image_columns=num_image_columns)
+        num_storm_image_columns=num_image_columns,
+        radar_field_names=radar_field_names,
+        reflectivity_heights_m_asl=refl_heights_m_asl)
     print SEPARATOR_STRING
 
     # Remove MYRORSS data that was just untarred.
     myrorss_io.remove_unzipped_data_1day(
         spc_date_string=spc_date_string,
         top_directory_name=untarred_myrorss_dir_name,
-        field_names=storm_images.DEFAULT_MYRORSS_MRMS_FIELD_NAMES)
+        field_names=radar_field_names)
     print SEPARATOR_STRING
 
 
@@ -171,6 +198,9 @@ if __name__ == '__main__':
 
     NUM_IMAGE_ROWS = getattr(INPUT_ARG_OBJECT, NUM_ROWS_ARG_NAME)
     NUM_IMAGE_COLUMNS = getattr(INPUT_ARG_OBJECT, NUM_COLUMNS_ARG_NAME)
+    RADAR_FIELD_NAMES = getattr(INPUT_ARG_OBJECT, RADAR_FIELD_NAMES_ARG_NAME)
+    REFL_HEIGHTS_M_ASL = numpy.array(
+        getattr(INPUT_ARG_OBJECT, REFL_HEIGHTS_ARG_NAME), dtype=int)
 
     SPC_DATE_STRING = getattr(INPUT_ARG_OBJECT, SPC_DATE_ARG_NAME)
     TARRED_MYRORSS_DIR_NAME = getattr(
@@ -184,7 +214,8 @@ if __name__ == '__main__':
 
     _extract_storm_images(
         num_image_rows=NUM_IMAGE_ROWS, num_image_columns=NUM_IMAGE_COLUMNS,
-        spc_date_string=SPC_DATE_STRING,
+        radar_field_names=RADAR_FIELD_NAMES,
+        refl_heights_m_asl=REFL_HEIGHTS_M_ASL, spc_date_string=SPC_DATE_STRING,
         tarred_myrorss_dir_name=TARRED_MYRORSS_DIR_NAME,
         untarred_myrorss_dir_name=UNTARRED_MYRORSS_DIR_NAME,
         tracking_dir_name=TRACKING_DIR_NAME,
