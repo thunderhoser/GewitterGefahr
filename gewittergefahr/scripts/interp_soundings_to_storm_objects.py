defaed4abebf3dad01e8dea66f679d0f55f30119
thunderhoser <ryan.lagerquist@ou.edu>
2018-06-22 14:50:06 -0500

    Added 'input_rap_directory_name' as input argument for interp_soundings_to_storm_objects.py.

--- gewittergefahr/scripts/interp_soundings_to_storm_objects.py
+++ gewittergefahr/scripts/interp_soundings_to_storm_objects.py
@@ -6,17 +6,27 @@ from gewittergefahr.gg_io import storm_tracking_io as tracking_io
 from gewittergefahr.gg_utils import nwp_model_utils
 from gewittergefahr.gg_utils import soundings_only
 from gewittergefahr.gg_utils import echo_top_tracking
+from gewittergefahr.gg_utils import time_conversion
+from gewittergefahr.gg_utils import number_rounding
 from gewittergefahr.gg_utils import storm_tracking_utils as tracking_utils
 
+HOURS_TO_SECONDS = 3600
+STORM_TIME_FORMAT = '%Y-%m-%d-%H%M%S'
+MODEL_INIT_TIME_FORMAT = '%Y-%m-%d-%H'
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
 WGRIB_EXE_NAME = '/condo/swatwork/ralager/wgrib/wgrib'
 WGRIB2_EXE_NAME = '/condo/swatwork/ralager/grib2/wgrib2/wgrib2'
 
+FIRST_RAP_TIME_STRING = '2012-05-01-00'
+FIRST_RAP_TIME_UNIX_SEC = time_conversion.string_to_unix_sec(
+    FIRST_RAP_TIME_STRING, MODEL_INIT_TIME_FORMAT)
+
 SPC_DATE_ARG_NAME = 'spc_date_string'
 LEAD_TIMES_ARG_NAME = 'lead_times_seconds'
 LAG_TIME_ARG_NAME = 'lag_time_for_convective_contamination_sec'
 RUC_DIRECTORY_ARG_NAME = 'input_ruc_directory_name'
+RAP_DIRECTORY_ARG_NAME = 'input_rap_directory_name'
 TRACKING_DIR_ARG_NAME = 'input_tracking_dir_name'
 TRACKING_SCALE_ARG_NAME = 'tracking_scale_metres2'
 OUTPUT_DIR_ARG_NAME = 'output_sounding_dir_name'
@@ -35,7 +45,13 @@ LAG_TIME_HELP_STRING = (
 ).format(LEAD_TIMES_ARG_NAME)
 RUC_DIRECTORY_HELP_STRING = (
     'Name of top-level directory with grib files containing RUC (Rapid Update '
-    'Cycle) data.')
+    'Cycle) data, which will be used for all model-initialization times < '
+    '{0:s}.'
+).format(FIRST_RAP_TIME_STRING)
+RAP_DIRECTORY_HELP_STRING = (
+    'Name of top-level directory with grib files containing RAP (Rapid Refresh)'
+    ' data, which will be used for all model-initialization times >= {0:s}.'
+).format(FIRST_RAP_TIME_STRING)
 TRACKING_DIR_HELP_STRING = (
     'Name of top-level directory with storm tracks (one file per time step, '
     'readable by `storm_tracking_io.read_processed_file`).')
@@ -68,6 +84,10 @@ INPUT_ARG_PARSER.add_argument(
     help=RUC_DIRECTORY_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
+    '--' + RAP_DIRECTORY_ARG_NAME, type=str, required=True,
+    help=RAP_DIRECTORY_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
     '--' + TRACKING_DIR_ARG_NAME, type=str, required=True,
     help=TRACKING_DIR_HELP_STRING)
 
@@ -83,16 +103,20 @@ INPUT_ARG_PARSER.add_argument(
 def _interp_soundings(
         spc_date_string, lead_times_seconds,
         lag_time_for_convective_contamination_sec, top_ruc_directory_name,
-        top_tracking_dir_name, tracking_scale_metres2, top_output_dir_name):
+        top_rap_directory_name, top_tracking_dir_name, tracking_scale_metres2,
+        top_output_dir_name):
     """Interpolates NWP sounding to each storm object at each lead time.
 
     :param spc_date_string: See documentation at top of file.
     :param lead_times_seconds: Same.
     :param lag_time_for_convective_contamination_sec: Same.
     :param top_ruc_directory_name: Same.
+    :param top_rap_directory_name: Same.
     :param top_tracking_dir_name: Same.
     :param tracking_scale_metres2: Same.
     :param top_output_dir_name: Same.
+    :raises: ValueError: if model-initialization times needed are on opposite
+        sides of 0000 UTC 1 May 2012 (the cutoff between RUC and RAP models).
     """
 
     lead_times_seconds = numpy.array(lead_times_seconds, dtype=int)
@@ -107,12 +131,55 @@ def _interp_soundings(
         tracking_file_names)
     print SEPARATOR_STRING
 
+    first_storm_time_unix_sec = numpy.min(
+        storm_object_table[tracking_utils.TIME_COLUMN].values)
+    last_storm_time_unix_sec = numpy.max(
+        storm_object_table[tracking_utils.TIME_COLUMN].values)
+
+    first_init_time_unix_sec = number_rounding.floor_to_nearest(
+        (first_storm_time_unix_sec + numpy.min(lead_times_seconds) -
+         lag_time_for_convective_contamination_sec),
+        HOURS_TO_SECONDS)
+    last_init_time_unix_sec = number_rounding.floor_to_nearest(
+        (last_storm_time_unix_sec + numpy.max(lead_times_seconds) -
+         lag_time_for_convective_contamination_sec),
+        HOURS_TO_SECONDS)
+
+    extreme_init_times_unix_sec = numpy.array(
+        [first_init_time_unix_sec, last_init_time_unix_sec], dtype=int)
+
+    if numpy.all(extreme_init_times_unix_sec < FIRST_RAP_TIME_UNIX_SEC):
+        top_grib_directory_name = top_ruc_directory_name
+        model_name = nwp_model_utils.RUC_MODEL_NAME
+    elif numpy.all(extreme_init_times_unix_sec >= FIRST_RAP_TIME_UNIX_SEC):
+        top_grib_directory_name = top_rap_directory_name
+        model_name = nwp_model_utils.RAP_MODEL_NAME
+    else:
+        first_storm_time_string = time_conversion.unix_sec_to_string(
+            first_storm_time_unix_sec, STORM_TIME_FORMAT)
+        last_storm_time_string = time_conversion.unix_sec_to_string(
+            last_storm_time_unix_sec, STORM_TIME_FORMAT)
+        first_init_time_string = time_conversion.unix_sec_to_string(
+            first_init_time_unix_sec, MODEL_INIT_TIME_FORMAT)
+        last_init_time_string = time_conversion.unix_sec_to_string(
+            last_init_time_unix_sec, MODEL_INIT_TIME_FORMAT)
+
+        error_string = (
+            'First and last storm times are {0:s} and {1:s}.  Thus, first and '
+            'last model-initialization times needed are {2:s} and {3:s}, which '
+            'are on opposite sides of {4:s} (the cutoff between RUC and RAP '
+            'models).  The code is not generalized enough to interp data from '
+            'two different models.  Sorry, eh?'
+        ).format(first_storm_time_string, last_storm_time_string,
+                 first_init_time_string, last_init_time_string,
+                 FIRST_RAP_TIME_STRING)
+        raise ValueError(error_string)
+
     sounding_dict_by_lead_time = (
         soundings_only.interp_soundings_to_storm_objects(
             storm_object_table=storm_object_table,
-            top_grib_directory_name=top_ruc_directory_name,
-            model_name=nwp_model_utils.RUC_MODEL_NAME,
-            lead_times_seconds=lead_times_seconds,
+            top_grib_directory_name=top_grib_directory_name,
+            model_name=model_name, lead_times_seconds=lead_times_seconds,
             lag_time_for_convective_contamination_sec=
             lag_time_for_convective_contamination_sec, include_surface=False,
             use_all_grids=True, wgrib_exe_name=WGRIB_EXE_NAME,
@@ -148,6 +215,8 @@ if __name__ == '__main__':
             INPUT_ARG_OBJECT, LAG_TIME_ARG_NAME),
         top_ruc_directory_name=getattr(
             INPUT_ARG_OBJECT, RUC_DIRECTORY_ARG_NAME),
+        top_rap_directory_name=getattr(
+            INPUT_ARG_OBJECT, RAP_DIRECTORY_ARG_NAME),
         top_tracking_dir_name=getattr(INPUT_ARG_OBJECT, TRACKING_DIR_ARG_NAME),
         tracking_scale_metres2=getattr(
             INPUT_ARG_OBJECT, TRACKING_SCALE_ARG_NAME),
