46afa1a6941d577955541ca0c6291999e71087f1
thunderhoser <ryan.lagerquist@ou.edu>
2018-07-06 01:32:41 -0500

    Cleaned up more plotting code.

--- gewittergefahr/plotting/nwp_plotting.py
+++ gewittergefahr/plotting/nwp_plotting.py
@@ -18,17 +18,23 @@ from gewittergefahr.gg_utils import grids
 from gewittergefahr.gg_utils import projections
 from gewittergefahr.gg_utils import nwp_model_utils
 from gewittergefahr.gg_utils import error_checking
+from gewittergefahr.plotting import wind_plotting
 
 DEFAULT_FIGURE_WIDTH_INCHES = 15.
 DEFAULT_FIGURE_HEIGHT_INCHES = 15.
 DEFAULT_BOUNDARY_RESOLUTION_STRING = 'l'
 
+X_COORD_MATRIX_KEY = 'grid_point_x_matrix_metres'
+Y_COORD_MATRIX_KEY = 'grid_point_y_matrix_metres'
+LATITUDE_MATRIX_KEY = 'grid_point_lat_matrix_deg'
+LONGITUDE_MATRIX_KEY = 'grid_point_lng_matrix_deg'
 
-def _get_xy_grid_point_matrices(
+
+def _get_grid_point_coords(
         model_name, first_row_in_full_grid, last_row_in_full_grid,
         first_column_in_full_grid, last_column_in_full_grid, grid_id=None,
         basemap_object=None):
-    """Returns x-y coordinates for a subgrid of the full model grid.
+    """Returns x-y and lat-long coords for a subgrid of the full model grid.
 
     This method generates different x-y coordinates than
     `nwp_model_utils.get_xy_grid_point_matrices`, because (like
@@ -52,8 +58,15 @@ def _get_xy_grid_point_matrices(
     :param basemap_object: Instance of `mpl_toolkits.basemap.Basemap` for the
         given NWP model.  If you don't have one, no big deal -- leave this
         argument empty.
-    :return: grid_point_x_matrix_metres: M-by-N numpy array of x-coordinates.
-    :return: grid_point_y_matrix_metres: M-by-N numpy array of y-coordinates.
+    :return: coordinate_dict: Dictionary with the following keys.
+    coordinate_dict['grid_point_x_matrix_metres']: M-by-N numpy array of
+        x-coordinates.
+    coordinate_dict['grid_point_y_matrix_metres']: M-by-N numpy array of
+        y-coordinates.
+    coordinate_dict['grid_point_lat_matrix_deg']: M-by-N numpy array of
+        latitudes (deg N).
+    coordinate_dict['grid_point_lng_matrix_deg']: M-by-N numpy array of
+        longitudes (deg E).
     """
 
     (num_rows_in_full_grid, num_columns_in_full_grid
@@ -82,14 +95,14 @@ def _get_xy_grid_point_matrices(
     error_checking.assert_is_less_than(
         last_column_in_full_grid, num_columns_in_full_grid)
 
-    (latitude_matrix_deg, longitude_matrix_deg
+    (grid_point_lat_matrix_deg, grid_point_lng_matrix_deg
     ) = nwp_model_utils.get_latlng_grid_point_matrices(
         model_name=model_name, grid_id=grid_id)
 
-    latitude_matrix_deg = latitude_matrix_deg[
+    grid_point_lat_matrix_deg = grid_point_lat_matrix_deg[
         first_row_in_full_grid:(last_row_in_full_grid + 1),
         first_column_in_full_grid:(last_column_in_full_grid + 1)]
-    longitude_matrix_deg = longitude_matrix_deg[
+    grid_point_lng_matrix_deg = grid_point_lng_matrix_deg[
         first_row_in_full_grid:(last_row_in_full_grid + 1),
         first_column_in_full_grid:(last_column_in_full_grid + 1)]
 
@@ -103,15 +116,20 @@ def _get_xy_grid_point_matrices(
 
         (grid_point_x_matrix_metres, grid_point_y_matrix_metres
         ) = projections.project_latlng_to_xy(
-            latitudes_deg=latitude_matrix_deg,
-            longitudes_deg=longitude_matrix_deg,
+            latitudes_deg=grid_point_lat_matrix_deg,
+            longitudes_deg=grid_point_lng_matrix_deg,
             projection_object=projection_object, false_northing_metres=0.,
             false_easting_metres=0.)
     else:
         (grid_point_x_matrix_metres, grid_point_y_matrix_metres
-        ) = basemap_object(longitude_matrix_deg, latitude_matrix_deg)
+        ) = basemap_object(grid_point_lng_matrix_deg, grid_point_lat_matrix_deg)
 
-    return grid_point_x_matrix_metres, grid_point_y_matrix_metres
+    return {
+        X_COORD_MATRIX_KEY: grid_point_x_matrix_metres,
+        Y_COORD_MATRIX_KEY: grid_point_y_matrix_metres,
+        LATITUDE_MATRIX_KEY: grid_point_lat_matrix_deg,
+        LONGITUDE_MATRIX_KEY: grid_point_lng_matrix_deg,
+    }
 
 
 def init_basemap(
@@ -133,7 +151,7 @@ def init_basemap(
         political borders).  Options are "c" for crude, "l" for low, "i" for
         intermediate, "h" for high, and "f" for full.  Keep in mind that higher-
         resolution boundaries take much longer to draw.
-    :param first_row_in_full_grid: See doc for `_get_xy_grid_point_matrices`.
+    :param first_row_in_full_grid: See doc for `_get_grid_point_coords`.
     :param last_row_in_full_grid: Same.
     :param first_column_in_full_grid: Same.
     :param last_column_in_full_grid: Same.
@@ -146,13 +164,14 @@ def init_basemap(
     error_checking.assert_is_greater(figure_height_inches, 0.)
     error_checking.assert_is_string(resolution_string)
 
-    (grid_point_x_matrix_metres, grid_point_y_matrix_metres
-    ) = _get_xy_grid_point_matrices(
+    coordinate_dict = _get_grid_point_coords(
         model_name=model_name, grid_id=grid_id,
         first_row_in_full_grid=first_row_in_full_grid,
         last_row_in_full_grid=last_row_in_full_grid,
         first_column_in_full_grid=first_column_in_full_grid,
         last_column_in_full_grid=last_column_in_full_grid)
+    grid_point_x_matrix_metres = coordinate_dict[X_COORD_MATRIX_KEY]
+    grid_point_y_matrix_metres = coordinate_dict[Y_COORD_MATRIX_KEY]
 
     figure_object, axes_object = pyplot.subplots(
         1, 1, figsize=(figure_width_inches, figure_height_inches))
@@ -190,9 +209,9 @@ def plot_subgrid(
     :param grid_id: Grid for NWP model (must be accepted by
         `nwp_model_utils.check_grid_id`).
     :param first_row_in_full_grid: Row 0 in the subgrid (i.e., row 0 in
-        field_matrix) is row `first_row_in_full_grid` in the full grid.
+        `field_matrix` is row `first_row_in_full_grid` in the full grid).
     :param first_column_in_full_grid: Column 0 in the subgrid (i.e., column 0 in
-        field_matrix) is column `first_column_in_full_grid` in the full grid.
+        `field_matrix` is column `first_column_in_full_grid` in the full grid).
     :param opacity: Opacity of colour map (from 0...1).
     """
 
@@ -204,8 +223,7 @@ def plot_subgrid(
     num_rows_in_subgrid = field_matrix.shape[0]
     num_columns_in_subgrid = field_matrix.shape[1]
 
-    (grid_point_x_matrix_metres, grid_point_y_matrix_metres
-    ) = _get_xy_grid_point_matrices(
+    coordinate_dict = _get_grid_point_coords(
         model_name=model_name, grid_id=grid_id,
         first_row_in_full_grid=first_row_in_full_grid,
         last_row_in_full_grid=first_row_in_full_grid + num_rows_in_subgrid - 1,
@@ -213,6 +231,8 @@ def plot_subgrid(
         last_column_in_full_grid=
         first_column_in_full_grid + num_columns_in_subgrid - 1,
         basemap_object=basemap_object)
+    grid_point_x_matrix_metres = coordinate_dict[X_COORD_MATRIX_KEY]
+    grid_point_y_matrix_metres = coordinate_dict[Y_COORD_MATRIX_KEY]
 
     x_spacing_metres = (
         (grid_point_x_matrix_metres[0, -1] - grid_point_x_matrix_metres[0, 0]) /
@@ -236,3 +256,98 @@ def plot_subgrid(
         field_matrix_at_edges, cmap=colour_map, vmin=min_value_in_colour_map,
         vmax=max_value_in_colour_map, shading='flat', edgecolors='None',
         axes=axes_object, zorder=-1e9, alpha=opacity)
+
+
+def plot_wind_barbs_on_subgrid(
+        u_wind_matrix_m_s01, v_wind_matrix_m_s01, model_name, axes_object,
+        basemap_object, grid_id=None, first_row_in_full_grid=0,
+        first_column_in_full_grid=0, plot_every_k_rows=1,
+        plot_every_k_columns=1, barb_length=wind_plotting.DEFAULT_BARB_LENGTH,
+        empty_barb_radius=wind_plotting.DEFAULT_EMPTY_BARB_RADIUS,
+        fill_empty_barb=wind_plotting.FILL_EMPTY_BARB_DEFAULT,
+        colour_map=wind_plotting.DEFAULT_COLOUR_MAP,
+        colour_minimum_kt=wind_plotting.DEFAULT_COLOUR_MINIMUM_KT,
+        colour_maximum_kt=wind_plotting.DEFAULT_COLOUR_MAXIMUM_KT):
+    """Plots wind barbs over subgrid.
+
+    :param u_wind_matrix_m_s01: M-by-N numpy array of zonal wind speeds (metres
+        per second).
+    :param v_wind_matrix_m_s01: M-by-N numpy array of meridional wind speeds
+        (metres per second).
+    :param model_name: See doc for `plot_subgrid`.
+    :param axes_object: Same.
+    :param basemap_object: Same.
+    :param grid_id: Same.
+    :param first_row_in_full_grid: Row 0 in the subgrid (i.e., row 0 in
+        `u_wind_matrix_m_s01` and `v_wind_matrix_m_s01` is row
+        `first_row_in_full_grid` in the full grid).
+    :param first_column_in_full_grid: Column 0 in the subgrid (i.e., column 0 in
+        `u_wind_matrix_m_s01` and `v_wind_matrix_m_s01` is column
+        `first_column_in_full_grid` in the full grid).
+    :param plot_every_k_rows: Wind barbs will be plotted for every [k]th row in
+        the subgrid, where k = `plot_every_k_rows`.  For example, if
+        `plot_every_k_rows = 2`, wind barbs will be plotted for rows 0, 2, 4,
+        etc.
+    :param plot_every_k_columns: Same as above, but for columns.
+    :param barb_length: See doc for `wind_plotting.plot_wind_barbs`.
+    :param empty_barb_radius: Same.
+    :param fill_empty_barb: Same.
+    :param colour_map: Same.
+    :param colour_minimum_kt: Same.
+    :param colour_maximum_kt: Same.
+    """
+
+    error_checking.assert_is_real_numpy_array(u_wind_matrix_m_s01)
+    error_checking.assert_is_numpy_array(u_wind_matrix_m_s01, num_dimensions=2)
+    error_checking.assert_is_real_numpy_array(v_wind_matrix_m_s01)
+    error_checking.assert_is_numpy_array(
+        v_wind_matrix_m_s01,
+        exact_dimensions=numpy.array(u_wind_matrix_m_s01.shape, dtype=int))
+
+    error_checking.assert_is_integer(plot_every_k_rows)
+    error_checking.assert_is_geq(plot_every_k_rows, 1)
+    error_checking.assert_is_integer(plot_every_k_columns)
+    error_checking.assert_is_geq(plot_every_k_columns, 1)
+
+    num_rows_in_subgrid = u_wind_matrix_m_s01.shape[0]
+    num_columns_in_subgrid = u_wind_matrix_m_s01.shape[1]
+
+    coordinate_dict = _get_grid_point_coords(
+        model_name=model_name, grid_id=grid_id,
+        first_row_in_full_grid=first_row_in_full_grid,
+        last_row_in_full_grid=first_row_in_full_grid + num_rows_in_subgrid - 1,
+        first_column_in_full_grid=first_column_in_full_grid,
+        last_column_in_full_grid=
+        first_column_in_full_grid + num_columns_in_subgrid - 1,
+        basemap_object=basemap_object)
+    grid_point_lat_matrix_deg = coordinate_dict[LATITUDE_MATRIX_KEY]
+    grid_point_lng_matrix_deg = coordinate_dict[LONGITUDE_MATRIX_KEY]
+
+    u_wind_matrix_m_s01 = u_wind_matrix_m_s01[
+        ::plot_every_k_rows, ::plot_every_k_columns]
+    v_wind_matrix_m_s01 = v_wind_matrix_m_s01[
+        ::plot_every_k_rows, ::plot_every_k_columns]
+    grid_point_lat_matrix_deg = grid_point_lat_matrix_deg[
+        ::plot_every_k_rows, ::plot_every_k_columns]
+    grid_point_lng_matrix_deg = grid_point_lng_matrix_deg[
+        ::plot_every_k_rows, ::plot_every_k_columns]
+
+    num_wind_barbs = u_wind_matrix_m_s01.size
+    u_winds_m_s01 = numpy.reshape(u_wind_matrix_m_s01, num_wind_barbs)
+    v_winds_m_s01 = numpy.reshape(v_wind_matrix_m_s01, num_wind_barbs)
+    latitudes_deg = numpy.reshape(grid_point_lat_matrix_deg, num_wind_barbs)
+    longitudes_deg = numpy.reshape(grid_point_lng_matrix_deg, num_wind_barbs)
+
+    not_nan_indices = numpy.where(numpy.invert(numpy.logical_or(
+        numpy.isnan(u_winds_m_s01), numpy.isnan(v_winds_m_s01))))[0]
+
+    wind_plotting.plot_wind_barbs(
+        basemap_object=basemap_object, axes_object=axes_object,
+        latitudes_deg=latitudes_deg[not_nan_indices],
+        longitudes_deg=longitudes_deg[not_nan_indices],
+        u_winds_m_s01=u_winds_m_s01[not_nan_indices],
+        v_winds_m_s01=v_winds_m_s01[not_nan_indices],
+        barb_length=barb_length, empty_barb_radius=empty_barb_radius,
+        fill_empty_barb=fill_empty_barb, colour_map=colour_map,
+        colour_minimum_kt=colour_minimum_kt,
+        colour_maximum_kt=colour_maximum_kt)
--- gewittergefahr/plotting/wind_plotting.py
+++ gewittergefahr/plotting/wind_plotting.py
@@ -8,8 +8,8 @@ from gewittergefahr.gg_utils import longitude_conversion as lng_conversion
 from gewittergefahr.gg_utils import error_checking
 
 DEFAULT_BARB_LENGTH = 6
-DEFAULT_EMPTY_BARB_RADIUS = 0.25
-FILL_EMPTY_BARB_DEFAULT = False
+DEFAULT_EMPTY_BARB_RADIUS = 0.1
+FILL_EMPTY_BARB_DEFAULT = True
 DEFAULT_COLOUR_MAP = pyplot.cm.rainbow
 DEFAULT_COLOUR_MINIMUM_KT = 0.
 DEFAULT_COLOUR_MAXIMUM_KT = 50.
@@ -30,22 +30,24 @@ def plot_wind_barbs(
 
     :param basemap_object: Instance of `mpl_toolkits.basemap.Basemap`.
     :param axes_object: Instance of `matplotlib.axes._subplots.AxesSubplot`.
-    :param latitudes_deg: length-N numpy array of latitudes (deg N).
-    :param longitudes_deg: length-N numpy array of longitudes (deg E).
-    :param u_winds_m_s01: length-N numpy array of eastward wind velocities
-        (m/s).
-    :param v_winds_m_s01: length-N numpy array of northward wind velocities
-        (m/s).
-    :param barb_length: Length of each wind barb.
-    :param empty_barb_radius: Radius of circle for 0-metre-per-second wind barb.
-    :param fill_empty_barb: Boolean flag.  If fill_empty_barb = True,
-        0-metre-per-second wind barb will be a filled circle.  Otherwise, it
-        will be an empty circle.
+    :param latitudes_deg: length-N numpy array with latitudes (deg N) of wind-
+        observation points.
+    :param longitudes_deg: length-N numpy array with longitudes (deg E) of wind-
+        observation points.
+    :param u_winds_m_s01: length-N numpy array with zonal wind components
+        ("u-winds") at observation points (metres per second).
+    :param v_winds_m_s01: length-N numpy array with meridional wind components
+        ("v-winds") at observation points (metres per second).
+    :param barb_length: Length for each wind barb.
+    :param empty_barb_radius: Radius of "empty" wind barb (which is actually a
+        circle, denoting 0 metres per second).
+    :param fill_empty_barb: Boolean flag.  If True, "empty" wind barbs (circles)
+        will be coloured in.
     :param colour_map: Instance of `matplotlib.pyplot.cm`.
-    :param colour_minimum_kt: Minimum speed for colour map (kt or nautical miles
-        per hour).
-    :param colour_maximum_kt: Maximum speed for colour map (kt or nautical miles
-        per hour).
+    :param colour_minimum_kt: Minimum wind speed (nautical miles per hour) in
+        colour map.
+    :param colour_maximum_kt: Max wind speed (nautical miles per hour) in colour
+        map.
     """
 
     error_checking.assert_is_valid_lat_numpy_array(latitudes_deg)
