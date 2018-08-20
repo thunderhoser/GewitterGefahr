54b8d5aba5be42b7a44f7ed60370c1a6cf95f273
thunderhoser <ryan.lagerquist@ou.edu>
2018-08-20 17:53:54 -0500

    plot_storm_objects.py can now take two types of input files (containing model activations or just ID-time pairs).

--- gewittergefahr/scripts/plot_storm_objects.py
+++ gewittergefahr/scripts/plot_storm_objects.py
@@ -1,11 +1,12 @@
 """Plots radar and sounding data for each storm object."""
 
-import pickle
+import os.path
 import argparse
 import numpy
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as pyplot
+from gewittergefahr.gg_io import storm_tracking_io as tracking_io
 from gewittergefahr.gg_utils import labels
 from gewittergefahr.gg_utils import link_events_to_storms as events2storms
 from gewittergefahr.gg_utils import radar_utils
@@ -17,6 +18,8 @@ from gewittergefahr.deep_learning import storm_images
 from gewittergefahr.deep_learning import deep_learning_utils as dl_utils
 from gewittergefahr.deep_learning import deployment_io
 from gewittergefahr.deep_learning import training_validation_io as trainval_io
+from gewittergefahr.deep_learning import model_activation
+from gewittergefahr.deep_learning import cnn
 from gewittergefahr.plotting import plotting_utils
 from gewittergefahr.plotting import sounding_plotting
 from gewittergefahr.plotting import radar_plotting
@@ -40,6 +43,7 @@ STORM_IDS_KEY = 'storm_ids'
 STORM_TIMES_KEY = 'storm_times_unix_sec'
 RADAR_IMAGE_MATRIX_KEY = 'radar_image_matrix'
 SOUNDING_MATRIX_KEY = 'sounding_matrix'
+STORM_ACTIVATIONS_KEY = 'storm_activations'
 
 LARGE_INTEGER = int(1e10)
 TIME_FORMAT = '%Y-%m-%d-%H%M%S'
@@ -48,7 +52,8 @@ NUM_PANEL_ROWS = 3
 TITLE_FONT_SIZE = 20
 DOTS_PER_INCH = 300
 
-METADATA_FILE_ARG_NAME = 'input_storm_metafile_name'
+ACTIVATION_FILE_ARG_NAME = 'input_activation_file_name'
+STORM_METAFILE_ARG_NAME = 'input_storm_metafile_name'
 RADAR_IMAGE_DIR_ARG_NAME = 'input_radar_image_dir_name'
 RADAR_SOURCE_ARG_NAME = 'radar_source'
 RADAR_FIELDS_ARG_NAME = 'radar_field_names'
@@ -61,11 +66,18 @@ SOUNDING_LAG_TIME_ARG_NAME = 'sounding_lag_time_sec'
 SOUNDING_LEAD_TIME_ARG_NAME = 'sounding_lead_time_sec'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
-METADATA_FILE_HELP_STRING = (
-    'Path to input file, containing ID/time of each storm object to plot.  This'
-    ' file should contain a single dictionary with the keys "{0:s}" and '
-    '"{1:s}".'
-).format(STORM_IDS_KEY, STORM_TIMES_KEY)
+ACTIVATION_FILE_HELP_STRING = (
+    'Path to activation file.  This will be read by `model_activation.'
+    'read_file`, and all storm objects therein will be plotted.  If this '
+    'argument is empty, `{0:s}` will be used, instead.'
+).format(STORM_METAFILE_ARG_NAME)
+
+STORM_METAFILE_HELP_STRING = (
+    'Path to metafile (with ID-time for each storm object).  This will be read '
+    'by `storm_tracking_io.read_storm_ids_and_times`, and all storm objects '
+    'therein will be plotted.  If this argument is empty, `{0:s}` will be used,'
+    ' instead.'
+).format(ACTIVATION_FILE_ARG_NAME)
 
 RADAR_IMAGE_DIR_HELP_STRING = (
     'Name of top-level directory with storm-centered radar images.  Files '
@@ -73,68 +85,76 @@ RADAR_IMAGE_DIR_HELP_STRING = (
     '`storm_images.read_storm_images`.')
 
 RADAR_SOURCE_HELP_STRING = (
-    'Radar source.  Must be one of the following list.\n{0:s}'
-).format(str(radar_utils.DATA_SOURCE_IDS))
+    '[used only if `{0:s}` is empty] Radar source.  Must be in the following '
+    'list.\n{1:s}'
+).format(ACTIVATION_FILE_ARG_NAME, str(radar_utils.DATA_SOURCE_IDS))
 
 RADAR_FIELDS_HELP_STRING = (
-    'List of radar fields to plot.  Each must be accepted by `radar_utils.'
-    'check_field_name`.')
+    '[used only if `{0:s}` is empty] List of radar fields to plot.  Each must '
+    'be in the following list.\n{1:s}'
+).format(ACTIVATION_FILE_ARG_NAME, str(radar_utils.RADAR_FIELD_NAMES))
 
 RADAR_HEIGHTS_HELP_STRING = (
-    '[used only if {0:s} = "{1:s}"] List of radar heights.  Each field in '
-    '`{2:s}` will be plotted at each height.'
-).format(RADAR_SOURCE_ARG_NAME, radar_utils.GRIDRAD_SOURCE_ID,
-         RADAR_FIELDS_ARG_NAME)
+    '[used only if {0:s} is empty and {1:s} = "{2:s}"] List of radar heights.  '
+    'Each field in `{3:s}` will be plotted at each height.'
+).format(ACTIVATION_FILE_ARG_NAME, RADAR_SOURCE_ARG_NAME,
+         radar_utils.GRIDRAD_SOURCE_ID, RADAR_FIELDS_ARG_NAME)
 
 REFL_HEIGHTS_HELP_STRING = (
-    '[used only if {0:s} = "{1:s}"] List of reflectivity heights.  "{2:s}" will'
-    ' be plotted at each of these heights.'
-).format(RADAR_SOURCE_ARG_NAME, radar_utils.MYRORSS_SOURCE_ID,
-         radar_utils.REFL_NAME)
+    '[used only if {0:s} is empty and {1:s} = "{2:s}"] List of reflectivity '
+    'heights.  Field "{3:s}" will be plotted at each height.'
+).format(ACTIVATION_FILE_ARG_NAME, RADAR_SOURCE_ARG_NAME,
+         radar_utils.MYRORSS_SOURCE_ID, radar_utils.REFL_NAME)
 
 NUM_ROWS_TO_KEEP_HELP_STRING = (
-    'Number of rows to keep from each storm-centered radar image.  If less than'
-    ' total number of rows, images will be cropped around the center.  To use '
-    'the full images, leave this alone.')
+    '[used only if {0:s} is empty] Number of rows to keep in each storm-'
+    'centered radar image.  To use the full images, leave this alone.'
+).format(ACTIVATION_FILE_ARG_NAME)
 
 NUM_COLUMNS_TO_KEEP_HELP_STRING = (
-    'Number of columns to keep from each storm-centered radar image.  If less '
-    'than total number of columns, images will be cropped around the center.  '
-    'To use the full images, leave this alone.')
+    '[used only if {0:s} is empty] Number of columns to keep in each storm-'
+    'centered radar image.  To use the full images, leave this alone.'
+).format(ACTIVATION_FILE_ARG_NAME)
 
 SOUNDING_DIR_HELP_STRING = (
     'Name of top-level directory with storm-centered soundings.  Files therein '
     'will be found by `soundings_only.find_sounding_file` and read by '
-    '`soundings_only.read_soundings`.  If you do not want to plot soundings, '
-    'leave this argument alone.')
+    '`soundings_only.read_soundings`.  To plot only radar (no soundings), leave'
+    ' this alone.')
 
 SOUNDING_LAG_TIME_HELP_STRING = (
-    'Lag time (used to find sounding files).  If you do not want to plot '
-    'soundings, leave this argument alone.')
+    '[used only if {0:s} is empty and {1:s} is non-empty] Lag time (used to '
+    'find sounding files).'
+).format(ACTIVATION_FILE_ARG_NAME, SOUNDING_DIR_ARG_NAME)
 
 SOUNDING_LEAD_TIME_HELP_STRING = (
-    'Lead time (used to find sounding files).  If you do not want to plot '
-    'soundings, leave this argument alone.')
+    '[used only if {0:s} is empty and {1:s} is non-empty] Lead time (used to '
+    'find sounding files).'
+).format(ACTIVATION_FILE_ARG_NAME, SOUNDING_DIR_ARG_NAME)
 
 OUTPUT_DIR_HELP_STRING = (
     'Name of output directory.  Figures will be saved here.')
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
-    '--' + METADATA_FILE_ARG_NAME, type=str, required=True,
-    help=METADATA_FILE_HELP_STRING)
+    '--' + ACTIVATION_FILE_ARG_NAME, type=str, required=False, default='',
+    help=ACTIVATION_FILE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + STORM_METAFILE_ARG_NAME, type=str, required=False, default='',
+    help=STORM_METAFILE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + RADAR_IMAGE_DIR_ARG_NAME, type=str, required=True,
     help=RADAR_IMAGE_DIR_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + RADAR_SOURCE_ARG_NAME, type=str, required=True,
+    '--' + RADAR_SOURCE_ARG_NAME, type=str, required=False, default='',
     help=RADAR_SOURCE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + RADAR_FIELDS_ARG_NAME, type=str, nargs='+', required=True,
-    help=RADAR_FIELDS_HELP_STRING)
+    '--' + RADAR_FIELDS_ARG_NAME, type=str, nargs='+', required=False,
+    default=[''], help=RADAR_FIELDS_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + RADAR_HEIGHTS_ARG_NAME, type=int, nargs='+', required=False,
@@ -157,11 +177,11 @@ INPUT_ARG_PARSER.add_argument(
     help=SOUNDING_DIR_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + SOUNDING_LAG_TIME_ARG_NAME, type=int, required=False, default=1800,
+    '--' + SOUNDING_LAG_TIME_ARG_NAME, type=int, required=False, default=-1,
     help=SOUNDING_LAG_TIME_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + SOUNDING_LEAD_TIME_ARG_NAME, type=int, required=False, default=0,
+    '--' + SOUNDING_LEAD_TIME_ARG_NAME, type=int, required=False, default=-1,
     help=SOUNDING_LEAD_TIME_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
@@ -355,7 +375,11 @@ def _read_inputs(
 def _plot_storm_objects(storm_object_dict, output_dir_name):
     """Plots radar and sounding data for each storm object.
 
-    :param storm_object_dict: Dictionary created by `_read_inputs`.
+    :param storm_object_dict: Dictionary created by `_read_inputs`, but with one
+        additional key.
+    storm_object_dict['storm_activations']: length-E numpy array of model
+        activations, where E = number of storm objects.  This may also be None,
+        in which case activations will not be included in figure titles.
     :param output_dir_name: Name of output directory.  Figures will be saved
         here.
     """
@@ -368,6 +392,7 @@ def _plot_storm_objects(storm_object_dict, output_dir_name):
     storm_times_unix_sec = storm_object_dict[STORM_TIMES_KEY]
     radar_image_matrix = storm_object_dict[RADAR_IMAGE_MATRIX_KEY]
     sounding_matrix = storm_object_dict[SOUNDING_MATRIX_KEY]
+    storm_activations = storm_object_dict[STORM_ACTIVATIONS_KEY]
 
     plot_soundings = sounding_matrix is not None
     if plot_soundings:
@@ -384,6 +409,10 @@ def _plot_storm_objects(storm_object_dict, output_dir_name):
             storm_times_unix_sec[i], TIME_FORMAT)
         this_base_title_string = 'Storm "{0:s}" at {1:s}'.format(
             storm_ids[i], this_time_string)
+        if storm_activations is not None:
+            this_base_title_string += ' (activation = {0:.3f})'.format(
+                storm_activations[i])
+
         this_base_file_name = '{0:s}/storm={1:s}_{2:s}'.format(
             output_dir_name, storm_ids[i].replace('_', '-'), this_time_string)
 
@@ -446,15 +475,17 @@ def _plot_storm_objects(storm_object_dict, output_dir_name):
 
 
 def _run(
-        storm_metadata_file_name, top_radar_image_dir_name, radar_source,
-        radar_field_names, radar_heights_m_asl, refl_heights_m_asl,
-        num_rows_to_keep, num_columns_to_keep, top_sounding_dir_name,
-        sounding_lag_time_sec, sounding_lead_time_sec, output_dir_name):
+        input_activation_file_name, input_storm_metafile_name,
+        top_radar_image_dir_name, radar_source, radar_field_names,
+        radar_heights_m_asl, refl_heights_m_asl, num_rows_to_keep,
+        num_columns_to_keep, top_sounding_dir_name, sounding_lag_time_sec,
+        sounding_lead_time_sec, output_dir_name):
     """Plots radar and sounding data for each storm object.
 
     This is effectively the main method.
 
-    :param storm_metadata_file_name: See documentation at top of file.
+    :param input_activation_file_name: See documentation at top of file.
+    :param input_storm_metafile_name: Same.
     :param top_radar_image_dir_name: Same.
     :param radar_source: Same.
     :param radar_field_names: Same.
@@ -466,25 +497,71 @@ def _run(
     :param sounding_lag_time_sec: Same.
     :param sounding_lead_time_sec: Same.
     :param output_dir_name: Same.
+    :raises: ValueError: if the activation file contains activations for more
+        than one model component.
     """
+
+    if input_activation_file_name != '':
+
+        # Read activation file.
+        print 'Reading data from: "{0:s}"...'.format(input_activation_file_name)
+        (activation_matrix, activation_metadata_dict
+        ) = model_activation.read_file(input_activation_file_name)
+
+        num_model_components = activation_matrix.shape[1]
+        if num_model_components > 1:
+            error_string = (
+                'The file should contain activations for only one model '
+                'component, not {0:d}.'
+            ).format(num_model_components)
+            raise ValueError(error_string)
+
+        storm_ids = activation_metadata_dict[model_activation.STORM_IDS_KEY]
+        storm_times_unix_sec = activation_metadata_dict[
+            model_activation.STORM_TIMES_KEY]
+        storm_activations = activation_matrix[:, 0]
+
+        # Read metadata for model that generated the activations.
+        model_file_name = activation_metadata_dict[
+            model_activation.MODEL_FILE_NAME_KEY]
+        model_metafile_name = '{0:s}/model_metadata.p'.format(
+            os.path.split(model_file_name)[0])
+
+        print 'Reading metadata from: "{0:s}"...'.format(model_metafile_name)
+        model_metadata_dict = cnn.read_model_metadata(model_metafile_name)
+
+        radar_source = model_metadata_dict[cnn.RADAR_SOURCE_KEY]
+        radar_field_names = model_metadata_dict[cnn.RADAR_FIELD_NAMES_KEY]
+        radar_heights_m_asl = model_metadata_dict[cnn.RADAR_HEIGHTS_KEY]
+        refl_heights_m_asl = model_metadata_dict[cnn.REFLECTIVITY_HEIGHTS_KEY]
+        num_rows_to_keep = model_metadata_dict[cnn.NUM_ROWS_TO_KEEP_KEY]
+        num_columns_to_keep = model_metadata_dict[cnn.NUM_COLUMNS_TO_KEEP_KEY]
+        sounding_lag_time_sec = model_metadata_dict[cnn.SOUNDING_LAG_TIME_KEY]
+
+        target_param_dict = labels.column_name_to_label_params(
+            model_metadata_dict[cnn.TARGET_NAME_KEY])
+        min_lead_time_sec = target_param_dict[labels.MIN_LEAD_TIME_KEY]
+        max_lead_time_sec = target_param_dict[labels.MAX_LEAD_TIME_KEY]
+        sounding_lead_time_sec = int(numpy.round(
+            numpy.mean([min_lead_time_sec, max_lead_time_sec])))
+    else:
+
+        # Read storm IDs and times.
+        print 'Reading data from: "{0:s}"...'.format(input_storm_metafile_name)
+        storm_ids, storm_times_unix_sec = tracking_io.read_storm_ids_and_times(
+            input_storm_metafile_name)
+        storm_activations = None
 
     if num_rows_to_keep <= 0:
         num_rows_to_keep = None
     if num_columns_to_keep <= 0:
         num_columns_to_keep = None
-
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=output_dir_name)
 
-    print 'Reading storm ID/time pairs from: "{0:s}"...'.format(
-        storm_metadata_file_name)
-    pickle_file_handle = open(storm_metadata_file_name, 'rb')
-    storm_object_dict = pickle.load(pickle_file_handle)
-    pickle_file_handle.close()
-
     storm_object_dict = _read_inputs(
-        storm_ids=storm_object_dict[STORM_IDS_KEY],
-        storm_times_unix_sec=storm_object_dict[STORM_TIMES_KEY],
+        storm_ids=storm_ids,
+        storm_times_unix_sec=storm_times_unix_sec,
         top_radar_image_dir_name=top_radar_image_dir_name,
         radar_source=radar_source, radar_field_names=radar_field_names,
         radar_heights_m_asl=radar_heights_m_asl,
@@ -496,6 +573,15 @@ def _run(
         sounding_lead_time_sec=sounding_lead_time_sec)
     print SEPARATOR_STRING
 
+    if storm_activations is not None:
+        these_indices = storm_images.find_storm_objects(
+            all_storm_ids=storm_object_dict[STORM_IDS_KEY],
+            all_valid_times_unix_sec=storm_object_dict[STORM_TIMES_KEY],
+            storm_ids_to_keep=storm_ids,
+            valid_times_to_keep_unix_sec=storm_times_unix_sec)
+        storm_activations = storm_activations[these_indices]
+
+    storm_object_dict.update({STORM_ACTIVATIONS_KEY: storm_activations})
     _plot_storm_objects(
         storm_object_dict=storm_object_dict, output_dir_name=output_dir_name)
 
@@ -504,8 +590,10 @@ if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
     _run(
-        storm_metadata_file_name=getattr(
-            INPUT_ARG_OBJECT, METADATA_FILE_ARG_NAME),
+        input_activation_file_name=getattr(
+            INPUT_ARG_OBJECT, ACTIVATION_FILE_ARG_NAME),
+        input_storm_metafile_name=getattr(
+            INPUT_ARG_OBJECT, STORM_METAFILE_ARG_NAME),
         top_radar_image_dir_name=getattr(
             INPUT_ARG_OBJECT, RADAR_IMAGE_DIR_ARG_NAME),
         radar_source=getattr(INPUT_ARG_OBJECT, RADAR_SOURCE_ARG_NAME),
