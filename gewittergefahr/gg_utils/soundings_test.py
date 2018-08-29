501fb638959171f42f9a2de0e25e440e068689c3
thunderhoser <ryan.lagerquist@ou.edu>
2018-08-28 23:19:46 -0500

    Fixed unit test for soundings.py (pursuant to the last commit).

--- gewittergefahr/gg_utils/soundings_test.py
+++ gewittergefahr/gg_utils/soundings_test.py
@@ -221,9 +221,10 @@ SOUNDING_DICT_P_COORDS_NO_SURFACE = {
 }
 
 THIS_MATRIX = numpy.array(
-    [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+      2000],
      [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
-      40, 42]], dtype=float)
+      40, 4200]], dtype=float)
 INTERP_TABLE_WITH_SURFACE = pandas.DataFrame(THIS_MATRIX)
 
 for k in range(len(FIELD_NAMES_WITH_SURFACE)):
