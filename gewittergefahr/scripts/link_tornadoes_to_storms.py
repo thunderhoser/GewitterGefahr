60ab6c4864b3644b29b56a447c8d51eab8a21cd3
thunderhoser <ryan.lagerquist@ou.edu>
2018-11-30 23:09:02 -0600

    Modified scripts to handle new linkage.py.

--- gewittergefahr/scripts/link_tornadoes_to_storms.py
+++ gewittergefahr/scripts/link_tornadoes_to_storms.py
@@ -1,15 +1,11 @@
-"""Links each storm cell to zero or more tornadoes."""
+"""Runs `linkage.link_tornadoes_to_storms`."""
 
 import argparse
 import numpy
 from gewittergefahr.gg_io import storm_tracking_io as tracking_io
 from gewittergefahr.gg_utils import storm_tracking_utils as tracking_utils
 from gewittergefahr.gg_utils import echo_top_tracking
-from gewittergefahr.gg_utils import link_events_to_storms as events2storms
-
-# TODO(thunderhoser): All input args to
-# `link_events_to_storms.link_each_storm_to_tornadoes` should be input args to
-# this script.
+from gewittergefahr.gg_utils import linkage
 
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
@@ -17,24 +13,30 @@ SPC_DATE_ARG_NAME = 'spc_date_string'
 TORNADO_DIR_ARG_NAME = 'input_tornado_dir_name'
 TRACKING_DIR_ARG_NAME = 'input_tracking_dir_name'
 TRACKING_SCALE_ARG_NAME = 'tracking_scale_metres2'
-LINKAGE_DIR_ARG_NAME = 'output_linkage_dir_name'
+OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 SPC_DATE_HELP_STRING = (
-    'SPC (Storm Prediction Center) date in format "yyyymmdd".  Each storm cell '
-    'on this date will be linked to zero or more tornadoes.')
+    'SPC date (format "yyyymmdd").  Each storm cell on this day will be linked '
+    'to zero or more wind observations.')
+
 TORNADO_DIR_HELP_STRING = (
-    'Name of top-level directory with tornado observations (files readable by'
-    ' `tornado_io.read_processed_file`).')
+    'Name of directory with tornado observations.  Relevant files will be found'
+    ' by `tornado_io.find_processed_file` and read by '
+    '`tornado_io.read_processed_file`.')
+
 TRACKING_DIR_HELP_STRING = (
-    'Name of top-level directory with storm tracks (files readable by'
-    ' `storm_tracking_io.read_processed_file`).')
+    'Name of top-level tracking directory.  Files therein will be found by '
+    '`storm_tracking_io.find_processed_files_one_spc_date` and read by '
+    '`storm_tracking_io.read_processed_file`.')
+
 TRACKING_SCALE_HELP_STRING = (
-    'Tracking scale (minimum object area).  This is used to find exact files in'
-    ' `{0:s}`.').format(TRACKING_DIR_ARG_NAME)
-LINKAGE_DIR_HELP_STRING = (
-    'Name of top-level directory for linkage files (created by'
-    ' `link_events_to_storms.write_storm_to_tornadoes_table`).  One file will '
-    'be created here, for the whole SPC date.')
+    'Tracking scale (minimum object area).  Used to find files in `{0:s}`.'
+).format(TRACKING_DIR_ARG_NAME)
+
+OUTPUT_DIR_HELP_STRING = (
+    'Name of top-level directory for linkage files.  Files will be written by '
+    '`linkage.write_linkage_file`, to locations therein determined by '
+    '`linkage.find_linkage_file`.')
 
 TOP_TORNADO_DIR_NAME_DEFAULT = (
     '/condo/swatwork/ralager/tornado_observations/processed')
@@ -59,20 +61,21 @@ INPUT_ARG_PARSER.add_argument(
     default=DEFAULT_TRACKING_SCALE_METRES2, help=TRACKING_SCALE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + LINKAGE_DIR_ARG_NAME, type=str, required=True,
-    help=LINKAGE_DIR_HELP_STRING)
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_DIR_HELP_STRING)
 
 
-def _link_tornadoes_to_storms(
-        spc_date_string, top_tornado_dir_name, top_tracking_dir_name,
-        tracking_scale_metres2, top_linkage_dir_name):
-    """Links each storm cell to zero or more tornadoes.
+def _run(spc_date_string, tornado_dir_name, top_tracking_dir_name,
+         tracking_scale_metres2, top_output_dir_name):
+    """Runs `linkage.link_tornadoes_to_storms`.
+
+    This is effectively the main method.
 
     :param spc_date_string: See documentation at top of file.
-    :param top_tornado_dir_name: Same.
+    :param tornado_dir_name: Same.
     :param top_tracking_dir_name: Same.
     :param tracking_scale_metres2: Same.
-    :param top_linkage_dir_name: Same.
+    :param top_output_dir_name: Same.
     """
 
     tracking_file_names, _ = tracking_io.find_processed_files_one_spc_date(
@@ -82,34 +85,29 @@ def _link_tornadoes_to_storms(
         tracking_scale_metres2=tracking_scale_metres2,
         raise_error_if_missing=True)
 
-    storm_to_tornadoes_table = events2storms.link_each_storm_to_tornadoes(
+    storm_to_tornadoes_table = linkage.link_storms_to_tornadoes(
         tracking_file_names=tracking_file_names,
-        tornado_directory_name=top_tornado_dir_name)
+        tornado_directory_name=tornado_dir_name)
     print SEPARATOR_STRING
 
-    linkage_file_name = events2storms.find_storm_to_events_file(
-        top_directory_name=top_linkage_dir_name,
-        event_type_string=events2storms.TORNADO_EVENT_TYPE_STRING,
+    output_file_name = linkage.find_linkage_file(
+        top_directory_name=top_output_dir_name,
+        event_type_string=linkage.TORNADO_EVENT_STRING,
         spc_date_string=spc_date_string, raise_error_if_missing=False)
 
-    print 'Writing linkages to: "{0:s}"...'.format(linkage_file_name)
-    events2storms.write_storm_to_tornadoes_table(
-        storm_to_tornadoes_table=storm_to_tornadoes_table,
-        pickle_file_name=linkage_file_name)
+    print 'Writing linkages to: "{0:s}"...'.format(output_file_name)
+    linkage.write_linkage_file(storm_to_events_table=storm_to_tornadoes_table,
+                               pickle_file_name=output_file_name)
 
 
 if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
-    SPC_DATE_STRING = getattr(INPUT_ARG_OBJECT, SPC_DATE_ARG_NAME)
-    TOP_TORNADO_DIR_NAME = getattr(INPUT_ARG_OBJECT, TORNADO_DIR_ARG_NAME)
-    TOP_TRACKING_DIR_NAME = getattr(INPUT_ARG_OBJECT, TRACKING_DIR_ARG_NAME)
-    TRACKING_SCALE_METRES2 = getattr(INPUT_ARG_OBJECT, TRACKING_SCALE_ARG_NAME)
-    TOP_LINKAGE_DIR_NAME = getattr(INPUT_ARG_OBJECT, LINKAGE_DIR_ARG_NAME)
-
-    _link_tornadoes_to_storms(
-        spc_date_string=SPC_DATE_STRING,
-        top_tornado_dir_name=TOP_TORNADO_DIR_NAME,
-        top_tracking_dir_name=TOP_TRACKING_DIR_NAME,
-        tracking_scale_metres2=TRACKING_SCALE_METRES2,
-        top_linkage_dir_name=TOP_LINKAGE_DIR_NAME)
+    _run(
+        spc_date_string=getattr(INPUT_ARG_OBJECT, SPC_DATE_ARG_NAME),
+        tornado_dir_name=getattr(INPUT_ARG_OBJECT, TORNADO_DIR_ARG_NAME),
+        top_tracking_dir_name=getattr(INPUT_ARG_OBJECT, TRACKING_DIR_ARG_NAME),
+        tracking_scale_metres2=getattr(
+            INPUT_ARG_OBJECT, TRACKING_SCALE_ARG_NAME),
+        top_output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
+    )
--- gewittergefahr/scripts/share_linkages_across_spc_dates.py
+++ gewittergefahr/scripts/share_linkages_across_spc_dates.py
@@ -1,41 +1,38 @@
-"""Shares linkage info across SPC dates.
-
-For more information on this procedure, see documentation for
-`link_events_to_storms.share_linkages_across_spc_dates`.
-"""
+"""Runs `linkage.share_linkages_across_spc_dates`."""
 
 import argparse
-from gewittergefahr.gg_utils import link_events_to_storms as events2storms
+from gewittergefahr.gg_utils import linkage
 
-FIRST_SPC_DATE_ARG_NAME = 'first_spc_date_string'
-LAST_SPC_DATE_ARG_NAME = 'last_spc_date_string'
+FIRST_DATE_ARG_NAME = 'first_spc_date_string'
+LAST_DATE_ARG_NAME = 'last_spc_date_string'
 INPUT_DIR_ARG_NAME = 'input_dir_name'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 EVENT_TYPE_ARG_NAME = 'event_type_string'
 
 SPC_DATE_HELP_STRING = (
-    'SPC (Storm Prediction Center) date in format "yyyymmdd".  Linkage info '
-    'will be shared across all dates from `{0:s}`...`{1:s}`.'
-).format(FIRST_SPC_DATE_ARG_NAME, LAST_SPC_DATE_ARG_NAME)
+    'SPC date (format "yyyymmdd").  Linkages will be shared across all dates '
+    'from `{0:s}`...`{1:s}`.'
+).format(FIRST_DATE_ARG_NAME, LAST_DATE_ARG_NAME)
 
 INPUT_DIR_HELP_STRING = (
-    'Name of top-level input directory with original linkage files (before '
-    'sharing info across SPC dates).')
+    'Name of top-level input directory.  Files therein will be found by '
+    '`find_linkage_file` and read by `read_linkage_file`.')
 
 OUTPUT_DIR_HELP_STRING = (
-    'Name of top-level output directory for new linkage files (after sharing '
-    'across SPC dates).')
+    'Name of top-level output directory.  Files will be written by '
+    '`write_linkage_file` to locations therein, determined by '
+    '`find_linkage_file`.')
 
 EVENT_TYPE_HELP_STRING = (
-    'Type of linkages to be handled (either "wind" or "tornado").')
+    'Event type (must be accepted by `linkage.check_event_type`).')
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
-    '--' + FIRST_SPC_DATE_ARG_NAME, type=str, required=True,
+    '--' + FIRST_DATE_ARG_NAME, type=str, required=True,
     help=SPC_DATE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + LAST_SPC_DATE_ARG_NAME, type=str, required=True,
+    '--' + LAST_DATE_ARG_NAME, type=str, required=True,
     help=SPC_DATE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
@@ -53,10 +50,11 @@ INPUT_ARG_PARSER.add_argument(
 if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
-    events2storms.share_linkages_across_spc_dates(
+    linkage.share_linkages_across_spc_dates(
         first_spc_date_string=getattr(
-            INPUT_ARG_OBJECT, FIRST_SPC_DATE_ARG_NAME),
-        last_spc_date_string=getattr(INPUT_ARG_OBJECT, LAST_SPC_DATE_ARG_NAME),
+            INPUT_ARG_OBJECT, FIRST_DATE_ARG_NAME),
+        last_spc_date_string=getattr(INPUT_ARG_OBJECT, LAST_DATE_ARG_NAME),
         top_input_dir_name=getattr(INPUT_ARG_OBJECT, INPUT_DIR_ARG_NAME),
         top_output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME),
-        event_type_string=getattr(INPUT_ARG_OBJECT, EVENT_TYPE_ARG_NAME))
+        event_type_string=getattr(INPUT_ARG_OBJECT, EVENT_TYPE_ARG_NAME)
+    )
