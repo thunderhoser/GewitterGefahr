f779992af88a5acddcc52713062df80014471576
thunderhoser <ryan.lagerquist@ou.edu>
2018-12-03 20:03:17 -0600

    Added permutation.py and run_permutation_test.py.

---
+++ gewittergefahr/deep_learning/permutation.py
@@ -0,0 +1,272 @@
+"""Methods for running the permutation test.
+
+The permutation test (or "permutation-based variable importance") is explained
+in Lakshmanan et al. (2015).
+
+--- REFERENCES ---
+
+Lakshmanan, V., C. Karstens, J. Krause, K. Elmore, A. Ryzhkov, and S. Berkseth,
+    2015: "Which polarimetric variables are important for weather/no-weather
+    discrimination?" Journal of Atmospheric and Oceanic Technology, 32 (6),
+    1209-1223.
+"""
+
+import copy
+import numpy
+import keras.utils
+from gewittergefahr.gg_utils import error_checking
+from gewittergefahr.deep_learning import cnn
+
+
+def prediction_function_2d_cnn(model_object, list_of_input_matrices):
+    """Prediction function for 2-D GewitterGefahr CNN.
+
+    E = number of examples
+    K = number of target classes
+
+    :param model_object: See doc for `run_permutation_test`.
+    :param list_of_input_matrices: Same.
+    :return: class_probability_matrix: E-by-K numpy array, where
+        class_probability_matrix[i, k] is the probability that the [i]th example
+        belongs to the [k]th class.
+    """
+
+    if len(list_of_input_matrices) == 2:
+        sounding_matrix = list_of_input_matrices[1]
+    else:
+        sounding_matrix = None
+
+    return cnn.apply_2d_cnn(
+        model_object=model_object, radar_image_matrix=list_of_input_matrices[0],
+        sounding_matrix=sounding_matrix)
+
+
+def prediction_function_3d_cnn(model_object, list_of_input_matrices):
+    """Prediction function for 3-D GewitterGefahr CNN.
+
+    E = number of examples
+    K = number of target classes
+
+    :param model_object: See doc for `run_permutation_test`.
+    :param list_of_input_matrices: Same.
+    :return: class_probability_matrix: E-by-K numpy array, where
+        class_probability_matrix[i, k] is the probability that the [i]th example
+        belongs to the [k]th class.
+    """
+
+    if len(list_of_input_matrices) == 2:
+        sounding_matrix = list_of_input_matrices[1]
+    else:
+        sounding_matrix = None
+
+    return cnn.apply_3d_cnn(
+        model_object=model_object, radar_image_matrix=list_of_input_matrices[0],
+        sounding_matrix=sounding_matrix)
+
+
+def prediction_function_2d3d_cnn(model_object, list_of_input_matrices):
+    """Prediction function for hybrid 2D/3D GewitterGefahr CNN.
+
+    E = number of examples
+    K = number of target classes
+
+    :param model_object: See doc for `run_permutation_test`.
+    :param list_of_input_matrices: Same.
+    :return: class_probability_matrix: E-by-K numpy array, where
+        class_probability_matrix[i, k] is the probability that the [i]th example
+        belongs to the [k]th class.
+    """
+
+    if len(list_of_input_matrices) == 3:
+        sounding_matrix = list_of_input_matrices[2]
+    else:
+        sounding_matrix = None
+
+    return cnn.apply_2d3d_cnn(
+        model_object=model_object,
+        reflectivity_image_matrix_dbz=list_of_input_matrices[0],
+        az_shear_image_matrix_s01=list_of_input_matrices[1],
+        sounding_matrix=sounding_matrix)
+
+
+def cross_entropy_function(target_values, class_probability_matrix):
+    """Cross-entropy cost function.
+
+    This function works for binary or multi-class classification.
+
+    :param target_values: See doc for `run_permutation_test`.
+    :param class_probability_matrix: Same.
+    :return: cross_entropy: Scalar.
+    """
+
+    num_examples = class_probability_matrix.shape[0]
+    num_classes = class_probability_matrix.shape[1]
+
+    target_matrix = keras.utils.to_categorical(
+        target_values, num_classes
+    ).astype(int)
+
+    return -1 * numpy.sum(
+        target_matrix * numpy.log2(class_probability_matrix)
+    ) / num_examples
+
+
+def run_permutation_test(
+        model_object, list_of_input_matrices, predictor_names_by_matrix,
+        target_values, prediction_function, cost_function):
+    """Runs the permutation test.
+
+    N = number of input matrices
+    E = number of examples
+    C_i = number of channels (predictors) in the [i]th matrix
+    K = number of target classes
+
+    :param model_object: Trained instance of `keras.models.Model` or
+        `keras.models.Sequential`.
+    :param list_of_input_matrices: length-T list of matrices (numpy arrays), in
+        the order that they were fed to the model for training.  In other words,
+        if the order of training matrices was [radar images, soundings], the
+        order of these matrices must be [radar images, soundings].  The first
+        axis of each matrix should have length E, and the last axis of the [i]th
+        matrix should have length C_i.
+    :param predictor_names_by_matrix: length-T list of lists.  The [i]th list
+        should be a list of predictor variables in the [i]th matrix, with length
+        C_i.
+    :param target_values: length-E numpy array of target values (integer class
+        labels).
+    :param prediction_function: Function used to generate predictions from the
+        model.  Should have the following inputs and outputs.
+    Input: model_object: Same as input to this method.
+    Input: list_of_input_matrices: Same as input to this method, except maybe
+        with permuted values.
+    Output: class_probability_matrix: E-by-K numpy array, where
+        class_probability_matrix[i, k] is the probability that the [i]th example
+        belongs to the [k]th class.
+
+    :param cost_function: Function used to evaluate predictions from the model.
+        Should have the following inputs and outputs.  This method will assume
+        that lower values are better.  In other words, the cost function must be
+        negatively oriented.
+    Input: target_values: Same as input to this method.
+    Input: class_probability_matrix: Output from `prediction_function`.
+    Output: cost: Scalar value.
+
+    :return: Not sure yet.
+    :raises: ValueError: if length of `list_of_input_matrices` != length of
+        `predictor_names_by_matrix`.
+    :raises: ValueError: if any input matrix has < 3 dimensions.
+    """
+
+    error_checking.assert_is_integer_numpy_array(target_values)
+    error_checking.assert_is_geq_numpy_array(target_values, 0)
+
+    if len(list_of_input_matrices) != len(predictor_names_by_matrix):
+        error_string = (
+            'Number of input matrices ({0:d}) should equal number of predictor-'
+            'name lists ({1:d}).'
+        ).format(len(list_of_input_matrices), len(predictor_names_by_matrix))
+
+        raise ValueError(error_string)
+
+    num_input_matrices = len(list_of_input_matrices)
+    num_examples = len(target_values)
+
+    for i in range(num_input_matrices):
+        error_checking.assert_is_numpy_array_without_nan(
+            list_of_input_matrices[i])
+
+        this_num_dimensions = len(list_of_input_matrices[i].shape)
+        if this_num_dimensions < 3:
+            error_string = (
+                '{0:d}th input matrix has {1:d} dimensions.  Should have at '
+                'least 3.'
+            ).format(i + 1, this_num_dimensions)
+
+            raise ValueError(error_string)
+
+        error_checking.assert_is_string_list(predictor_names_by_matrix[i])
+        this_num_predictors = len(predictor_names_by_matrix[i])
+
+        these_expected_dimensions = (
+            (num_examples,) + list_of_input_matrices[i].shape[1:-1] +
+            (this_num_predictors,)
+        )
+        these_expected_dimensions = numpy.array(
+            these_expected_dimensions, dtype=int)
+
+        error_checking.assert_is_numpy_array(
+            list_of_input_matrices[i],
+            exact_dimensions=these_expected_dimensions)
+
+    # Get original cost (with no permutation).
+    class_probability_matrix = prediction_function(
+        model_object, list_of_input_matrices)
+    original_cost = cost_function(target_values, class_probability_matrix)
+    print 'Original cost (no permutation): {0:.4e}'.format(original_cost)
+
+    remaining_predictor_names_by_matrix = copy.deepcopy(
+        predictor_names_by_matrix)
+    step_num = 0
+
+    while True:
+        print '\n'
+        step_num += 1
+
+        highest_cost = -numpy.inf
+        best_matrix_index = None
+        best_predictor_name = None
+        best_predictor_permuted_values = None
+
+        stopping_criterion = True
+
+        for i in range(num_input_matrices):
+            if len(remaining_predictor_names_by_matrix[i]) == 0:
+                continue
+
+            for this_predictor_name in remaining_predictor_names_by_matrix[i]:
+                stopping_criterion = False
+
+                print (
+                    'Trying predictor "{0:s}" at step {1:d} of permutation '
+                    'test...'
+                ).format(this_predictor_name, step_num)
+
+                these_input_matrices = copy.deepcopy(list_of_input_matrices)
+                this_predictor_index = predictor_names_by_matrix[i].index(
+                    this_predictor_name)
+
+                these_input_matrices[i][..., this_predictor_index] = (
+                    numpy.random.permutation(
+                        these_input_matrices[i][..., this_predictor_index])
+                )
+
+                this_probability_matrix = prediction_function(
+                    model_object, these_input_matrices)
+                this_cost = cost_function(
+                    target_values, this_probability_matrix)
+
+                if this_cost < highest_cost:
+                    continue
+
+                highest_cost = this_cost + 0.
+                best_matrix_index = i
+                best_predictor_name = this_predictor_name + ''
+                best_predictor_permuted_values = these_input_matrices[i][
+                    ..., this_predictor_index]
+
+        if stopping_criterion:  # No more predictors to permute.
+            break
+
+        # Remove best predictor from list.
+        remaining_predictor_names_by_matrix[best_matrix_index].remove(
+            best_predictor_name)
+
+        # Leave values of best predictor permuted.
+        this_best_predictor_index = predictor_names_by_matrix[
+            best_matrix_index].index(best_predictor_name)
+        list_of_input_matrices[best_matrix_index][
+            ..., this_best_predictor_index
+        ] = best_predictor_permuted_values
+
+        print 'Best predictor = "{0:s}" ... new cost = {1:.4e}'.format(
+            best_predictor_name, highest_cost)
---
+++ gewittergefahr/scripts/run_permutation_test.py
@@ -0,0 +1,241 @@
+"""Runs permutation test for predictor importance."""
+
+import copy
+import os.path
+import argparse
+import random
+import numpy
+from keras import backend as K
+from gewittergefahr.gg_utils import radar_utils
+from gewittergefahr.gg_utils import time_conversion
+from gewittergefahr.deep_learning import cnn
+from gewittergefahr.deep_learning import input_examples
+from gewittergefahr.deep_learning import testing_io
+from gewittergefahr.deep_learning import training_validation_io as trainval_io
+from gewittergefahr.deep_learning import permutation
+
+random.seed(6695)
+numpy.random.seed(6695)
+
+K.set_session(K.tf.Session(config=K.tf.ConfigProto(
+    intra_op_parallelism_threads=1, inter_op_parallelism_threads=1
+)))
+
+SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
+
+MODEL_FILE_ARG_NAME = 'input_model_file_name'
+EXAMPLE_DIR_ARG_NAME = 'input_example_dir_name'
+FIRST_SPC_DATE_ARG_NAME = 'first_spc_date_string'
+LAST_SPC_DATE_ARG_NAME = 'last_spc_date_string'
+NUM_EXAMPLES_ARG_NAME = 'num_examples'
+CLASS_FRACTION_KEYS_ARG_NAME = 'class_fraction_keys'
+CLASS_FRACTION_VALUES_ARG_NAME = 'class_fraction_values'
+OUTPUT_FILE_ARG_NAME = 'output_file_name'
+
+MODEL_FILE_HELP_STRING = (
+    'Path to file with trained CNN.  Will be read by `cnn.read_model`.')
+
+EXAMPLE_DIR_HELP_STRING = (
+    'Name of top-level directory with input examples.  Files therein will be '
+    'found by `input_examples.find_example_file` and read by '
+    '`input_examples.read_example_file`.')
+
+SPC_DATE_HELP_STRING = (
+    'SPC date (format "yyyymmdd").  This script will evaluate predictions on '
+    'examples from the period `{0:s}`...`{1:s}`.'
+).format(FIRST_SPC_DATE_ARG_NAME, LAST_SPC_DATE_ARG_NAME)
+
+NUM_EXAMPLES_HELP_STRING = 'Number of examples to use.'
+
+CLASS_FRACTION_KEYS_HELP_STRING = (
+    'List of keys used to create input `class_to_sampling_fraction_dict` for '
+    '`deep_learning_utils.sample_by_class`.  If you do not want class-'
+    'conditional sampling, leave this alone.'
+)
+
+CLASS_FRACTION_VALUES_HELP_STRING = (
+    'List of values used to create input `class_to_sampling_fraction_dict` for '
+    '`deep_learning_utils.sample_by_class`.  If you do not want class-'
+    'conditional sampling, leave this alone.'
+)
+
+OUTPUT_FILE_HELP_STRING = (
+    'Path to output (Pickle) file.  Will be written by'
+    '`permutation_importance.write_results`.')
+
+INPUT_ARG_PARSER = argparse.ArgumentParser()
+INPUT_ARG_PARSER.add_argument(
+    '--' + MODEL_FILE_ARG_NAME, type=str, required=True,
+    help=MODEL_FILE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + EXAMPLE_DIR_ARG_NAME, type=str, required=True,
+    help=EXAMPLE_DIR_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + FIRST_SPC_DATE_ARG_NAME, type=str, required=True,
+    help=SPC_DATE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + LAST_SPC_DATE_ARG_NAME, type=str, required=True,
+    help=SPC_DATE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_EXAMPLES_ARG_NAME, type=int, required=True,
+    help=NUM_EXAMPLES_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + CLASS_FRACTION_KEYS_ARG_NAME, type=int, nargs='+',
+    required=False, default=[0], help=CLASS_FRACTION_KEYS_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + CLASS_FRACTION_VALUES_ARG_NAME, type=float, nargs='+',
+    required=False, default=[0.], help=CLASS_FRACTION_VALUES_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_FILE_ARG_NAME, type=str, required=True,
+    help=OUTPUT_FILE_HELP_STRING)
+
+
+def _run(model_file_name, top_example_dir_name,
+         first_spc_date_string, last_spc_date_string, num_examples,
+         class_fraction_keys, class_fraction_values, output_file_name):
+    """Runs permutation test for predictor importance.
+
+    This is effectively the main method.
+
+    :param model_file_name: See documentation at top of file.
+    :param top_example_dir_name: Same.
+    :param first_spc_date_string: Same.
+    :param last_spc_date_string: Same.
+    :param num_examples: Same.
+    :param class_fraction_keys: Same.
+    :param class_fraction_values: Same.
+    :param output_file_name: Same.
+    """
+
+    print 'Reading model from: "{0:s}"...'.format(model_file_name)
+    model_object = cnn.read_model(model_file_name)
+
+    model_directory_name, _ = os.path.split(model_file_name)
+    metadata_file_name = '{0:s}/model_metadata.p'.format(model_directory_name)
+
+    print 'Reading metadata from: "{0:s}"...'.format(metadata_file_name)
+    model_metadata_dict = cnn.read_model_metadata(metadata_file_name)
+    training_option_dict = model_metadata_dict[cnn.TRAINING_OPTION_DICT_KEY]
+
+    if len(class_fraction_keys) > 1:
+        class_to_sampling_fraction_dict = dict(zip(
+            class_fraction_keys, class_fraction_values))
+    else:
+        class_to_sampling_fraction_dict = None
+
+    training_option_dict[
+        trainval_io.SAMPLING_FRACTIONS_KEY] = class_to_sampling_fraction_dict
+
+    example_file_names = input_examples.find_many_example_files(
+        top_directory_name=top_example_dir_name, shuffled=False,
+        first_spc_date_string=first_spc_date_string,
+        last_spc_date_string=last_spc_date_string,
+        raise_error_if_any_missing=False)
+
+    training_option_dict[trainval_io.EXAMPLE_FILES_KEY] = example_file_names
+    training_option_dict[trainval_io.FIRST_STORM_TIME_KEY] = (
+        time_conversion.get_start_of_spc_date(first_spc_date_string)
+    )
+    training_option_dict[trainval_io.LAST_STORM_TIME_KEY] = (
+        time_conversion.get_end_of_spc_date(last_spc_date_string)
+    )
+
+    if model_metadata_dict[cnn.USE_2D3D_CONVOLUTION_KEY]:
+        generator_object = testing_io.example_generator_2d3d_myrorss(
+            option_dict=training_option_dict, num_examples_total=num_examples)
+    else:
+        generator_object = testing_io.example_generator_2d_or_3d(
+            option_dict=training_option_dict, num_examples_total=num_examples)
+
+    storm_ids = []
+    storm_times_unix_sec = numpy.array([], dtype=int)
+    target_values = numpy.array([], dtype=int)
+    list_of_predictor_matrices = None
+
+    for _ in range(len(example_file_names)):
+        try:
+            this_storm_object_dict = next(generator_object)
+            print SEPARATOR_STRING
+        except StopIteration:
+            break
+
+        storm_ids += this_storm_object_dict[testing_io.STORM_IDS_KEY]
+        storm_times_unix_sec = numpy.concatenate((
+            storm_times_unix_sec,
+            this_storm_object_dict[testing_io.STORM_TIMES_KEY]
+        ))
+        target_values = numpy.concatenate((
+            target_values, this_storm_object_dict[testing_io.TARGET_ARRAY_KEY]
+        ))
+
+        these_predictor_matrices = this_storm_object_dict[
+            testing_io.INPUT_MATRICES_KEY]
+
+        if list_of_predictor_matrices is None:
+            list_of_predictor_matrices = copy.deepcopy(these_predictor_matrices)
+        else:
+            for k in range(list_of_predictor_matrices):
+                list_of_predictor_matrices[k] = numpy.concatenate((
+                    list_of_predictor_matrices[k], these_predictor_matrices[k]
+                ))
+
+    if model_metadata_dict[cnn.USE_2D3D_CONVOLUTION_KEY]:
+        prediction_function = permutation.prediction_function_2d3d_cnn
+        predictor_names_by_matrix = [
+            training_option_dict[trainval_io.RADAR_FIELDS_KEY],
+            [radar_utils.REFL_NAME]
+        ]
+
+        if len(list_of_predictor_matrices) == 3:
+            predictor_names_by_matrix.append(
+                training_option_dict[trainval_io.SOUNDING_FIELDS_KEY]
+            )
+
+    else:
+        predictor_names_by_matrix = [
+            training_option_dict[trainval_io.RADAR_FIELDS_KEY]
+        ]
+
+        if len(list_of_predictor_matrices) == 2:
+            predictor_names_by_matrix.append(
+                training_option_dict[trainval_io.SOUNDING_FIELDS_KEY]
+            )
+
+        num_radar_dimensions = len(list_of_predictor_matrices[0].shape) - 2
+        if num_radar_dimensions == 2:
+            prediction_function = permutation.prediction_function_2d_cnn
+        else:
+            prediction_function = permutation.prediction_function_3d_cnn
+
+    permutation.run_permutation_test(
+        model_object=model_object,
+        list_of_input_matrices=list_of_predictor_matrices,
+        predictor_names_by_matrix=predictor_names_by_matrix,
+        target_values=target_values, prediction_function=prediction_function,
+        cost_function=permutation.cross_entropy_function)
+
+
+if __name__ == '__main__':
+    INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
+
+    _run(
+        model_file_name=getattr(INPUT_ARG_OBJECT, MODEL_FILE_ARG_NAME),
+        top_example_dir_name=getattr(INPUT_ARG_OBJECT, EXAMPLE_DIR_ARG_NAME),
+        first_spc_date_string=getattr(
+            INPUT_ARG_OBJECT, FIRST_SPC_DATE_ARG_NAME),
+        last_spc_date_string=getattr(INPUT_ARG_OBJECT, LAST_SPC_DATE_ARG_NAME),
+        num_examples=getattr(INPUT_ARG_OBJECT, NUM_EXAMPLES_ARG_NAME),
+        class_fraction_keys=numpy.array(
+            getattr(INPUT_ARG_OBJECT, CLASS_FRACTION_KEYS_ARG_NAME), dtype=int),
+        class_fraction_values=numpy.array(
+            getattr(INPUT_ARG_OBJECT, CLASS_FRACTION_VALUES_ARG_NAME),
+            dtype=float),
+        output_file_name=getattr(INPUT_ARG_OBJECT, OUTPUT_FILE_ARG_NAME)
+    )
