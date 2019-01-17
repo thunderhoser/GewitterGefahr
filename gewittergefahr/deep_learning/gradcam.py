cc63771cd1f11c9a70eb45031ac2166c6615a53c
thunderhoser <lagerqui@ualberta.ca>
2019-01-17 01:16:10 +0000

    Finished gradcam.py (for now).

--- gewittergefahr/deep_learning/gradcam.py
+++ gewittergefahr/deep_learning/gradcam.py
@@ -16,37 +16,38 @@ import keras
 from keras import backend as K
 import tensorflow
 from tensorflow.python.framework import ops as tensorflow_ops
-from cv2 import resize as cv2_resize
+from scipy.interpolate import (
+    UnivariateSpline, RectBivariateSpline, RegularGridInterpolator)
 from gewittergefahr.gg_utils import error_checking
 
+BACKPROP_FUNCTION_NAME = 'GuidedBackProp'
 
-def _register_gradient():
-    """Registers gradient with TensorFlow backend?
 
-    :return: Don't know yet.
+def _find_relevant_input_matrix(list_of_input_matrices, num_spatial_dim):
+    """Finds relevant input matrix (with desired number of spatial dimensions).
+
+    :param list_of_input_matrices: See doc for `run_gradcam`.
+    :param num_spatial_dim: Desired number of spatial dimensions.
+    :return: relevant_index: Array index for relevant input matrix.  If
+        `relevant_index = q`, then list_of_input_matrices[q] is the relevant
+        matrix.
+    :raises: TypeError: if this method does not find exactly one input matrix
+        with desired number of spatial dimensions.
     """
 
-    if 'GuidedBackProp' not in tensorflow_ops._gradient_registry._registry:
-        @tensorflow_ops.RegisterGradient('GuidedBackProp')
-        def _GuidedBackProp(operation, gradient_tensor):
-            input_type = operation.inputs[0].dtype
+    num_spatial_dim_by_input = numpy.array(
+        [len(m.shape) - 2 for m in list_of_input_matrices], dtype=int)
 
-            return (
-                gradient_tensor *
-                tensorflow.cast(gradient_tensor > 0., input_type) *
-                tensorflow.cast(operation.inputs[0] > 0., input_type)
-            )
+    these_indices = numpy.where(num_spatial_dim_by_input == num_spatial_dim)[0]
+    if len(these_indices) != 1:
+        error_string = (
+            'Expected one input matrix with {0:d} dimensions.  Found {1:d} such'
+            ' input matrices.'
+        ).format(num_spatial_dim, len(these_indices))
 
+        raise TypeError(error_string)
 
-def _normalize_tensor(input_tensor):
-    """Normalizes tensor by its L2 norm.
-
-    :param input_tensor: Unnormalized tensor.
-    :return: output_tensor: Normalized tensor.
-    """
-
-    rms_tensor = K.sqrt(K.mean(K.square(input_tensor)))
-    return input_tensor / (rms_tensor + K.epsilon())
+    return these_indices[0]
 
 
 def _compute_gradients(loss_tensor, list_of_input_tensors):
@@ -70,14 +71,98 @@ def _compute_gradients(loss_tensor, list_of_input_tensors):
     return list_of_gradient_tensors
 
 
-def _change_backprop_function(model_object,
-                              backprop_function_name='GuidedBackProp'):
+def _normalize_tensor(input_tensor):
+    """Normalizes tensor by its L2 norm.
+
+    :param input_tensor: Unnormalized tensor.
+    :return: output_tensor: Normalized tensor.
+    """
+
+    rms_tensor = K.sqrt(K.mean(K.square(input_tensor)))
+    return input_tensor / (rms_tensor + K.epsilon())
+
+
+def _upsample_cam(class_activation_matrix, new_dimensions):
+    """Upsamples class-activation matrix (CAM).
+
+    CAM may be 1-D, 2-D, or 3-D.
+
+    :param class_activation_matrix: numpy array containing 1-D, 2-D, or 3-D
+        class-activation matrix.
+    :param new_dimensions: numpy array of new dimensions.  If matrix is
+        {1D, 2D, 3D}, this must be a length-{1, 2, 3} array, respectively.
+    :return: class_activation_matrix: Upsampled version of input.
+    """
+
+    num_rows_new = new_dimensions[0]
+    row_indices_new = numpy.linspace(
+        1, num_rows_new, num=num_rows_new, dtype=float)
+    row_indices_orig = numpy.linspace(
+        1, num_rows_new, num=class_activation_matrix.shape[0], dtype=float)
+
+    if len(new_dimensions) == 1:
+        interp_object = UnivariateSpline(
+            x=row_indices_orig, y=numpy.ravel(class_activation_matrix),
+            k=1, s=0)
+
+        return interp_object(row_indices_new)
+
+    num_columns_new = new_dimensions[1]
+    column_indices_new = numpy.linspace(
+        1, num_columns_new, num=num_columns_new, dtype=float)
+    column_indices_orig = numpy.linspace(
+        1, num_columns_new, num=class_activation_matrix.shape[1],
+        dtype=float)
+
+    if len(new_dimensions) == 2:
+        interp_object = RectBivariateSpline(
+            x=row_indices_orig, y=column_indices_orig,
+            z=class_activation_matrix, kx=1, ky=1, s=0)
+
+        return interp_object(x=row_indices_new, y=column_indices_new, grid=True)
+
+    num_heights_new = new_dimensions[2]
+    height_indices_new = numpy.linspace(
+        1, num_heights_new, num=num_heights_new, dtype=float)
+    height_indices_orig = numpy.linspace(
+        1, num_heights_new, num=class_activation_matrix.shape[2],
+        dtype=float)
+
+    interp_object = RegularGridInterpolator(
+        points=(row_indices_orig, column_indices_orig, height_indices_orig),
+        values=class_activation_matrix, method='linear')
+
+    row_index_matrix, column_index_matrix, height_index_matrix = (
+        numpy.meshgrid(row_indices_new, column_indices_new, height_indices_new)
+    )
+    query_point_matrix = numpy.stack(
+        (row_index_matrix, column_index_matrix, height_index_matrix), axis=-1)
+
+    return interp_object(query_point_matrix)
+
+
+def _register_guided_backprop():
+    """Registers guided-backprop method with TensorFlow backend."""
+
+    if (BACKPROP_FUNCTION_NAME not in
+            tensorflow_ops._gradient_registry._registry):
+
+        @tensorflow_ops.RegisterGradient(BACKPROP_FUNCTION_NAME)
+        def _GuidedBackProp(operation, gradient_tensor):
+            input_type = operation.inputs[0].dtype
+
+            return (
+                gradient_tensor *
+                tensorflow.cast(gradient_tensor > 0., input_type) *
+                tensorflow.cast(operation.inputs[0] > 0., input_type)
+            )
+
+
+def _change_backprop_function(model_object):
     """Changes backpropagation function for Keras model.
 
     :param model_object: Instance of `keras.models.Model` or
         `keras.models.Sequential`.
-    :param backprop_function_name: Name of backprop function that will replace
-        the default.
     :return: new_model_object: Same as `model_object` but with new backprop
         function.
     """
@@ -85,10 +170,10 @@ def _change_backprop_function(model_object,
     # TODO(thunderhoser): I know that "Relu" is a valid operation name, but I
     # have no clue about the last three.
     orig_to_new_operation_dict = {
-        'Relu': backprop_function_name,
-        'LeakyRelu': backprop_function_name,
-        'Elu': backprop_function_name,
-        'Selu': backprop_function_name
+        'Relu': BACKPROP_FUNCTION_NAME,
+        'LeakyRelu': BACKPROP_FUNCTION_NAME,
+        'Elu': BACKPROP_FUNCTION_NAME,
+        'Selu': BACKPROP_FUNCTION_NAME
     }
 
     graph_object = tensorflow.get_default_graph()
@@ -111,75 +196,84 @@ def _make_saliency_function(model_object, layer_name):
     :return: saliency_function: Instance of `keras.backend.function`.
     """
 
-    layer_dict = dict([
-        (lyr.name, lyr) for lyr in model_object.layers[1:]
-    ])
+    output_tensor = model_object.get_layer(name=layer_name).output
+    filter_maxxed_output_tensor = K.max(output_tensor, axis=-1)
 
-    # TODO(thunderhoser): Do I need list of input tensors?
-    output_tensor = layer_dict[layer_name].output
-    max_output_tensor = K.max(output_tensor, axis=-1)
-    saliency_tensor = K.gradients(
-        K.sum(max_output_tensor), model_object.input
-    )[0]
+    if isinstance(model_object.input, list):
+        list_of_input_tensors = model_object.input
+    else:
+        list_of_input_tensors = [model_object.input]
+
+    list_of_saliency_tensors = K.gradients(
+        K.sum(filter_maxxed_output_tensor), list_of_input_tensors)
 
     return K.function(
-        [model_object.input, K.learning_phase()], [saliency_tensor]
+        list_of_input_tensors + [K.learning_phase()],
+        list_of_saliency_tensors
     )
 
 
-def _normalize_guided_gradcam_output(image_matrix):
+def _normalize_guided_gradcam_output(gradient_matrix):
     """Normalizes image produced by guided Grad-CAM.
 
-    M = number of rows in grid
-    N = number of columns in grid
-    C = number of channels
-
-    :param image_matrix: numpy array with output of guided Grad-CAM.  Dimensions
-        may be 1 x M x N x C or M x N x C.
-    :return: image_matrix: Same as input, except that values are normalized and
-        dimensions are always M x N x C.
+    :param gradient_matrix: numpy array with output of guided Grad-CAM.
+    :return: gradient_matrix: Normalized version of input.  If the first axis
+        had length 1, it has been removed ("squeezed out").
     """
 
-    if image_matrix.shape[0] == 1:
-        image_matrix = image_matrix[0, ...]
+    if gradient_matrix.shape[0] == 1:
+        gradient_matrix = gradient_matrix[0, ...]
 
     # Standardize.
-    image_matrix -= numpy.mean(image_matrix)
-    image_matrix /= (numpy.std(image_matrix, ddof=0) + K.epsilon())
+    gradient_matrix -= numpy.mean(gradient_matrix)
+    gradient_matrix /= (numpy.std(gradient_matrix, ddof=0) + K.epsilon())
 
     # Force standard deviation of 0.1 and mean of 0.5.
-    image_matrix = 0.5 + image_matrix * 0.1
-    image_matrix[image_matrix < 0.] = 0.
-    image_matrix[image_matrix > 1.] = 1.
+    gradient_matrix = 0.5 + gradient_matrix * 0.1
+    gradient_matrix[gradient_matrix < 0.] = 0.
+    gradient_matrix[gradient_matrix > 1.] = 1.
 
-    return image_matrix
+    return gradient_matrix
 
 
-def run_gradcam(model_object, input_matrix, target_class, target_layer_name):
+def run_gradcam(model_object, list_of_input_matrices, target_class,
+                target_layer_name):
     """Runs Grad-CAM.
 
-    M = number of rows in grid
-    N = number of columns in grid
+    T = number of input tensors to the model
 
     :param model_object: Trained instance of `keras.models.Model` or
-        `keras.models.Sequential`.  Grad-CAM will be run for this model.
-    :param input_matrix: numpy array containing one input example.  Array
-        dimensions must be the same as input dimensions for `model_object`.
-    :param target_class: Target class (integer from 0...[K - 1], where K =
-        number of classes).  The class-activation map (CAM) will be created for
-        this class.
+        `keras.models.Sequential`.
+    :param list_of_input_matrices: length-T list of numpy arrays, containing
+        only one example (storm object).  list_of_input_matrices[i] must have
+        the same dimensions as the [i]th input tensor to the model.
+    :param target_class: Activation maps will be created for this class.  Must
+        be an integer in 0...(K - 1), where K = number of classes.
     :param target_layer_name: Name of target layer.  Neuron-importance weights
         will be based on activations in this layer.
-    :return: class_activation_matrix: M-by-N numpy array of class activations.
+    :return: class_activation_matrix: Class-activation matrix.  Dimensions of
+        this numpy array will be the spatial dimensions of whichever input
+        tensor feeds into the target layer.  For example, if the given input
+        tensor is 2-dimensional with M rows and N columns, this array will be
+        M x N.
     """
 
-    # TODO(thunderhoser): Check input dimensions.
+    # Check input args.
+    error_checking.assert_is_string(target_layer_name)
+    for q in range(len(list_of_input_matrices)):
+        error_checking.assert_is_numpy_array(list_of_input_matrices[q])
 
-    num_output_neurons = model_object.layers[-1].output.get_shape().as_list()[
-        -1]
+        if list_of_input_matrices[q].shape[0] != 1:
+            list_of_input_matrices[q] = numpy.expand_dims(
+                list_of_input_matrices[q], axis=0)
+
+    # Create loss tensor.
+    output_layer_object = model_object.layers[-1].output
+    num_output_neurons = output_layer_object.get_shape().as_list()[-1]
 
     if num_output_neurons == 1:
         error_checking.assert_is_leq(target_class, 1)
+
         if target_class == 1:
             loss_tensor = model_object.layers[-1].output[..., 0]
         else:
@@ -188,6 +282,7 @@ def run_gradcam(model_object, input_matrix, target_class, target_layer_name):
         error_checking.assert_is_less_than(target_class, num_output_neurons)
         loss_tensor = model_object.layers[-1].output[..., target_class]
 
+    # Create gradient function.
     target_layer_activation_tensor = model_object.get_layer(
         name=target_layer_name
     ).output
@@ -197,18 +292,25 @@ def run_gradcam(model_object, input_matrix, target_class, target_layer_name):
     )[0]
     gradient_tensor = _normalize_tensor(gradient_tensor)
 
+    if isinstance(model_object.input, list):
+        list_of_input_tensors = model_object.input
+    else:
+        list_of_input_tensors = [model_object.input]
+
     gradient_function = K.function(
-        [model_object.input], [target_layer_activation_tensor, gradient_tensor]
+        list_of_input_tensors, [target_layer_activation_tensor, gradient_tensor]
     )
 
+    # Evaluate gradient function.
     target_layer_activation_matrix, gradient_matrix = gradient_function(
-        [input_matrix])
+        list_of_input_matrices)
     target_layer_activation_matrix = target_layer_activation_matrix[0, ...]
     gradient_matrix = gradient_matrix[0, ...]
 
+    # Compute class-activation matrix.
     mean_weight_by_filter = numpy.mean(gradient_matrix, axis=(0, 1))
     class_activation_matrix = numpy.ones(
-        target_layer_activation_matrix.shape[:2])
+        target_layer_activation_matrix.shape[:-1])
 
     num_filters = len(mean_weight_by_filter)
     for m in range(num_filters):
@@ -216,17 +318,23 @@ def run_gradcam(model_object, input_matrix, target_class, target_layer_name):
             mean_weight_by_filter[m] * target_layer_activation_matrix[..., m]
         )
 
-    num_input_rows = input_matrix.shape[1]
-    num_input_columns = input_matrix.shape[2]
-    class_activation_matrix = cv2_resize(
-        class_activation_matrix, (num_input_rows, num_input_columns)
+    input_index = _find_relevant_input_matrix(
+        list_of_input_matrices=list_of_input_matrices,
+        num_spatial_dim=len(class_activation_matrix.shape)
     )
 
+    spatial_dimensions = numpy.array(
+        list_of_input_matrices[input_index].shape[1:-1], dtype=int)
+    class_activation_matrix = _upsample_cam(
+        class_activation_matrix=class_activation_matrix,
+        new_dimensions=spatial_dimensions)
+
     class_activation_matrix[class_activation_matrix < 0.] = 0.
-    return class_activation_matrix / numpy.max(class_activation_matrix)
+    denominator = numpy.maximum(numpy.max(class_activation_matrix), K.epsilon())
+    return class_activation_matrix / denominator
 
 
-def run_guided_gradcam(model_object, input_matrix, target_layer_name,
+def run_guided_gradcam(model_object, list_of_input_matrices, target_layer_name,
                        class_activation_matrix):
     """Runs guided Grad-CAM.
 
@@ -235,19 +343,27 @@ def run_guided_gradcam(model_object, input_matrix, target_layer_name,
     C = number of channels
 
     :param model_object: See doc for `run_gradcam`.
-    :param input_matrix: Same.
+    :param list_of_input_matrices: Same.
     :param target_layer_name: Same.
     :param class_activation_matrix: Matrix created by `run_gradcam`.
     :return: gradient_matrix: M-by-N-by-C numpy array of gradients.
     """
 
-    _register_gradient()
+    _register_guided_backprop()
 
     new_model_object = _change_backprop_function(model_object=model_object)
     saliency_function = _make_saliency_function(
         model_object=new_model_object, layer_name=target_layer_name)
 
-    saliency_matrix = saliency_function([input_matrix, 0])[0]
+    input_index = _find_relevant_input_matrix(
+        list_of_input_matrices=list_of_input_matrices,
+        num_spatial_dim=len(class_activation_matrix.shape)
+    )
+
+    saliency_matrix = saliency_function(
+        list_of_input_matrices + [0]
+    )[input_index]
+
     gradient_matrix = saliency_matrix * class_activation_matrix[
         ..., numpy.newaxis]
     return _normalize_guided_gradcam_output(gradient_matrix)
---
+++ gewittergefahr/deep_learning/gradcam_test.py
@@ -0,0 +1,154 @@
+"""Unit tests for gradcam.py."""
+
+import unittest
+import numpy
+from keras import backend as K
+from gewittergefahr.deep_learning import gradcam
+
+TOLERANCE = 1e-6
+
+# The following constants are used to test _find_relevant_input_matrix.
+REFLECTIVITY_MATRIX_DBZ = numpy.random.uniform(
+    low=0., high=1., size=(1, 32, 32, 12, 1)
+)
+AZIMUTHAL_SHEAR_MATRIX_S01 = numpy.random.uniform(
+    low=0., high=1., size=(1, 64, 64, 2)
+)
+SOUNDING_MATRIX = numpy.random.uniform(low=0., high=1., size=(1, 49, 5))
+
+LIST_OF_INPUT_MATRICES = [
+    REFLECTIVITY_MATRIX_DBZ, AZIMUTHAL_SHEAR_MATRIX_S01, SOUNDING_MATRIX
+]
+
+# The following constants are used to test _upsample_cam.
+CLASS_ACTIV_MATRIX_1D = numpy.linspace(1, 49, num=49, dtype=float)
+CLASS_ACTIV_MATRIX_COARSE_1D = CLASS_ACTIV_MATRIX_1D[::2]
+
+THESE_ROW_INDICES = numpy.linspace(1, 64, num=64, dtype=float)
+THESE_COLUMN_INDICES = numpy.linspace(1, 64, num=64, dtype=float)
+THIS_ROW_INDEX_MATRIX, THIS_COLUMN_INDEX_MATRIX = numpy.meshgrid(
+    THESE_ROW_INDICES, THESE_COLUMN_INDICES)
+
+CLASS_ACTIV_MATRIX_2D = THIS_ROW_INDEX_MATRIX + THIS_COLUMN_INDEX_MATRIX
+CLASS_ACTIV_MATRIX_COARSE_2D = CLASS_ACTIV_MATRIX_2D[::3, ::3]
+
+THESE_ROW_INDICES = numpy.linspace(1, 33, num=33, dtype=float)
+THESE_COLUMN_INDICES = numpy.linspace(1, 33, num=33, dtype=float)
+THESE_HEIGHT_INDICES = numpy.linspace(1, 13, num=13, dtype=float)
+THIS_ROW_INDEX_MATRIX, THIS_COLUMN_INDEX_MATRIX, THIS_HEIGHT_INDEX_MATRIX = (
+    numpy.meshgrid(THESE_ROW_INDICES, THESE_COLUMN_INDICES,
+                   THESE_HEIGHT_INDICES)
+)
+
+CLASS_ACTIV_MATRIX_3D = (
+    THIS_ROW_INDEX_MATRIX + THIS_COLUMN_INDEX_MATRIX + THIS_HEIGHT_INDEX_MATRIX
+)
+CLASS_ACTIV_MATRIX_COARSE_3D = CLASS_ACTIV_MATRIX_3D[::4, ::4, ::4]
+
+# The following constants are used to test _normalize_guided_gradcam_output.
+GRADIENT_MATRIX_DENORM = numpy.array([[0, 2, 4, 2, 0],
+                                      [1, 4, 7, 4, 1],
+                                      [-5, -5, -5, -5, -5]], dtype=float)
+
+GRADIENT_MATRIX_DENORM = numpy.expand_dims(GRADIENT_MATRIX_DENORM, axis=0)
+
+THIS_STANDARD_DEVIATION = (
+    numpy.sqrt(numpy.mean(GRADIENT_MATRIX_DENORM ** 2)) + K.epsilon()
+)
+
+GRADIENT_MATRIX_NORM = GRADIENT_MATRIX_DENORM / THIS_STANDARD_DEVIATION
+GRADIENT_MATRIX_NORM = 0.5 + GRADIENT_MATRIX_NORM * 0.1
+GRADIENT_MATRIX_NORM[GRADIENT_MATRIX_NORM < 0.] = 0.
+GRADIENT_MATRIX_NORM[GRADIENT_MATRIX_NORM > 1.] = 1.
+
+
+class GradcamTests(unittest.TestCase):
+    """Each method is a unit test for gradcam.py."""
+
+    def test_find_relevant_input_matrix_refl(self):
+        """Ensures correct output from _find_relevant_input_matrix.
+
+        In this case, relevant matrix contains reflectivity.
+        """
+
+        this_index = gradcam._find_relevant_input_matrix(
+            list_of_input_matrices=LIST_OF_INPUT_MATRICES, num_spatial_dim=3)
+
+        self.assertTrue(this_index == 0)
+
+    def test_find_relevant_input_matrix_az_shear(self):
+        """Ensures correct output from _find_relevant_input_matrix.
+
+        In this case, relevant matrix contains azimuthal shear.
+        """
+
+        this_index = gradcam._find_relevant_input_matrix(
+            list_of_input_matrices=LIST_OF_INPUT_MATRICES, num_spatial_dim=2)
+
+        self.assertTrue(this_index == 1)
+
+    def test_find_relevant_input_matrix_soundings(self):
+        """Ensures correct output from _find_relevant_input_matrix.
+
+        In this case, relevant matrix contains soundings.
+        """
+
+        this_index = gradcam._find_relevant_input_matrix(
+            list_of_input_matrices=LIST_OF_INPUT_MATRICES, num_spatial_dim=1)
+
+        self.assertTrue(this_index == 2)
+
+    def test_upsample_cam_1d(self):
+        """Ensures correct output from _upsample_cam.
+
+        In this case the CAM (class-activation map) is 1-D.
+        """
+
+        this_matrix = gradcam._upsample_cam(
+            class_activation_matrix=CLASS_ACTIV_MATRIX_COARSE_1D + 0.,
+            new_dimensions=numpy.array(CLASS_ACTIV_MATRIX_1D.shape, dtype=int)
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, CLASS_ACTIV_MATRIX_1D, atol=TOLERANCE))
+
+    def test_upsample_cam_2d(self):
+        """Ensures correct output from _upsample_cam.
+
+        In this case the CAM (class-activation map) is 2-D.
+        """
+
+        this_matrix = gradcam._upsample_cam(
+            class_activation_matrix=CLASS_ACTIV_MATRIX_COARSE_2D + 0.,
+            new_dimensions=numpy.array(CLASS_ACTIV_MATRIX_2D.shape, dtype=int)
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, CLASS_ACTIV_MATRIX_2D, atol=TOLERANCE))
+
+    def test_upsample_cam_3d(self):
+        """Ensures correct output from _upsample_cam.
+
+        In this case the CAM (class-activation map) is 3-D.
+        """
+
+        this_matrix = gradcam._upsample_cam(
+            class_activation_matrix=CLASS_ACTIV_MATRIX_COARSE_3D + 0.,
+            new_dimensions=numpy.array(CLASS_ACTIV_MATRIX_3D.shape, dtype=int)
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, CLASS_ACTIV_MATRIX_3D, atol=TOLERANCE))
+
+    def test_normalize_guided_gradcam_output(self):
+        """Ensures correct output from _normalize_guided_gradcam_output."""
+
+        this_matrix = gradcam._normalize_guided_gradcam_output(
+            GRADIENT_MATRIX_DENORM + 0.)
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, GRADIENT_MATRIX_NORM, atol=TOLERANCE))
+
+
+if __name__ == '__main__':
+    unittest.main()
