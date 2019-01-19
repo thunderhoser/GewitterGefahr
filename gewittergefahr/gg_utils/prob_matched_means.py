539f0885aa129fda6d418bd8765e1cc781b42c1c
thunderhoser <lagerqui@ualberta.ca>
2019-01-19 01:55:59 +0000

    Added prob_matched_means.py.

---
+++ gewittergefahr/gg_utils/prob_matched_means.py
@@ -0,0 +1,196 @@
+"""Probability-matched means (PMM).
+
+--- REFERENCE ---
+
+Ebert, E.E., 2001: "Ability of a poor man's ensemble to predict the probability
+and distribution of precipitation". Monthly Weather Review, 129 (10), 2461-2480,
+https://doi.org/10.1175/1520-0493(2001)129%3C2461:AOAPMS%3E2.0.CO;2.
+"""
+
+import numpy
+from scipy.interpolate import interp1d
+from gewittergefahr.gg_utils import error_checking
+
+DEFAULT_MAX_PERCENTILE_LEVEL = 99.
+
+MINIMUM_STRING = 'min'
+MAXIMUM_STRING = 'max'
+VALID_THRESHOLD_TYPE_STRINGS = [MINIMUM_STRING, MAXIMUM_STRING]
+
+
+def _check_threshold_type(threshold_type_string):
+    """Error-checks threshold type.
+
+    :param threshold_type_string: See doc for `run_pmm`.
+    :raises: ValueError: if
+        `threshold_type_string not in VALID_THRESHOLD_TYPE_STRINGS`.
+    """
+
+    error_checking.assert_is_string(threshold_type_string)
+
+    if threshold_type_string not in VALID_THRESHOLD_TYPE_STRINGS:
+        error_string = (
+            '\n{0:s}\nValid threshold types (listed above) do not include '
+            '"{1:s}".'
+        ).format(str(VALID_THRESHOLD_TYPE_STRINGS), threshold_type_string)
+
+        raise ValueError(error_string)
+
+
+def run_pmm_one_variable(
+        input_matrix, max_percentile_level=DEFAULT_MAX_PERCENTILE_LEVEL,
+        threshold_value=None, threshold_type_string=None):
+    """Applies PMM to one variable.
+
+    E = number of examples (realizations over which to average)
+
+    :param input_matrix: numpy array.  The first axis must have length E.  Other
+        axes are assumed to be spatial dimensions.  Thus, input_matrix[i, ...]
+        is the spatial field for the [i]th example.
+    :param max_percentile_level: Maximum percentile.  No output value will
+        exceed the [q]th percentile of `input_matrix`, where q =
+        `max_percentile_level`.  Similarly, no output value will be less than
+        the [100 - q]th percentile of `input_matrix`.
+    :param threshold_value: At each grid point, this method will count the
+        number of examples that meet this threshold.  If you do not care about
+        these counts, leave this argument as None.
+    :param threshold_type_string: Threshold type (must be accepted by
+        `_check_threshold_type`).  If type is "min", then at each grid point,
+        this method will count the number of examples with value >=
+        `threshold_value`.
+    :return: mean_field_matrix: numpy array of probability-matched means.  Will
+        have the same dimensions as `input_matrix`, except without the first
+        axis.  For example, if `input_matrix` is E x 32 x 32 x 12, this will be
+        32 x 32 x 12.
+    :return: threshold_count_matrix: numpy array of threshold counts.  Will have
+        the same dimensions as `input_matrix`, except without the first axis.
+        If no thresholding was done, this will be None.
+    """
+
+    # Check input args.
+    error_checking.assert_is_numpy_array_without_nan(input_matrix)
+    num_spatial_dimensions = len(input_matrix.shape) - 1
+    error_checking.assert_is_geq(num_spatial_dimensions, 1)
+
+    error_checking.assert_is_greater(max_percentile_level, 50.)
+    error_checking.assert_is_leq(max_percentile_level, 100.)
+
+    use_threshold = not (
+        threshold_value is None and threshold_type_string is None
+    )
+
+    if use_threshold:
+        _check_threshold_type(threshold_type_string)
+        error_checking.assert_is_not_nan(threshold_value)
+
+        if threshold_type_string == MINIMUM_STRING:
+            threshold_count_matrix = numpy.sum(
+                input_matrix >= threshold_value, axis=0)
+        else:
+            threshold_count_matrix = numpy.sum(
+                input_matrix <= threshold_value, axis=0)
+    else:
+        threshold_count_matrix = None
+
+    # Pool values over all dimensions and remove extremes.
+    pooled_values = numpy.ravel(input_matrix)
+    pooled_values = numpy.sort(pooled_values)
+
+    max_pooled_value = numpy.percentile(pooled_values, max_percentile_level)
+    pooled_values = pooled_values[pooled_values <= max_pooled_value]
+
+    min_pooled_value = numpy.percentile(
+        pooled_values, 100 - max_percentile_level)
+    pooled_values = pooled_values[pooled_values >= min_pooled_value]
+
+    # Find ensemble mean at each grid point.
+    mean_field_matrix = numpy.mean(input_matrix, axis=0)
+    mean_field_flattened = numpy.ravel(mean_field_matrix)
+
+    # At each grid point, replace ensemble mean with the same percentile from
+    # pooled array.
+    pooled_value_percentiles = numpy.linspace(
+        0, 100, num=len(pooled_values), dtype=float)
+    mean_value_percentiles = numpy.linspace(
+        0, 100, num=len(mean_field_flattened), dtype=float)
+
+    sort_indices = numpy.argsort(mean_field_flattened)
+    mean_value_percentiles = mean_value_percentiles[sort_indices]
+
+    interp_object = interp1d(
+        pooled_value_percentiles, pooled_values, kind='linear',
+        bounds_error=True, assume_sorted=True)
+
+    mean_field_flattened = interp_object(mean_value_percentiles)
+    mean_field_matrix = numpy.reshape(
+        mean_field_flattened, mean_field_matrix.shape)
+
+    return mean_field_matrix, threshold_count_matrix
+
+
+def run_pmm_many_variables(
+        input_matrix, max_percentile_level=DEFAULT_MAX_PERCENTILE_LEVEL,
+        threshold_var_index=None, threshold_value=None,
+        threshold_type_string=None):
+    """Applies PMM to each variable separately.
+
+    E = number of examples (realizations over which to average)
+    V = number of variables
+
+    :param input_matrix: numpy array.  The first axis must have length E, and the
+        last axis must have length V.  Other axes are assumed to be spatial
+        dimensions.  Thus, input_matrix[i, ..., j] is the spatial field for the
+        [j]th variable and [i]th example.
+    :param max_percentile_level: See doc for `run_pmm_one_variable`.
+    :param threshold_var_index: Determines variable to which threshold will be
+        applied.  If `threshold_var_index = j`, threshold will be applied to
+        [j]th variable.  See `run_pmm_one_variable` for more about thresholds.
+    :param threshold_value: See doc for `run_pmm_one_variable`.
+    :param threshold_type_string: Same.
+    :return: mean_field_matrix: numpy array of probability-matched means.  Will
+        have the same dimensions as `input_matrix`, except without the first
+        axis.  For example, if `input_matrix` is E x 32 x 32 x 12 x V, this will
+        be 32 x 32 x 12 x V.
+    :return: threshold_count_matrix: numpy array of threshold counts.  Will have
+        the same dimensions as `input_matrix`, except without the first and last
+        axes.  If no thresholding was done, this will be None.
+    """
+
+    error_checking.assert_is_numpy_array_without_nan(input_matrix)
+    num_spatial_dimensions = len(input_matrix.shape) - 2
+    error_checking.assert_is_geq(num_spatial_dimensions, 1)
+
+    num_variables = input_matrix.shape[-1]
+
+    use_threshold = not (
+        threshold_var_index is None
+        and threshold_value is None
+        and threshold_type_string is None
+    )
+
+    if use_threshold:
+        error_checking.assert_is_integer(threshold_var_index)
+        error_checking.assert_is_geq(threshold_var_index, 0)
+        error_checking.assert_is_less_than(threshold_var_index, num_variables)
+    else:
+        threshold_var_index = -1
+
+    mean_field_matrix = numpy.full(input_matrix.shape[1:], numpy.nan)
+    threshold_count_matrix = None
+
+    for j in range(num_variables):
+        if threshold_var_index == j:
+            mean_field_matrix[..., j], threshold_count_matrix = (
+                run_pmm_one_variable(
+                    input_matrix=input_matrix[..., j],
+                    max_percentile_level=max_percentile_level,
+                    threshold_value=threshold_value,
+                    threshold_type_string=threshold_type_string)
+            )
+        else:
+            mean_field_matrix[..., j] = run_pmm_one_variable(
+                input_matrix=input_matrix[..., j],
+                max_percentile_level=max_percentile_level
+            )[0]
+
+    return mean_field_matrix, threshold_count_matrix
---
+++ gewittergefahr/gg_utils/prob_matched_means_test.py
@@ -0,0 +1,176 @@
+"""Unit tests for prob_matched_means.py."""
+
+import unittest
+import numpy
+from gewittergefahr.gg_utils import prob_matched_means as pmm
+
+TOLERANCE = 1e-6
+
+MAX_PERCENTILE_LEVEL = 99.
+THRESHOLD_VALUE = 50.
+THRESHOLD_VAR_INDEX = 0
+
+# The following constants are used to test run_pmm_one_variable.
+THIS_EXAMPLE_MATRIX = numpy.array([
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+], dtype=float)
+
+INPUT_MATRIX_ONE_VAR = numpy.stack(
+    (THIS_EXAMPLE_MATRIX, 2 * THIS_EXAMPLE_MATRIX, 3 * THIS_EXAMPLE_MATRIX,
+     4 * THIS_EXAMPLE_MATRIX),
+    axis=0)
+
+MEAN_FIELD_MATRIX_ONE_VAR = numpy.array([
+    [2, 4, 6, 8, 9, 12, 13, 15, 17, 19, 21, 23, 24],
+    [27, 30, 34, 38, 42, 45, 48, 52, 60, 66, 72, 80, 96]
+], dtype=float)
+
+MIN_THRESHOLD_COUNT_MATRIX = numpy.array([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3]
+], dtype=float)
+
+MAX_THRESHOLD_COUNT_MATRIX = numpy.array([
+    [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3],
+    [3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+], dtype=float)
+
+# The following constants are used to test run_pmm_many_variables.
+INPUT_MATRIX_MANY_VARS = numpy.stack(
+    (INPUT_MATRIX_ONE_VAR, 2 * INPUT_MATRIX_ONE_VAR, 3 * INPUT_MATRIX_ONE_VAR),
+    axis=-1)
+
+MEAN_FIELD_MATRIX_MANY_VARS = numpy.stack(
+    (MEAN_FIELD_MATRIX_ONE_VAR, 2 * MEAN_FIELD_MATRIX_ONE_VAR,
+     3 * MEAN_FIELD_MATRIX_ONE_VAR),
+    axis=-1)
+
+
+class ProbMatchedMeansTests(unittest.TestCase):
+    """Each method is a unit test for prob_matched_means.py."""
+
+    def test_run_pmm_one_variable_no_threshold(self):
+        """Ensures correct output from run_pmm_one_variable.
+
+        In this case there is no thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_one_variable(
+                input_matrix=INPUT_MATRIX_ONE_VAR,
+                max_percentile_level=MAX_PERCENTILE_LEVEL)
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_ONE_VAR, atol=TOLERANCE))
+        self.assertTrue(this_threshold_count_matrix is None)
+
+    def test_run_pmm_one_variable_min_threshold(self):
+        """Ensures correct output from run_pmm_one_variable.
+
+        In this case there is minimum-thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_one_variable(
+                input_matrix=INPUT_MATRIX_ONE_VAR,
+                max_percentile_level=MAX_PERCENTILE_LEVEL,
+                threshold_value=THRESHOLD_VALUE,
+                threshold_type_string=pmm.MINIMUM_STRING
+            )
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_ONE_VAR, atol=TOLERANCE))
+
+        self.assertTrue(numpy.array_equal(
+            this_threshold_count_matrix, MIN_THRESHOLD_COUNT_MATRIX))
+
+    def test_run_pmm_one_variable_max_threshold(self):
+        """Ensures correct output from run_pmm_one_variable.
+
+        In this case there is maximum-thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_one_variable(
+                input_matrix=INPUT_MATRIX_ONE_VAR,
+                max_percentile_level=MAX_PERCENTILE_LEVEL,
+                threshold_value=THRESHOLD_VALUE,
+                threshold_type_string=pmm.MAXIMUM_STRING
+            )
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_ONE_VAR, atol=TOLERANCE))
+
+        self.assertTrue(numpy.array_equal(
+            this_threshold_count_matrix, MAX_THRESHOLD_COUNT_MATRIX))
+
+    def test_run_pmm_many_variables_no_threshold(self):
+        """Ensures correct output from run_pmm_many_variables.
+
+        In this case there is no thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_many_variables(
+                input_matrix=INPUT_MATRIX_MANY_VARS,
+                max_percentile_level=MAX_PERCENTILE_LEVEL)
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_MANY_VARS, atol=TOLERANCE
+        ))
+        self.assertTrue(this_threshold_count_matrix is None)
+
+    def test_run_pmm_many_variables_min_threshold(self):
+        """Ensures correct output from run_pmm_many_variables.
+
+        In this case there is minimum-thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_many_variables(
+                input_matrix=INPUT_MATRIX_MANY_VARS,
+                max_percentile_level=MAX_PERCENTILE_LEVEL,
+                threshold_var_index=THRESHOLD_VAR_INDEX,
+                threshold_value=THRESHOLD_VALUE,
+                threshold_type_string=pmm.MINIMUM_STRING
+            )
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_MANY_VARS, atol=TOLERANCE
+        ))
+
+        self.assertTrue(numpy.array_equal(
+            this_threshold_count_matrix, MIN_THRESHOLD_COUNT_MATRIX))
+
+    def test_run_pmm_many_variables_max_threshold(self):
+        """Ensures correct output from run_pmm_many_variables.
+
+        In this case there is maximum-thresholding.
+        """
+
+        this_mean_field_matrix, this_threshold_count_matrix = (
+            pmm.run_pmm_many_variables(
+                input_matrix=INPUT_MATRIX_MANY_VARS,
+                max_percentile_level=MAX_PERCENTILE_LEVEL,
+                threshold_var_index=THRESHOLD_VAR_INDEX,
+                threshold_value=THRESHOLD_VALUE,
+                threshold_type_string=pmm.MAXIMUM_STRING
+            )
+        )
+
+        self.assertTrue(numpy.allclose(
+            this_mean_field_matrix, MEAN_FIELD_MATRIX_MANY_VARS, atol=TOLERANCE
+        ))
+
+        self.assertTrue(numpy.array_equal(
+            this_threshold_count_matrix, MAX_THRESHOLD_COUNT_MATRIX))
+
+
+if __name__ == '__main__':
+    unittest.main()
