97b94b52827dd5b6f55fe9bdfc5565b73cb261a9
thunderhoser <ryan.lagerquist@ou.edu>
2019-05-09 16:08:54 -0500

    Added input args to reanalyze_storm_tracks.py.

--- gewittergefahr/scripts/reanalyze_storm_tracks.py
+++ gewittergefahr/scripts/reanalyze_storm_tracks.py
@@ -13,6 +13,8 @@ FIRST_SPC_DATE_ARG_NAME = 'first_spc_date_string'
 LAST_SPC_DATE_ARG_NAME = 'last_spc_date_string'
 FIRST_TIME_ARG_NAME = 'first_time_string'
 LAST_TIME_ARG_NAME = 'last_time_string'
+MAX_VELOCITY_DIFF_ARG_NAME = 'max_velocity_diff_m_s01'
+MAX_LINK_DISTANCE_ARG_NAME = 'max_link_distance_m_s01'
 MAX_JOIN_TIME_ARG_NAME = 'max_join_time_seconds'
 MAX_JOIN_ERROR_ARG_NAME = 'max_join_error_m_s01'
 MIN_DURATION_ARG_NAME = 'min_duration_seconds'
@@ -38,13 +40,21 @@ LAST_TIME_HELP_STRING = (
     'Last time in period (format "yyyy-mm-dd-HHMMSS").  If empty, defaults to '
     'last time on last SPC date.')
 
+MAX_VELOCITY_DIFF_HELP_STRING = (
+    'Used only to bridge the gap between two SPC dates.  See '
+    '`echo_top_tracking._link_local_maxima_in_time` for details.')
+
+MAX_LINK_DISTANCE_HELP_STRING = (
+    'Used only to bridge the gap between two SPC dates.  See '
+    '`echo_top_tracking._link_local_maxima_in_time` for details.')
+
 MAX_JOIN_TIME_HELP_STRING = (
-    'Max time difference (between end of early track and start of late track) '
-    'for joining two tracks.')
+    'Max time difference (between end of early track and beginning of late '
+    'track) for joining collinear tracks.')
 
 MAX_JOIN_ERROR_HELP_STRING = (
-    'Max join error (metres per second) for joining tracks, created by '
-    'extrapolating early track to first time in late track.')
+    'Max extrapolation error (between end of early track and beginning of late '
+    'track) for joining collinear tracks.')
 
 MIN_DURATION_HELP_STRING = (
     'Minimum storm duration.  Shorter-lived storms will be thrown out.')
@@ -80,6 +90,16 @@ INPUT_ARG_PARSER.add_argument(
     help=LAST_TIME_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
+    '--' + MAX_VELOCITY_DIFF_ARG_NAME, type=float, required=False,
+    default=echo_top_tracking.DEFAULT_MAX_VELOCITY_DIFF_M_S01,
+    help=MAX_VELOCITY_DIFF_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + MAX_LINK_DISTANCE_ARG_NAME, type=float, required=False,
+    default=echo_top_tracking.DEFAULT_MAX_LINK_DISTANCE_M_S01,
+    help=MAX_LINK_DISTANCE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
     '--' + MAX_JOIN_TIME_ARG_NAME, type=int, required=False,
     default=echo_top_tracking.DEFAULT_MAX_JOIN_TIME_SEC,
     help=MAX_JOIN_TIME_HELP_STRING)
@@ -104,9 +124,9 @@ INPUT_ARG_PARSER.add_argument(
 
 
 def _run(top_input_dir_name, first_spc_date_string, last_spc_date_string,
-         first_time_string, last_time_string, max_join_time_seconds,
-         max_join_error_m_s01, min_duration_seconds, for_myrorss_climo,
-         top_output_dir_name):
+         first_time_string, last_time_string, max_velocity_diff_m_s01,
+         max_link_distance_m_s01, max_join_time_seconds, max_join_error_m_s01,
+         min_duration_seconds, for_myrorss_climo, top_output_dir_name):
     """Reanalyzes storm tracks (preferably over many SPC dates).
 
     This is effectively the main method.
@@ -116,6 +136,8 @@ def _run(top_input_dir_name, first_spc_date_string, last_spc_date_string,
     :param last_spc_date_string: Same.
     :param first_time_string: Same.
     :param last_time_string: Same.
+    :param max_velocity_diff_m_s01: Same.
+    :param max_link_distance_m_s01: Same.
     :param max_join_time_seconds: Same.
     :param max_join_error_m_s01: Same.
     :param min_duration_seconds: Same.
@@ -153,7 +175,8 @@ def _run(top_input_dir_name, first_spc_date_string, last_spc_date_string,
         last_time_unix_sec=last_time_unix_sec,
         tracking_start_time_unix_sec=tracking_start_time_unix_sec,
         tracking_end_time_unix_sec=tracking_end_time_unix_sec,
-        max_link_time_seconds=max_join_time_seconds,
+        max_velocity_diff_m_s01=max_velocity_diff_m_s01,
+        max_link_distance_m_s01=max_link_distance_m_s01,
         max_join_time_seconds=max_join_time_seconds,
         max_join_error_m_s01=max_join_error_m_s01,
         min_track_duration_seconds=min_duration_seconds)
@@ -169,6 +192,10 @@ if __name__ == '__main__':
         last_spc_date_string=getattr(INPUT_ARG_OBJECT, LAST_SPC_DATE_ARG_NAME),
         first_time_string=getattr(INPUT_ARG_OBJECT, FIRST_TIME_ARG_NAME),
         last_time_string=getattr(INPUT_ARG_OBJECT, LAST_TIME_ARG_NAME),
+        max_velocity_diff_m_s01=getattr(
+            INPUT_ARG_OBJECT, MAX_VELOCITY_DIFF_ARG_NAME),
+        max_link_distance_m_s01=getattr(
+            INPUT_ARG_OBJECT, MAX_LINK_DISTANCE_ARG_NAME),
         max_join_time_seconds=getattr(INPUT_ARG_OBJECT, MAX_JOIN_TIME_ARG_NAME),
         max_join_error_m_s01=getattr(INPUT_ARG_OBJECT, MAX_JOIN_ERROR_ARG_NAME),
         min_duration_seconds=getattr(INPUT_ARG_OBJECT, MIN_DURATION_ARG_NAME),
