8db2e4b32a077e98dc03b11338c21d96ca19c07d
thunderhoser <ryan.lagerquist@ou.edu>
2019-05-17 00:43:24 -0500

    Still trying to allow for times with zero storm objects.

--- gewittergefahr/deep_learning/prediction_io.py
+++ gewittergefahr/deep_learning/prediction_io.py
@@ -214,16 +214,24 @@ def read_ungridded_predictions(netcdf_file_name):
 
     prediction_dict = {
         TARGET_NAME_KEY: str(getattr(dataset_object, TARGET_NAME_KEY)),
-        STORM_IDS_KEY: [
-            str(s) for s in
-            netCDF4.chartostring(dataset_object.variables[STORM_IDS_KEY][:])
-        ],
         STORM_TIMES_KEY: numpy.array(
             dataset_object.variables[STORM_TIMES_KEY][:], dtype=int
         ),
         PROBABILITY_MATRIX_KEY:
             dataset_object.variables[PROBABILITY_MATRIX_KEY][:]
     }
+
+    num_examples = len(prediction_dict[STORM_TIMES_KEY])
+
+    if num_examples == 0:
+        prediction_dict.update({STORM_IDS_KEY: []})
+    else:
+        prediction_dict.update({
+            STORM_IDS_KEY: [
+                str(s) for s in
+                netCDF4.chartostring(dataset_object.variables[STORM_IDS_KEY][:])
+            ]
+        })
 
     if OBSERVED_LABELS_KEY in dataset_object.variables:
         prediction_dict.update({
