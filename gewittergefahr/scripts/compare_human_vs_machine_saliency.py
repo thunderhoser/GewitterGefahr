f89a1b4234d7c37915b42458c6f4d9873e9a4871
thunderhoser <ryan.lagerquist@ou.edu>
2019-06-11 23:14:53 -0600

    Added plotting to compare_human_vs_machine_saliency.py.

--- gewittergefahr/scripts/compare_human_vs_machine_saliency.py
+++ gewittergefahr/scripts/compare_human_vs_machine_saliency.py
@@ -3,21 +3,35 @@
 import os.path
 import argparse
 import numpy
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as pyplot
 from gewittergefahr.gg_utils import human_polygons
 from gewittergefahr.gg_utils import radar_utils
 from gewittergefahr.gg_utils import storm_tracking_utils as tracking_utils
+from gewittergefahr.gg_utils import file_system_utils
 from gewittergefahr.gg_utils import error_checking
 from gewittergefahr.deep_learning import cnn
 from gewittergefahr.deep_learning import saliency_maps
+from gewittergefahr.deep_learning import input_examples
 from gewittergefahr.deep_learning import training_validation_io as trainval_io
 from gewittergefahr.scripts import plot_input_examples
+from gewittergefahr.plotting import plotting_utils
+from gewittergefahr.plotting import radar_plotting
+from gewittergefahr.plotting import imagemagick_utils
 
 TOLERANCE = 1e-6
+METRES_TO_KM = 0.001
 TIME_FORMAT = '%Y-%m-%d-%H%M%S'
+
+FIGURE_WIDTH_INCHES = 15
+FIGURE_HEIGHT_INCHES = 15
+FIGURE_RESOLUTION_DPI = 300
 
 HUMAN_FILE_ARG_NAME = 'input_human_file_name'
 MACHINE_FILE_ARG_NAME = 'input_machine_file_name'
 THRESHOLD_ARG_NAME = 'abs_percentile_threshold'
+OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 HUMAN_FILE_HELP_STRING = (
     'Path to file with human-generated polygons.  Will be read by '
@@ -36,6 +50,9 @@ THRESHOLD_HELP_STRING = (
     'percentile of negative values in the machine-generated map.'
 ).format(THRESHOLD_ARG_NAME)
 
+OUTPUT_DIR_HELP_STRING = (
+    'Name of output directory (figures will be saved here).')
+
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + HUMAN_FILE_ARG_NAME, type=str, required=True,
@@ -49,9 +66,102 @@ INPUT_ARG_PARSER.add_argument(
     '--' + THRESHOLD_ARG_NAME, type=float, required=False, default=90.,
     help=THRESHOLD_HELP_STRING)
 
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
+    help=OUTPUT_DIR_HELP_STRING)
+
+
+def _compute_iou(machine_mask_matrix, human_mask_matrix):
+    """Computes IoU (intersection over union) between human and machine masks.
+
+    :param machine_mask_matrix: Boolean numpy array, representing areas of
+        extreme positive or negative saliency.
+    :param human_mask_matrix: Same but for human.  The two numpy arrays must
+        have the same shape.
+    :return: iou: Intersection over union between the two masks.
+    """
+
+    union_matrix = numpy.logical_or(machine_mask_matrix, human_mask_matrix)
+    intersection_matrix = numpy.logical_and(
+        machine_mask_matrix, human_mask_matrix)
+
+    return float(numpy.sum(intersection_matrix)) / numpy.sum(union_matrix)
+
+
+def _plot_comparison(input_matrix, input_metadata_dict, machine_mask_matrix,
+                     human_mask_matrix, output_file_name):
+    """Plots comparison between human and machine masks.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    This method compares areas of extreme positive *or* negative saliency, not
+    both.
+
+    :param input_matrix: M-by-N numpy array with input data (radar image) over
+        which saliency was computed.
+    :param input_metadata_dict: Dictionary created by
+        `plot_input_examples.radar_fig_file_name_to_metadata`.
+    :param machine_mask_matrix: M-by-N Boolean numpy array, representing areas
+        of extreme saliency selon la machine.
+    :param human_mask_matrix: M-by-N Boolean numpy array, representing areas of
+        extreme saliency selon l'humain.
+    :param output_file_name: Path to output file (figure will be saved here).
+    """
+
+    radar_field_name = input_metadata_dict[plot_input_examples.RADAR_FIELD_KEY]
+    radar_height_m_asl = input_metadata_dict[
+        plot_input_examples.RADAR_HEIGHT_KEY]
+    layer_operation_dict = input_metadata_dict[
+        plot_input_examples.LAYER_OPERATION_KEY]
+
+    if radar_field_name is None:
+        radar_field_name = layer_operation_dict[input_examples.RADAR_FIELD_KEY]
+
+    _, axes_object = pyplot.subplots(
+        nrows=1, ncols=1,
+        figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+    )
+
+    radar_plotting.plot_2d_grid_without_coords(
+        field_matrix=numpy.flip(input_matrix, axis=0),
+        field_name=radar_field_name, axes_object=axes_object)
+
+    colour_map_object, colour_norm_object = (
+        radar_plotting.get_default_colour_scheme(radar_field_name)
+    )
+
+    colour_bar_object = plotting_utils.plot_colour_bar(
+        axes_object_or_matrix=axes_object, data_matrix=input_matrix,
+        colour_map_object=colour_map_object,
+        colour_norm_object=colour_norm_object,
+        orientation_string='horizontal', extend_min=True, extend_max=True,
+        fraction_of_axis_length=0.8)
+
+    if layer_operation_dict is None:
+        label_string = radar_plotting.FIELD_NAME_TO_VERBOSE_DICT[
+            radar_field_name]
+
+        if radar_height_m_asl is not None:
+            label_string += ' at {0:.2f} km AGL'.format(
+                radar_height_m_asl * METRES_TO_KM)
+    else:
+        label_string = radar_plotting.layer_ops_to_field_and_panel_names(
+            list_of_layer_operation_dicts=[layer_operation_dict]
+        )[-1][0]
+
+    colour_bar_object.set_label(label_string)
+    
+    print('Saving figure to: "{0:s}"...'.format(output_file_name))
+    pyplot.savefig(output_file_name, dpi=FIGURE_RESOLUTION_DPI)
+    pyplot.close()
+
+    imagemagick_utils.trim_whitespace(input_file_name=output_file_name,
+                                      output_file_name=output_file_name)
+
 
 def _run(input_human_file_name, input_machine_file_name,
-         abs_percentile_threshold):
+         abs_percentile_threshold, output_dir_name):
     """Compares human-generated vs. machine-generated saliency map.
 
     This is effectively the main method.
@@ -59,16 +169,20 @@ def _run(input_human_file_name, input_machine_file_name,
     :param input_human_file_name: See documentation at top of file.
     :param input_machine_file_name: Same.
     :param abs_percentile_threshold: Same.
+    :param output_dir_name: Same.
     """
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        directory_name=output_dir_name)
 
     error_checking.assert_is_geq(abs_percentile_threshold, 0.)
     error_checking.assert_is_leq(abs_percentile_threshold, 100.)
 
     human_polygon_dict = human_polygons.read_polygons(input_human_file_name)
-    human_negative_mask_matrix = human_polygon_dict[
-        human_polygons.NEGATIVE_MASK_MATRIX_KEY]
     human_positive_mask_matrix = human_polygon_dict[
         human_polygons.POSITIVE_MASK_MATRIX_KEY]
+    human_negative_mask_matrix = human_polygon_dict[
+        human_polygons.NEGATIVE_MASK_MATRIX_KEY]
 
     metadata_dict = plot_input_examples.radar_fig_file_name_to_metadata(
         human_polygon_dict[human_polygons.IMAGE_FILE_KEY]
@@ -212,20 +326,34 @@ def _run(input_human_file_name, input_machine_file_name,
     else:
         negative_saliency_threshold = -1 * TOLERANCE
 
-    machine_negative_mask_matrix = (
-        machine_saliency_matrix <= negative_saliency_threshold
-    )
-
     machine_positive_mask_matrix = (
         machine_saliency_matrix >= positive_saliency_threshold
     )
 
-    print(human_positive_mask_matrix.astype(int))
-    print('\n')
-    print(machine_positive_mask_matrix.astype(int))
-    print('\n\n\n')
-    print(numpy.sum(human_positive_mask_matrix.astype(int)))
-    print(numpy.sum(machine_positive_mask_matrix.astype(int)))
+    machine_negative_mask_matrix = (
+        machine_saliency_matrix <= negative_saliency_threshold
+    )
+
+    positive_iou = _compute_iou(
+        machine_mask_matrix=machine_positive_mask_matrix,
+        human_mask_matrix=human_positive_mask_matrix)
+
+    print('IoU for positive saliency = {0:.3f}'.format(positive_iou))
+
+    negative_iou = _compute_iou(
+        machine_mask_matrix=machine_negative_mask_matrix,
+        human_mask_matrix=human_negative_mask_matrix)
+
+    print('IoU for negative saliency = {0:.3f}'.format(negative_iou))
+
+    positive_figure_file_name = '{0:s}/positive_comparison.jpg'.format(
+        output_dir_name)
+
+    _plot_comparison(
+        input_matrix=input_matrix, input_metadata_dict=metadata_dict,
+        machine_mask_matrix=machine_positive_mask_matrix,
+        human_mask_matrix=human_positive_mask_matrix,
+        output_file_name=positive_figure_file_name)
 
 
 if __name__ == '__main__':
@@ -235,5 +363,6 @@ if __name__ == '__main__':
         input_human_file_name=getattr(INPUT_ARG_OBJECT, HUMAN_FILE_ARG_NAME),
         input_machine_file_name=getattr(
             INPUT_ARG_OBJECT, MACHINE_FILE_ARG_NAME),
-        abs_percentile_threshold=getattr(INPUT_ARG_OBJECT, THRESHOLD_ARG_NAME)
+        abs_percentile_threshold=getattr(INPUT_ARG_OBJECT, THRESHOLD_ARG_NAME),
+        output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
