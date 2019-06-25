9e78775ab7d6a77a27407b40369a0ce2710583ab
thunderhoser <ryan.lagerquist@ou.edu>
2019-06-24 19:30:14 -0600

    Made tweak to plot_examples_with_context.py.

--- gewittergefahr/scripts/plot_examples_with_context.py
+++ gewittergefahr/scripts/plot_examples_with_context.py
@@ -303,42 +303,42 @@ def _plot_one_example_one_time(
     this_storm_object_table = storm_object_table.loc[
         storm_object_table[tracking_utils.SECONDARY_ID_COLUMN] ==
         secondary_id_string
-    ]
-
-    storm_plotting.plot_storm_outlines(
-        storm_object_table=this_storm_object_table, axes_object=axes_object,
-        basemap_object=basemap_object, line_width=4, line_colour='k',
-        line_style='solid')
+        ]
 
     this_num_storm_objects = len(this_storm_object_table.index)
 
-    axes_object.text(
-        this_storm_object_table[
-            tracking_utils.CENTROID_LONGITUDE_COLUMN].values,
-        this_storm_object_table[
-            tracking_utils.CENTROID_LATITUDE_COLUMN].values,
-        ['S'] * this_num_storm_objects, fontsize=FONT_SIZE,
-        color=FONT_COLOUR, horizontalalignment='center',
-        verticalalignment='center')
+    if this_num_storm_objects > 0:
+        storm_plotting.plot_storm_outlines(
+            storm_object_table=this_storm_object_table, axes_object=axes_object,
+            basemap_object=basemap_object, line_width=4, line_colour='k',
+            line_style='solid')
 
-    this_polygon_object_latlng = this_storm_object_table[
-        tracking_utils.LATLNG_POLYGON_COLUMN].values[0]
+        axes_object.text(
+            this_storm_object_table[
+                tracking_utils.CENTROID_LONGITUDE_COLUMN].values[0],
+            this_storm_object_table[
+                tracking_utils.CENTROID_LATITUDE_COLUMN].values[0],
+            'S', fontsize=FONT_SIZE, color=FONT_COLOUR,
+            horizontalalignment='center', verticalalignment='center')
 
-    this_latitude_deg = 0.02 + numpy.max(
-        numpy.array(this_polygon_object_latlng.exterior.xy[1])
-    )
+        this_polygon_object_latlng = this_storm_object_table[
+            tracking_utils.LATLNG_POLYGON_COLUMN].values[0]
 
-    this_longitude_deg = 0.02 + numpy.max(
-        numpy.array(this_polygon_object_latlng.exterior.xy[0])
-    )
+        this_latitude_deg = 0.02 + numpy.max(
+            numpy.array(this_polygon_object_latlng.exterior.xy[1])
+        )
 
-    valid_time_string = time_conversion.unix_sec_to_string(
-        valid_time_unix_sec, TORNADO_TIME_FORMAT)
+        this_longitude_deg = 0.02 + numpy.max(
+            numpy.array(this_polygon_object_latlng.exterior.xy[0])
+        )
 
-    axes_object.text(
-        this_longitude_deg, this_latitude_deg, valid_time_string,
-        fontsize=FONT_SIZE, color=FONT_COLOUR, horizontalalignment='left',
-        verticalalignment='bottom')
+        valid_time_string = time_conversion.unix_sec_to_string(
+            valid_time_unix_sec, TORNADO_TIME_FORMAT)
+
+        axes_object.text(
+            this_longitude_deg, this_latitude_deg, valid_time_string,
+            fontsize=FONT_SIZE, color=FONT_COLOUR, horizontalalignment='left',
+            verticalalignment='bottom')
 
     tornado_latitudes_deg = tornado_table[linkage.EVENT_LATITUDE_COLUMN].values
     tornado_longitudes_deg = tornado_table[
