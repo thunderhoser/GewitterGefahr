7c92706384fa84dd5c628b10fb79b0e21ca0183f
thunderhoser <ryan.lagerquist@ou.edu>
2019-07-10 00:37:32 -0600

    Added debugging print statement to run_gradcam.py.

--- gewittergefahr/scripts/run_gradcam.py
+++ gewittergefahr/scripts/run_gradcam.py
@@ -173,6 +173,9 @@ def _run(model_file_name, target_class, target_layer_name, top_example_dir_name,
                 these_guided_cam_matrices)
         else:
             for j in range(len(these_cam_matrices)):
+                print(list_of_cam_matrices[j].shape)
+                print(these_cam_matrices[j].shape)
+
                 list_of_cam_matrices[j] = numpy.concatenate(
                     (list_of_cam_matrices[j], these_cam_matrices[j]), axis=0
                 )
