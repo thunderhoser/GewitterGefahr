7081348fc65a1aeb2488a1cb8ba1d270620e5dd5
thunderhoser <ryan.lagerquist@ou.edu>
2019-07-26 05:11:30 -0600

    Uncommented code in radar_plotting.py.

--- gewittergefahr/plotting/radar_plotting.py
+++ gewittergefahr/plotting/radar_plotting.py
@@ -613,17 +613,15 @@ def plot_latlng_grid(
         colour_map_object, colour_norm_object = get_default_colour_scheme(
             field_name)
     else:
-        pass
-
-        # if hasattr(colour_norm_object, 'boundaries'):
-        #     colour_norm_object.boundaries = _field_to_plotting_units(
-        #         field_matrix=colour_norm_object.boundaries,
-        #         field_name=field_name)
-        # else:
-        #     colour_norm_object.vmin = _field_to_plotting_units(
-        #         field_matrix=colour_norm_object.vmin, field_name=field_name)
-        #     colour_norm_object.vmax = _field_to_plotting_units(
-        #         field_matrix=colour_norm_object.vmax, field_name=field_name)
+        if hasattr(colour_norm_object, 'boundaries'):
+            colour_norm_object.boundaries = _field_to_plotting_units(
+                field_matrix=colour_norm_object.boundaries,
+                field_name=field_name)
+        else:
+            colour_norm_object.vmin = _field_to_plotting_units(
+                field_matrix=colour_norm_object.vmin, field_name=field_name)
+            colour_norm_object.vmax = _field_to_plotting_units(
+                field_matrix=colour_norm_object.vmax, field_name=field_name)
 
     if hasattr(colour_norm_object, 'boundaries'):
         min_colour_value = colour_norm_object.boundaries[0]
@@ -632,11 +630,11 @@ def plot_latlng_grid(
         min_colour_value = colour_norm_object.vmin
         max_colour_value = colour_norm_object.vmax
 
-    # if not use_default_colour_scheme:
-    #     min_colour_value = _field_to_plotting_units(
-    #         field_matrix=min_colour_value, field_name=field_name)
-    #     max_colour_value = _field_to_plotting_units(
-    #         field_matrix=max_colour_value, field_name=field_name)
+    if not use_default_colour_scheme:
+        min_colour_value = _field_to_plotting_units(
+            field_matrix=min_colour_value, field_name=field_name)
+        max_colour_value = _field_to_plotting_units(
+            field_matrix=max_colour_value, field_name=field_name)
 
     pyplot.pcolormesh(
         grid_cell_edge_longitudes_deg, grid_cell_edge_latitudes_deg,
