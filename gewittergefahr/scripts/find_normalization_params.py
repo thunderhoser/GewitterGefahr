49cb17ccccd4028086151d9cd9f683e5058f3732
thunderhoser <ryan.lagerquist@ou.edu>
2019-08-02 14:25:04 -0600

    Added input arg to find_normalization_params.py.

--- gewittergefahr/scripts/find_normalization_params.py
+++ gewittergefahr/scripts/find_normalization_params.py
@@ -11,6 +11,7 @@ import pandas
 from scipy.interpolate import interp1d
 from gewittergefahr.gg_utils import radar_utils
 from gewittergefahr.gg_utils import soundings
+from gewittergefahr.gg_utils import time_conversion
 from gewittergefahr.gg_utils import number_rounding
 from gewittergefahr.deep_learning import input_examples
 from gewittergefahr.deep_learning import deep_learning_utils as dl_utils
@@ -56,6 +57,8 @@ SOUNDING_INTERVAL_DICT = {
 }
 
 EXAMPLE_DIR_ARG_NAME = 'input_example_dir_name'
+FIRST_DATE_ARG_NAME = 'first_spc_date_string'
+LAST_DATE_ARG_NAME = 'last_spc_date_string'
 MIN_PERCENTILE_ARG_NAME = 'min_percentile_level'
 MAX_PERCENTILE_ARG_NAME = 'max_percentile_level'
 NUM_ROWS_ARG_NAME = 'num_radar_rows'
@@ -66,6 +69,11 @@ EXAMPLE_DIR_HELP_STRING = (
     'Name of top-level directory with input examples.  Shuffled files therein '
     'will be found by `input_examples.find_many_example_files` and read by '
     '`input_examples.read_example_file`.')
+
+SPC_DATE_HELP_STRING = (
+    'SPC date (format "yyyymmdd").  Normalization params will be computed only '
+    'for SPC dates in range `{0:s}`...`{1:s}`.'
+).format(FIRST_DATE_ARG_NAME, LAST_DATE_ARG_NAME)
 
 MIN_PERCENTILE_HELP_STRING = (
     'Minimum percentile level.  The "minimum value" for each field will '
@@ -91,6 +99,14 @@ INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + EXAMPLE_DIR_ARG_NAME, type=str, required=True,
     help=EXAMPLE_DIR_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + FIRST_DATE_ARG_NAME, type=str, required=True,
+    help=SPC_DATE_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + LAST_DATE_ARG_NAME, type=str, required=True,
+    help=SPC_DATE_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
     '--' + MIN_PERCENTILE_ARG_NAME, type=float, required=False, default=0.1,
@@ -299,13 +315,16 @@ def _convert_normalization_params(
         columns=column_dict_old_to_new, inplace=False)
 
 
-def _run(top_example_dir_name, min_percentile_level, max_percentile_level,
-         num_radar_rows, num_radar_columns, output_file_name):
+def _run(top_example_dir_name, first_spc_date_string, last_spc_date_string,
+         min_percentile_level, max_percentile_level, num_radar_rows,
+         num_radar_columns, output_file_name):
     """Finds normalization parameters for GridRad data.
 
     This is effectively the main method.
 
     :param top_example_dir_name: See documentation at top of file.
+    :param first_spc_date_string: Same.
+    :param last_spc_date_string: Same.
     :param min_percentile_level: Same.
     :param max_percentile_level: Same.
     :param num_radar_rows: Same.
@@ -317,6 +336,11 @@ def _run(top_example_dir_name, min_percentile_level, max_percentile_level,
         num_radar_rows = None
     if num_radar_columns <= 0:
         num_radar_columns = None
+
+    first_time_unix_sec = time_conversion.get_start_of_spc_date(
+        first_spc_date_string)
+    last_time_unix_sec = time_conversion.get_end_of_spc_date(
+        last_spc_date_string)
 
     example_file_names = input_examples.find_many_example_files(
         top_directory_name=top_example_dir_name, shuffled=True,
@@ -425,7 +449,13 @@ def _run(top_example_dir_name, min_percentile_level, max_percentile_level,
         this_example_dict = input_examples.read_example_file(
             netcdf_file_name=this_example_file_name, read_all_target_vars=True,
             num_rows_to_keep=num_radar_rows,
-            num_columns_to_keep=num_radar_columns)
+            num_columns_to_keep=num_radar_columns,
+            first_time_to_keep_unix_sec=first_time_unix_sec,
+            last_time_to_keep_unix_sec=last_time_unix_sec)
+
+        this_num_examples = len(this_example_dict[input_examples.FULL_IDS_KEY])
+        if this_num_examples == 0:
+            continue
 
         for j in range(num_radar_fields):
             print('Updating normalization params for "{0:s}"...'.format(
@@ -651,6 +681,8 @@ if __name__ == '__main__':
 
     _run(
         top_example_dir_name=getattr(INPUT_ARG_OBJECT, EXAMPLE_DIR_ARG_NAME),
+        first_spc_date_string=getattr(INPUT_ARG_OBJECT, FIRST_DATE_ARG_NAME),
+        last_spc_date_string=getattr(INPUT_ARG_OBJECT, LAST_DATE_ARG_NAME),
         min_percentile_level=getattr(INPUT_ARG_OBJECT, MIN_PERCENTILE_ARG_NAME),
         max_percentile_level=getattr(INPUT_ARG_OBJECT, MAX_PERCENTILE_ARG_NAME),
         num_radar_rows=getattr(INPUT_ARG_OBJECT, NUM_ROWS_ARG_NAME),
