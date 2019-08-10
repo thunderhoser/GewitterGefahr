f55162167833d446f75cb98db03d7c4f7d307750
Ryan Lagerquist <ryan.lagerquist@edmonton.protect.nssl>
2019-08-09 22:23:46 -0500

    Removed debugging print statement.

--- gewittergefahr/scripts/train_cnn_gridrad_2d_reduced.py
+++ gewittergefahr/scripts/train_cnn_gridrad_2d_reduced.py
@@ -276,10 +276,10 @@ def _run(input_model_file_name, radar_field_name_by_channel,
     model_object.summary()
     print(SEPARATOR_STRING)
 
-    print(K.eval(
-        model_object.get_layer(name='radar_conv2d_2').weights[0]
-    ))
-    print(SEPARATOR_STRING)
+    # print(K.eval(
+    #     model_object.get_layer(name='radar_conv2d_2').weights[0]
+    # ))
+    # print(SEPARATOR_STRING)
 
     # Write metadata.
     metadata_dict = {
