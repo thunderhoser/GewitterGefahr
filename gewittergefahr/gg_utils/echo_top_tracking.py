28d0e181948901eebc8619a611de1b24c5fb7f43
thunderhoser <ryan.lagerquist@ou.edu>
2019-09-08 20:04:03 -0500

    Added debugging print statements to echo_top_tracking.py.

--- gewittergefahr/gg_utils/echo_top_tracking.py
+++ gewittergefahr/gg_utils/echo_top_tracking.py
@@ -575,11 +575,10 @@ def _make_regions_contiguous(radar_to_region_matrix):
             )
 
             if k == 65:
-                print(numpy.sum(neigh_row_flags))
+                print(j - 1)
+                print(j + 1)
+                print(columns_in_region)
                 print(numpy.sum(neigh_column_flags))
-                print(numpy.sum(numpy.logical_and(
-                    neigh_row_flags, neigh_column_flags
-                )))
                 print('\n\n')
 
             num_neighbours = -1 + numpy.sum(numpy.logical_and(
