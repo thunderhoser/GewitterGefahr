a75e27266a95269934659084fd716efa63dfc9ae
thunderhoser <ryan.lagerquist@ou.edu>
2019-09-09 17:39:14 -0500

    Added another terrible hack to train_cnn_3d_gridrad.py.

--- gewittergefahr/scripts/train_cnn_3d_gridrad.py
+++ gewittergefahr/scripts/train_cnn_3d_gridrad.py
@@ -14,8 +14,13 @@ from gewittergefahr.deep_learning import input_examples
 from gewittergefahr.deep_learning import training_validation_io as trainval_io
 from gewittergefahr.scripts import deep_learning_helper as dl_helper
 
+# K.set_session(K.tf.Session(config=K.tf.ConfigProto(
+#     intra_op_parallelism_threads=7, inter_op_parallelism_threads=7
+# )))
+
 K.set_session(K.tf.Session(config=K.tf.ConfigProto(
-    intra_op_parallelism_threads=7, inter_op_parallelism_threads=7
+    intra_op_parallelism_threads=1, inter_op_parallelism_threads=1,
+    allow_soft_placement=False
 )))
 
 TIME_FORMAT = '%Y-%m-%d-%H%M%S'
