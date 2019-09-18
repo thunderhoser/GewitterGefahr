b24af5f1d88b544704a70e94903797dd38eb0a86
thunderhoser <ryan.lagerquist@ou.edu>
2019-09-17 23:00:07 -0500

    Added debugging print statements to model_evaluation_helper.py.

--- gewittergefahr/plotting/model_eval_plotting.py
+++ gewittergefahr/plotting/model_eval_plotting.py
@@ -710,9 +710,6 @@ def plot_reliability_curve(
         numpy.isnan(mean_forecast_by_bin), numpy.isnan(event_frequency_by_bin)
     )
 
-    print(mean_forecast_by_bin)
-    print(event_frequency_by_bin)
-
     if not numpy.all(nan_flags):
         real_indices = numpy.where(numpy.invert(nan_flags))[0]
 
