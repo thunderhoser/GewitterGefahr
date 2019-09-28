2d2b9d44001bcf5b844dc5427f0cc1c8d4b42b7a
thunderhoser <ryan.lagerquist@ou.edu>
2019-09-28 00:55:10 -0500

    Tweaked plotting thing.

--- gewittergefahr/prediction_paper_2019/make_extreme_gridrad_figure.py
+++ gewittergefahr/prediction_paper_2019/make_extreme_gridrad_figure.py
@@ -154,9 +154,9 @@ def _run(composite_file_names, composite_names, output_dir_name):
             list_of_predictor_matrices=list_of_mean_input_matrices,
             model_metadata_dict=model_metadata_dict, pmm_flag=True,
             plot_sounding=True, allow_whitespace=True, plot_panel_names=True,
-            panel_name_font_size=20,
+            panel_name_font_size=30,
             add_titles=False, label_colour_bars=True, colour_bar_length=0.25,
-            colour_bar_font_size=20, sounding_font_size=20)
+            colour_bar_font_size=30, sounding_font_size=30)
 
         sounding_figure_object = handle_dict[
             plot_input_examples.SOUNDING_FIGURE_KEY]
