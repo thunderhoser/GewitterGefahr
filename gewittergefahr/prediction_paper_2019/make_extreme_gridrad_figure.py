6f2775c88cd5fc1125ae6b49d71dddf6153884fc
thunderhoser <ryan.lagerquist@ou.edu>
2019-09-28 18:08:58 -0500

    Tweaked plotting thing.

--- gewittergefahr/prediction_paper_2019/make_extreme_gridrad_figure.py
+++ gewittergefahr/prediction_paper_2019/make_extreme_gridrad_figure.py
@@ -32,6 +32,13 @@ PANEL_NAME_FONT_SIZE = 30
 COLOUR_BAR_FONT_SIZE = 25
 SOUNDING_FONT_SIZE = 30
 
+CONVERT_EXE_NAME = '/usr/bin/convert'
+TITLE_FONT_SIZE = 125
+TITLE_FONT_TYPE = 'DejaVu-Sans-Condensed'
+
+BORDER_WIDTH_SANS_TITLE_PX = 300
+BORDER_WIDTH_WITH_TITLE_PX = 10
+
 FIGURE_RESOLUTION_DPI = 300
 CONCAT_FIGURE_SIZE_PX = int(1e7)
 
@@ -67,7 +74,7 @@ INPUT_ARG_PARSER.add_argument(
     help=OUTPUT_DIR_HELP_STRING)
 
 
-def _read_pmm_composite(pickle_file_name):
+def _read_composite(pickle_file_name):
     """Reads PMM composite of examples (storm objects) from Pickle file.
 
     T = number of input tensors to model
@@ -120,6 +127,122 @@ def _read_pmm_composite(pickle_file_name):
     return list_of_mean_input_matrices, model_metadata_dict
 
 
+def _overlay_text(
+        image_file_name, x_offset_from_center_px, y_offset_from_top_px,
+        text_string):
+    """Overlays text on image.
+
+    :param image_file_name: Path to image file.
+    :param x_offset_from_center_px: Center-relative x-coordinate (pixels).
+    :param y_offset_from_top_px: Top-relative y-coordinate (pixels).
+    :param text_string: String to overlay.
+    :raises: ValueError: if ImageMagick command (which is ultimately a Unix
+        command) fails.
+    """
+
+    command_string = (
+        '"{0:s}" "{1:s}" -gravity north -pointsize {2:d} -font "{3:s}" '
+        '-fill "rgb(0, 0, 0)" -annotate {4:+d}{5:+d} "{6:s}" "{1:s}"'
+    ).format(
+        CONVERT_EXE_NAME, image_file_name, TITLE_FONT_SIZE, TITLE_FONT_TYPE,
+        x_offset_from_center_px, y_offset_from_top_px, text_string
+    )
+
+    exit_code = os.system(command_string)
+    if exit_code == 0:
+        return
+
+    raise ValueError(imagemagick_utils.ERROR_STRING)
+
+
+def _plot_composite(
+        composite_file_name, composite_name_abbrev, composite_name_verbose,
+        output_dir_name):
+    """Plots one composite.
+    
+    :param composite_file_name: Path to input file.  Will be read by
+        `_read_composite`.
+    :param composite_name_abbrev: Abbreviated name for composite.  Will be used
+        in names of output files.
+    :param composite_name_verbose: Verbose name for composite.  Will be used as
+        figure title.
+    :param output_dir_name: Path to output directory.  Figures will be saved
+        here.
+    :return: radar_figure_file_name: Path to file with radar figure for this
+        composite.
+    :return: sounding_figure_file_name: Path to file with sounding figure for
+        this composite.
+    """
+
+    list_of_mean_input_matrices, model_metadata_dict = _read_composite(
+        composite_file_name)
+
+    radar_field_names = model_metadata_dict[
+        cnn.TRAINING_OPTION_DICT_KEY][trainval_io.RADAR_FIELDS_KEY]
+    num_radar_fields = len(radar_field_names)
+
+    handle_dict = plot_input_examples.plot_one_example(
+        list_of_predictor_matrices=list_of_mean_input_matrices,
+        model_metadata_dict=model_metadata_dict, pmm_flag=True,
+        plot_sounding=True, allow_whitespace=True, plot_panel_names=True,
+        panel_name_font_size=PANEL_NAME_FONT_SIZE,
+        add_titles=False, label_colour_bars=True,
+        colour_bar_length=COLOUR_BAR_LENGTH,
+        colour_bar_font_size=COLOUR_BAR_FONT_SIZE,
+        sounding_font_size=SOUNDING_FONT_SIZE)
+    
+    # TODO(thunderhoser): Do something with sounding.
+    sounding_figure_object = handle_dict[
+        plot_input_examples.SOUNDING_FIGURE_KEY]
+    pyplot.close(sounding_figure_object)
+
+    radar_figure_objects = handle_dict[
+        plot_input_examples.RADAR_FIGURES_KEY]
+    panel_file_names = [None] * num_radar_fields
+
+    for j in range(num_radar_fields):
+        panel_file_names[j] = '{0:s}/{1:s}_{2:s}.jpg'.format(
+            output_dir_name, composite_name_abbrev,
+            radar_field_names[j].replace('_', '-')
+        )
+
+        print('Saving figure to: "{0:s}"...'.format(panel_file_names[j]))
+
+        radar_figure_objects[j].savefig(
+            panel_file_names[j], dpi=FIGURE_RESOLUTION_DPI,
+            pad_inches=0, bbox_inches='tight'
+        )
+        pyplot.close(radar_figure_objects[j])
+
+    radar_figure_file_name = '{0:s}/{1:s}_radar.jpg'.format(
+        output_dir_name, composite_name_abbrev)
+
+    print('Concatenating panels to: "{0:s}"...'.format(radar_figure_file_name))
+
+    imagemagick_utils.concatenate_images(
+        input_file_names=panel_file_names,
+        output_file_name=radar_figure_file_name,
+        num_panel_rows=1, num_panel_columns=num_radar_fields,
+        border_width_pixels=BORDER_WIDTH_SANS_TITLE_PX)
+
+    _overlay_text(
+        image_file_name=radar_figure_file_name,
+        x_offset_from_center_px=0, y_offset_from_top_px=0,
+        text_string=composite_name_verbose)
+
+    imagemagick_utils.trim_whitespace(
+        input_file_name=radar_figure_file_name,
+        output_file_name=radar_figure_file_name,
+        border_width_pixels=BORDER_WIDTH_WITH_TITLE_PX)
+
+    imagemagick_utils.resize_image(
+        input_file_name=radar_figure_file_name,
+        output_file_name=radar_figure_file_name,
+        output_size_pixels=CONCAT_FIGURE_SIZE_PX)
+
+    return radar_figure_file_name
+
+
 def _run(composite_file_names, composite_names, output_dir_name):
     """Makes figure with extreme examples for GridRad model.
 
@@ -144,68 +267,15 @@ def _run(composite_file_names, composite_names, output_dir_name):
     ]
     composite_names_verbose = [n.replace('_', ' ') for n in composite_names]
 
+    radar_figure_file_names = [None] * num_composites
+
     for i in range(num_composites):
-        list_of_mean_input_matrices, model_metadata_dict = _read_pmm_composite(
-            composite_file_names[i]
+        radar_figure_file_names[i] = _plot_composite(
+            composite_file_name=composite_file_names[i],
+            composite_name_abbrev=composite_names_abbrev[i],
+            composite_name_verbose=composite_names_verbose[i],
+            output_dir_name=output_dir_name
         )
-
-        print('\n')
-
-        radar_field_names = model_metadata_dict[
-            cnn.TRAINING_OPTION_DICT_KEY][trainval_io.RADAR_FIELDS_KEY]
-        num_radar_fields = len(radar_field_names)
-
-        handle_dict = plot_input_examples.plot_one_example(
-            list_of_predictor_matrices=list_of_mean_input_matrices,
-            model_metadata_dict=model_metadata_dict, pmm_flag=True,
-            plot_sounding=True, allow_whitespace=True, plot_panel_names=True,
-            panel_name_font_size=PANEL_NAME_FONT_SIZE,
-            add_titles=False, label_colour_bars=True,
-            colour_bar_length=COLOUR_BAR_LENGTH,
-            colour_bar_font_size=COLOUR_BAR_FONT_SIZE,
-            sounding_font_size=SOUNDING_FONT_SIZE)
-
-        sounding_figure_object = handle_dict[
-            plot_input_examples.SOUNDING_FIGURE_KEY]
-        pyplot.close(sounding_figure_object)
-
-        radar_figure_objects = handle_dict[
-            plot_input_examples.RADAR_FIGURES_KEY]
-        radar_panel_file_names = [None] * num_radar_fields
-
-        for j in range(num_radar_fields):
-            radar_panel_file_names[j] = '{0:s}/{1:s}_{2:s}.jpg'.format(
-                output_dir_name, composite_names_abbrev[i],
-                radar_field_names[j].replace('_', '-')
-            )
-
-            print('Saving figure to: "{0:s}"...'.format(
-                radar_panel_file_names[j]
-            ))
-
-            radar_figure_objects[j].savefig(
-                radar_panel_file_names[j], dpi=FIGURE_RESOLUTION_DPI,
-                pad_inches=0, bbox_inches='tight'
-            )
-            pyplot.close(radar_figure_objects[j])
-
-        radar_concat_file_name = '{0:s}/{1:s}_radar.jpg'.format(
-            output_dir_name, composite_names_abbrev[i]
-        )
-
-        print('Concatenating panels to: "{0:s}"...'.format(
-            radar_concat_file_name
-        ))
-
-        imagemagick_utils.concatenate_images(
-            input_file_names=radar_panel_file_names,
-            output_file_name=radar_concat_file_name,
-            num_panel_rows=1, num_panel_columns=num_radar_fields)
-
-        imagemagick_utils.resize_image(
-            input_file_name=radar_concat_file_name,
-            output_file_name=radar_concat_file_name,
-            output_size_pixels=CONCAT_FIGURE_SIZE_PX)
 
         print('\n')
 
