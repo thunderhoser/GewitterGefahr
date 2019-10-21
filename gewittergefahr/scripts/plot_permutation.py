1a1e77d55c6d21c71d042d5af3d79aa05a2b729c
thunderhoser <ryan.lagerquist@ou.edu>
2019-10-20 22:38:38 -0500

    Fixed calls from plot_permutation.py (script) to permutation_plotting.py (module).

--- gewittergefahr/scripts/plot_permutation.py
+++ gewittergefahr/scripts/plot_permutation.py
@@ -1,6 +1,5 @@
 """Plots results of permutation test."""
 
-import os.path
 import argparse
 import matplotlib
 matplotlib.use('agg')
@@ -14,6 +13,7 @@ FIGURE_RESOLUTION_DPI = 300
 
 INPUT_FILE_ARG_NAME = 'input_file_name'
 NUM_PREDICTORS_ARG_NAME = 'num_predictors_to_plot'
+CONFIDENCE_LEVEL_ARG_NAME = 'confidence_level'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 INPUT_FILE_HELP_STRING = (
@@ -24,9 +24,11 @@ NUM_PREDICTORS_HELP_STRING = (
     'plot all predictors, leave this argument alone.'
 ).format(NUM_PREDICTORS_ARG_NAME)
 
+CONFIDENCE_LEVEL_HELP_STRING = (
+    'Confidence level for error bars (in range 0...1).')
+
 OUTPUT_DIR_HELP_STRING = (
-    'Path to output directory (figures will be saved here).  If you want output'
-    ' directory = input directory, leave this argument alone.')
+    'Path to output directory (figures will be saved here).')
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
@@ -38,88 +40,90 @@ INPUT_ARG_PARSER.add_argument(
     help=NUM_PREDICTORS_HELP_STRING)
 
 INPUT_ARG_PARSER.add_argument(
-    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=False, default='',
+    '--' + CONFIDENCE_LEVEL_ARG_NAME, type=float, required=False, default=0.95,
+    help=CONFIDENCE_LEVEL_HELP_STRING)
+
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
     help=OUTPUT_DIR_HELP_STRING)
 
 
-def _run(input_file_name, num_predictors_to_plot, output_dir_name):
+def _run(input_file_name, num_predictors_to_plot, confidence_level,
+         output_dir_name):
     """Plots results of permutation test.
 
     This is effectively the main method.
 
     :param input_file_name: See documentation at top of file.
+    :param num_predictors_to_plot: Same.
+    :param confidence_level: Same.
     :param output_dir_name: Same.
     """
 
     if num_predictors_to_plot <= 0:
         num_predictors_to_plot = None
 
-    if output_dir_name in ['', 'None']:
-        output_dir_name = os.path.split(input_file_name)[0]
-
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=output_dir_name)
 
-    print('Reading permutation results from: "{0:s}"...'.format(input_file_name))
+    print('Reading data from: "{0:s}"...'.format(input_file_name))
     permutation_dict = permutation.read_results(input_file_name)
 
+    figure_object, axes_object_matrix = plotting_utils.create_paneled_figure(
+        num_rows=1, num_columns=2, shared_x_axis=False, shared_y_axis=True,
+        keep_aspect_ratio=False)
+
+    permutation_plotting.plot_single_pass_test(
+        permutation_dict=permutation_dict, axes_object=axes_object_matrix[0, 0],
+        num_predictors_to_plot=num_predictors_to_plot,
+        plot_percent_increase=False, confidence_level=confidence_level)
+
+    axes_object_matrix[0, 0].set_title('Single-pass test')
+
+    permutation_plotting.plot_multipass_test(
+        permutation_dict=permutation_dict, axes_object=axes_object_matrix[0, 1],
+        num_predictors_to_plot=num_predictors_to_plot,
+        plot_percent_increase=False, confidence_level=confidence_level)
+
+    axes_object_matrix[0, 0].set_ylabel('')
+    axes_object_matrix[0, 0].set_title('Multi-pass test')
+
+    figure_file_name = '{0:s}/permutation_test_abs-values.jpg'.format(
+        output_dir_name)
+
+    print('Saving figure to: "{0:s}"...'.format(figure_file_name))
+    figure_object.savefig(
+        figure_file_name, dpi=FIGURE_RESOLUTION_DPI, pad_inches=0,
+        bbox_inches='tight')
+    pyplot.close(figure_object)
+
     _, axes_object_matrix = plotting_utils.create_paneled_figure(
         num_rows=1, num_columns=2, shared_x_axis=False, shared_y_axis=True,
         keep_aspect_ratio=False)
 
-    permutation_plotting.plot_breiman_results(
-        permutation_dict=permutation_dict,
-        axes_object=axes_object_matrix[0, 0],
-        plot_percent_increase=False,
-        num_predictors_to_plot=num_predictors_to_plot)
+    permutation_plotting.plot_single_pass_test(
+        permutation_dict=permutation_dict, axes_object=axes_object_matrix[0, 0],
+        num_predictors_to_plot=num_predictors_to_plot,
+        plot_percent_increase=True, confidence_level=confidence_level)
 
-    axes_object_matrix[0, 0].set_xlabel('AUC')
-    axes_object_matrix[0, 0].set_title('Single-pass')
+    axes_object_matrix[0, 1].set_title('Single-pass test')
 
-    permutation_plotting.plot_lakshmanan_results(
-        permutation_dict=permutation_dict,
-        axes_object=axes_object_matrix[0, 1],
-        plot_percent_increase=False, num_steps_to_plot=num_predictors_to_plot)
-
-    axes_object_matrix[0, 1].set_xlabel('AUC')
-    axes_object_matrix[0, 1].set_ylabel('')
-    axes_object_matrix[0, 1].set_title('Multi-pass')
-
-    pyplot.tight_layout()
-    absolute_value_file_name = '{0:s}/permutation_absolute-values.jpg'.format(
-        output_dir_name)
-
-    print('Saving figure to file: "{0:s}"...'.format(absolute_value_file_name))
-    pyplot.savefig(absolute_value_file_name, dpi=FIGURE_RESOLUTION_DPI)
-    pyplot.close()
-
-    _, axes_object_matrix = plotting_utils.create_paneled_figure(
-        num_rows=1, num_columns=2, shared_x_axis=False, shared_y_axis=True,
-        keep_aspect_ratio=False)
-
-    permutation_plotting.plot_breiman_results(
-        permutation_dict=permutation_dict,
-        axes_object=axes_object_matrix[0, 0],
-        plot_percent_increase=True,
-        num_predictors_to_plot=num_predictors_to_plot)
-
-    axes_object_matrix[0, 0].set_title('Single-pass')
-
-    permutation_plotting.plot_lakshmanan_results(
-        permutation_dict=permutation_dict,
-        axes_object=axes_object_matrix[0, 1],
-        plot_percent_increase=True, num_steps_to_plot=num_predictors_to_plot)
+    permutation_plotting.plot_multipass_test(
+        permutation_dict=permutation_dict, axes_object=axes_object_matrix[0, 1],
+        num_predictors_to_plot=num_predictors_to_plot,
+        plot_percent_increase=True, confidence_level=confidence_level)
 
     axes_object_matrix[0, 1].set_ylabel('')
-    axes_object_matrix[0, 1].set_title('Multi-pass')
+    axes_object_matrix[0, 1].set_title('Multi-pass test')
 
-    pyplot.tight_layout()
-    percentage_file_name = '{0:s}/permutation_percentage.jpg'.format(
+    figure_file_name = '{0:s}/permutation_test_percentage.jpg'.format(
         output_dir_name)
 
-    print('Saving figure to file: "{0:s}"...'.format(percentage_file_name))
-    pyplot.savefig(percentage_file_name, dpi=FIGURE_RESOLUTION_DPI)
-    pyplot.close()
+    print('Saving figure to: "{0:s}"...'.format(figure_file_name))
+    figure_object.savefig(
+        figure_file_name, dpi=FIGURE_RESOLUTION_DPI, pad_inches=0,
+        bbox_inches='tight')
+    pyplot.close(figure_object)
 
 
 if __name__ == '__main__':
@@ -129,5 +133,6 @@ if __name__ == '__main__':
         input_file_name=getattr(INPUT_ARG_OBJECT, INPUT_FILE_ARG_NAME),
         num_predictors_to_plot=getattr(
             INPUT_ARG_OBJECT, NUM_PREDICTORS_ARG_NAME),
+        confidence_level=getattr(INPUT_ARG_OBJECT, CONFIDENCE_LEVEL_ARG_NAME),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
