475decf1c9a448d8324920dbc4df5865a75cce11
thunderhoser <ryan.lagerquist@ou.edu>
2019-11-11 19:02:17 -0600

    Tweaked text labels in plot_examples_with_context.py.

--- gewittergefahr/scripts/plot_examples_with_context.py
+++ gewittergefahr/scripts/plot_examples_with_context.py
@@ -377,7 +377,7 @@ def _plot_one_example_one_time(
         #     horizontalalignment='center', verticalalignment='top', zorder=1e10)
 
         axes_object.set_title(
-            label_string.replace('\n', ''), fontsize=TITLE_FONT_SIZE
+            label_string.replace('\n', ' '), fontsize=TITLE_FONT_SIZE
         )
 
     tornado_latitudes_deg = tornado_table[linkage.EVENT_LATITUDE_COLUMN].values
