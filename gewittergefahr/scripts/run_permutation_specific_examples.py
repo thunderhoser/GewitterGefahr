60f31ab1ab4d06d649d3eff5ad014c71c0d713b8
thunderhoser <ryan.lagerquist@ou.edu>
2020-02-04 16:23:49 -0600

    Added input arg to run_permutation_specific_examples.py.

--- gewittergefahr/scripts/run_permutation_specific_examples.py
+++ gewittergefahr/scripts/run_permutation_specific_examples.py
@@ -3,6 +3,7 @@
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import argparse
+import numpy
 from keras import backend as K
 from gewittergefahr.gg_io import storm_tracking_io as tracking_io
 from gewittergefahr.deep_learning import cnn
@@ -16,79 +17,92 @@ K.set_session(K.tf.Session(config=K.tf.ConfigProto(
     allow_soft_placement=False
 )))
 
+RANDOM_SEED = 6695
 SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
 
 MODEL_FILE_ARG_NAME = 'input_model_file_name'
 EXAMPLE_DIR_ARG_NAME = 'input_example_dir_name'
 STORM_METAFILE_ARG_NAME = 'input_storm_metafile_name'
+NUM_EXAMPLES_ARG_NAME = 'num_examples'
 DO_BACKWARDS_ARG_NAME = 'do_backwards_test'
 SEPARATE_HEIGHTS_ARG_NAME = 'separate_radar_heights'
 NUM_BOOTSTRAP_ARG_NAME = 'num_bootstrap_reps'
 OUTPUT_FILE_ARG_NAME = 'output_file_name'
 
 MODEL_FILE_HELP_STRING = (
-    'Path to file with trained CNN.  Will be read by `cnn.read_model`.')
-
+    'Path to file with trained CNN.  Will be read by `cnn.read_model`.'
+)
 EXAMPLE_DIR_HELP_STRING = (
     'Name of top-level directory with examples.  Files therein will be '
     'found by `input_examples.find_example_file` and read by '
-    '`input_examples.read_example_file`.')
-
+    '`input_examples.read_example_file`.'
+)
 STORM_METAFILE_HELP_STRING = (
     'Path to Pickle file with storm IDs and times (will be read by '
     '`storm_tracking_io.read_ids_and_times`).  Permutation test will be done '
-    'with only these storm objects (examples).')
+    'with only these storm objects (examples).'
+)
+NUM_EXAMPLES_HELP_STRING = (
+    'Number of examples to take from `{0:s}`.  If you want to use all examples,'
+    ' leave this argument alone.'
+).format(STORM_METAFILE_ARG_NAME)
 
 DO_BACKWARDS_HELP_STRING = (
     'Boolean flag.  If 1, will run backwards test.  If 0, will run forward '
-    'test.')
-
+    'test.'
+)
 SEPARATE_HEIGHTS_HELP_STRING = (
     'Boolean flag.  If 1, 3-D radar fields will be separated by height, so each'
     ' step will involve permuting one variable at one height.  If 0, each step '
-    'will involve permuting one variable at all heights.')
-
+    'will involve permuting one variable at all heights.'
+)
 NUM_BOOTSTRAP_HELP_STRING = (
     'Number of bootstrap replicates (used to compute the cost function after '
-    'each permutation).  If you do not want bootstrapping, make this <= 1.')
-
+    'each permutation).  If you do not want bootstrapping, make this <= 1.'
+)
 OUTPUT_FILE_HELP_STRING = (
     'Path to output (Pickle) file.  Will be written by '
-    '`permutation_utils.write_results`.')
+    '`permutation_utils.write_results`.'
+)
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
     '--' + MODEL_FILE_ARG_NAME, type=str, required=True,
-    help=MODEL_FILE_HELP_STRING)
-
+    help=MODEL_FILE_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + EXAMPLE_DIR_ARG_NAME, type=str, required=True,
-    help=EXAMPLE_DIR_HELP_STRING)
-
+    help=EXAMPLE_DIR_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + STORM_METAFILE_ARG_NAME, type=str, required=True,
-    help=STORM_METAFILE_HELP_STRING)
-
+    help=STORM_METAFILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + NUM_EXAMPLES_ARG_NAME, type=int, required=False, default=-1,
+    help=NUM_EXAMPLES_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + DO_BACKWARDS_ARG_NAME, type=int, required=False, default=0,
-    help=DO_BACKWARDS_HELP_STRING)
-
+    help=DO_BACKWARDS_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + SEPARATE_HEIGHTS_ARG_NAME, type=int, required=False, default=0,
-    help=SEPARATE_HEIGHTS_HELP_STRING)
-
+    help=SEPARATE_HEIGHTS_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + NUM_BOOTSTRAP_ARG_NAME, type=int, required=False, default=1,
-    help=NUM_BOOTSTRAP_HELP_STRING)
-
+    help=NUM_BOOTSTRAP_HELP_STRING
+)
 INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_FILE_ARG_NAME, type=str, required=True,
-    help=OUTPUT_FILE_HELP_STRING)
+    help=OUTPUT_FILE_HELP_STRING
+)
 
 
 def _run(model_file_name, top_example_dir_name, storm_metafile_name,
-         do_backwards_test, separate_radar_heights, num_bootstrap_reps,
-         output_file_name):
+         num_examples, do_backwards_test, separate_radar_heights,
+         num_bootstrap_reps, output_file_name):
     """Runs permutation test with specific examples (storm objects).
 
     This is effectively the main method.
@@ -96,6 +110,7 @@ def _run(model_file_name, top_example_dir_name, storm_metafile_name,
     :param model_file_name: See documentation at top of file.
     :param top_example_dir_name: Same.
     :param storm_metafile_name: Same.
+    :param num_examples: Same.
     :param do_backwards_test: Same.
     :param separate_radar_heights: Same.
     :param num_bootstrap_reps: Same.
@@ -115,6 +130,14 @@ def _run(model_file_name, top_example_dir_name, storm_metafile_name,
         tracking_io.read_ids_and_times(storm_metafile_name)
     )
     print(SEPARATOR_STRING)
+
+    if 0 < num_examples < len(full_storm_id_strings):
+        numpy.random.seed(RANDOM_SEED)
+        good_indices = numpy.random.permutation(len(full_storm_id_strings))
+        good_indices = good_indices[:num_examples]
+
+        full_storm_id_strings = [full_storm_id_strings[k] for k in good_indices]
+        storm_times_unix_sec = storm_times_unix_sec[good_indices]
 
     example_dict = testing_io.read_predictors_specific_examples(
         top_example_dir_name=top_example_dir_name,
@@ -170,7 +193,8 @@ def _run(model_file_name, top_example_dir_name, storm_metafile_name,
 
     print('Writing results to: "{0:s}"...'.format(output_file_name))
     permutation_utils.write_results(
-        result_dict=result_dict, pickle_file_name=output_file_name)
+        result_dict=result_dict, pickle_file_name=output_file_name
+    )
 
 
 if __name__ == '__main__':
@@ -180,6 +204,7 @@ if __name__ == '__main__':
         model_file_name=getattr(INPUT_ARG_OBJECT, MODEL_FILE_ARG_NAME),
         top_example_dir_name=getattr(INPUT_ARG_OBJECT, EXAMPLE_DIR_ARG_NAME),
         storm_metafile_name=getattr(INPUT_ARG_OBJECT, STORM_METAFILE_ARG_NAME),
+        num_examples=getattr(INPUT_ARG_OBJECT, NUM_EXAMPLES_ARG_NAME),
         do_backwards_test=bool(getattr(
             INPUT_ARG_OBJECT, DO_BACKWARDS_ARG_NAME
         )),
