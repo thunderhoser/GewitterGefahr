6dee964909369cfbc400e9d25b4991e6aab5d979
thunderhoser <ryan.lagerquist@ou.edu>
2020-02-23 15:54:50 -0600

    Added unit tests and output file to link_warnings_to_storms.py.

--- gewittergefahr/scripts/link_warnings_to_storms.py
+++ gewittergefahr/scripts/link_warnings_to_storms.py
@@ -14,6 +14,7 @@ from gewittergefahr.gg_utils import polygons
 from gewittergefahr.gg_utils import geodetic_utils
 from gewittergefahr.gg_utils import projections
 from gewittergefahr.gg_utils import linkage
+from gewittergefahr.gg_utils import file_system_utils
 from gewittergefahr.gg_utils import error_checking
 from gewittergefahr.nature2019 import convert_warning_polygons
 
@@ -28,16 +29,16 @@ WARNING_START_TIME_KEY = convert_warning_polygons.START_TIME_COLUMN
 WARNING_END_TIME_KEY = convert_warning_polygons.END_TIME_COLUMN
 WARNING_LATLNG_POLYGON_KEY = convert_warning_polygons.POLYGON_COLUMN
 WARNING_XY_POLYGON_KEY = 'polygon_object_xy'
+LINKED_SECONDARY_IDS_KEY = 'linked_sec_id_strings'
 
-# TODO(thunderhoser): Still need output file.
-
-WARNING_FILE_ARG_NAME = 'input_warning_file_name'
+INPUT_WARNING_FILE_ARG_NAME = 'input_warning_file_name'
 TRACKING_DIR_ARG_NAME = 'input_tracking_dir_name'
 SPC_DATE_ARG_NAME = 'spc_date_string'
 MAX_DISTANCE_ARG_NAME = 'max_distance_metres'
 MIN_LIFETIME_FRACTION_ARG_NAME = 'min_lifetime_fraction'
+OUTPUT_WARNING_FILE_ARG_NAME = 'output_warning_file_name'
 
-WARNING_FILE_HELP_STRING = (
+INPUT_WARNING_FILE_HELP_STRING = (
     'Path to Pickle file with tornado warnings (created by '
     'convert_warning_polygons.py).'
 )
@@ -60,11 +61,15 @@ MIN_LIFETIME_FRACTION_HELP_STRING = (
     'as long as the storm is in existence for at least this fraction (range '
     '0...1) of the warning period.'
 )
+OUTPUT_WARNING_FILE_HELP_STRING = (
+    'Path to output file (same as input file but with an extra column, '
+    '"{0:s}", in the pandas table).'
+).format(LINKED_SECONDARY_IDS_KEY)
 
 INPUT_ARG_PARSER = argparse.ArgumentParser()
 INPUT_ARG_PARSER.add_argument(
-    '--' + WARNING_FILE_ARG_NAME, type=str, required=True,
-    help=WARNING_FILE_HELP_STRING
+    '--' + INPUT_WARNING_FILE_ARG_NAME, type=str, required=True,
+    help=INPUT_WARNING_FILE_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
     '--' + TRACKING_DIR_ARG_NAME, type=str, required=True,
@@ -81,6 +86,10 @@ INPUT_ARG_PARSER.add_argument(
 INPUT_ARG_PARSER.add_argument(
     '--' + MIN_LIFETIME_FRACTION_ARG_NAME, type=float, required=False,
     default=0.5, help=MIN_LIFETIME_FRACTION_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
+    '--' + OUTPUT_WARNING_FILE_ARG_NAME, type=str, required=True,
+    help=OUTPUT_WARNING_FILE_HELP_STRING
 )
 
 
@@ -162,15 +171,6 @@ def _find_one_distance(storm_x_vertices_metres, storm_y_vertices_metres,
     num_vertices = len(storm_x_vertices_metres)
     distance_metres = LARGE_NUMBER
 
-    # print('x-vertices:\n{0:s}\n'.format(str(storm_x_vertices_metres)))
-    # print('y-vertices:\n{0:s}\n'.format(str(storm_y_vertices_metres)))
-    # print('x-vertices in warning polygon:\n{0:s}\n'.format(
-    #     str(numpy.array(warning_polygon_object_xy.exterior.xy[0]))
-    # ))
-    # print('y-vertices in warning polygon:\n{0:s}\n'.format(
-    #     str(numpy.array(warning_polygon_object_xy.exterior.xy[1]))
-    # ))
-
     for k in range(num_vertices):
         this_flag = polygons.point_in_or_on_polygon(
             polygon_object=warning_polygon_object_xy,
@@ -184,15 +184,6 @@ def _find_one_distance(storm_x_vertices_metres, storm_y_vertices_metres,
         this_point_object = shapely.geometry.Point(
             storm_x_vertices_metres[k], storm_y_vertices_metres[k]
         )
-
-        # print(this_point_object)
-        # print(warning_polygon_object_xy.exterior)
-        # this_distance_metres = warning_polygon_object_xy.exterior.distance(
-        #     this_point_object
-        # )
-        # print(this_distance_metres)
-        # print('\n\n')
-
         this_distance_metres = this_point_object.distance(
             warning_polygon_object_xy
         )
@@ -202,7 +193,7 @@ def _find_one_distance(storm_x_vertices_metres, storm_y_vertices_metres,
 
 
 def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
-                      min_lifetime_fraction):
+                      min_lifetime_fraction, test_mode=False):
     """Links one warning to nearest storm.
 
     :param warning_table: pandas DataFrame with one row and the following
@@ -219,6 +210,7 @@ def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
         `storm_tracking_io.read_file`.
     :param max_distance_metres: See documentation at top of file.
     :param min_lifetime_fraction: Same.
+    :param test_mode: Never mind.  Just leave this alone.
     :return: secondary_id_strings: 1-D list of secondary IDs for storms to which
         warning is linked.  If warning is not linked to a storm, this is empty.
     """
@@ -230,25 +222,27 @@ def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
     warning_polygon_object_xy = warning_table[WARNING_XY_POLYGON_KEY].values[0]
 
     orig_num_storm_objects = len(storm_object_table.index)
+
     storm_object_table = linkage._filter_storms_by_time(
         storm_object_table=storm_object_table,
         max_start_time_unix_sec=warning_end_time_unix_sec + 720,
         min_end_time_unix_sec=warning_start_time_unix_sec - 720
     )
-    num_storm_objects = len(storm_object_table.index)
 
+    num_storm_objects = len(storm_object_table.index)
     print('Filtering by time removed {0:d} of {1:d} storm objects.'.format(
         orig_num_storm_objects - num_storm_objects, orig_num_storm_objects
     ))
 
     orig_num_storm_objects = num_storm_objects + 0
+
     storm_object_table = _remove_far_away_storms(
         warning_polygon_object_latlng=
         warning_table[WARNING_LATLNG_POLYGON_KEY].values[0],
         storm_object_table=storm_object_table
     )
-    num_storm_objects = len(storm_object_table.index)
 
+    num_storm_objects = len(storm_object_table.index)
     print('Filtering by distance removed {0:d} of {1:d} storm objects.'.format(
         orig_num_storm_objects - num_storm_objects, orig_num_storm_objects
     ))
@@ -256,7 +250,7 @@ def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
     warning_times_unix_sec = time_periods.range_and_interval_to_list(
         start_time_unix_sec=warning_start_time_unix_sec,
         end_time_unix_sec=warning_end_time_unix_sec,
-        time_interval_sec=60, include_endpoint=True
+        time_interval_sec=1 if test_mode else 60, include_endpoint=True
     )
 
     unique_sec_id_strings = numpy.unique(
@@ -273,7 +267,8 @@ def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
         this_interp_vertex_table = linkage._interp_storms_in_time(
             storm_object_table=storm_object_table,
             target_time_unix_sec=warning_times_unix_sec[j],
-            max_time_before_start_sec=180, max_time_after_end_sec=180
+            max_time_before_start_sec=0 if test_mode else 180,
+            max_time_after_end_sec=0 if test_mode else 180
         )
 
         for i in range(num_sec_id_strings):
@@ -319,25 +314,30 @@ def _link_one_warning(warning_table, storm_object_table, max_distance_metres,
     return [unique_sec_id_strings[k] for k in good_indices]
 
 
-def _run(warning_file_name, top_tracking_dir_name, spc_date_string,
-         max_distance_metres, min_lifetime_fraction):
+def _run(input_warning_file_name, top_tracking_dir_name, spc_date_string,
+         max_distance_metres, min_lifetime_fraction, output_warning_file_name):
     """Links each NWS tornado warning to nearest storm.
 
     This is effectively the main method.
 
-    :param warning_file_name: See documentation at top of file.
+    :param input_warning_file_name: See documentation at top of file.
     :param top_tracking_dir_name: Same.
     :param spc_date_string: Same.
     :param max_distance_metres: Same.
     :param min_lifetime_fraction: Same.
+    :param output_warning_file_name: Same.
     """
+
+    file_system_utils.mkdir_recursive_if_necessary(
+        file_name=output_warning_file_name
+    )
 
     error_checking.assert_is_greater(max_distance_metres, 0.)
     error_checking.assert_is_greater(min_lifetime_fraction, 0.)
     error_checking.assert_is_leq(min_lifetime_fraction, 1.)
 
-    print('Reading warnings from: "{0:s}"...'.format(warning_file_name))
-    this_file_handle = open(warning_file_name, 'rb')
+    print('Reading warnings from: "{0:s}"...'.format(input_warning_file_name))
+    this_file_handle = open(input_warning_file_name, 'rb')
     warning_table = pickle.load(this_file_handle)
     this_file_handle.close()
 
@@ -350,7 +350,7 @@ def _run(warning_file_name, top_tracking_dir_name, spc_date_string,
     warning_table = warning_table.loc[
         (warning_table[WARNING_START_TIME_KEY] >= date_start_time_unix_sec) &
         (warning_table[WARNING_START_TIME_KEY] <= date_end_time_unix_sec)
-    ]
+        ]
     num_warnings = len(warning_table.index)
 
     print('Number of warnings beginning on SPC date "{0:s}" = {1:d}'.format(
@@ -404,8 +404,13 @@ def _run(warning_file_name, top_tracking_dir_name, spc_date_string,
             projection_object=projection_object
         )[0]
 
+    nested_array = warning_table[[
+        WARNING_START_TIME_KEY, WARNING_START_TIME_KEY
+    ]].values.tolist()
+
     warning_table = warning_table.assign(**{
-        WARNING_XY_POLYGON_KEY: warning_polygon_objects_xy
+        WARNING_XY_POLYGON_KEY: warning_polygon_objects_xy,
+        LINKED_SECONDARY_IDS_KEY: nested_array
     })
 
     for k in range(num_warnings):
@@ -423,7 +428,7 @@ def _run(warning_file_name, top_tracking_dir_name, spc_date_string,
             this_start_time_string, this_end_time_string
         ))
 
-        these_sec_id_strings = _link_one_warning(
+        warning_table[LINKED_SECONDARY_IDS_KEY].values[k] = _link_one_warning(
             warning_table=warning_table.iloc[[k]],
             storm_object_table=copy.deepcopy(storm_object_table),
             max_distance_metres=max_distance_metres,
@@ -432,16 +437,28 @@ def _run(warning_file_name, top_tracking_dir_name, spc_date_string,
 
         print('\n')
 
+    warning_table.drop(WARNING_XY_POLYGON_KEY, axis=1, inplace=True)
+
+    print('Writing results to: "{0:s}"...'.format(output_warning_file_name))
+    pickle_file_handle = open(output_warning_file_name, 'wb')
+    pickle.dump(warning_table, pickle_file_handle)
+    pickle_file_handle.close()
+
 
 if __name__ == '__main__':
     INPUT_ARG_OBJECT = INPUT_ARG_PARSER.parse_args()
 
     _run(
-        warning_file_name=getattr(INPUT_ARG_OBJECT, WARNING_FILE_ARG_NAME),
+        input_warning_file_name=getattr(
+            INPUT_ARG_OBJECT, INPUT_WARNING_FILE_ARG_NAME
+        ),
         top_tracking_dir_name=getattr(INPUT_ARG_OBJECT, TRACKING_DIR_ARG_NAME),
         spc_date_string=getattr(INPUT_ARG_OBJECT, SPC_DATE_ARG_NAME),
         max_distance_metres=getattr(INPUT_ARG_OBJECT, MAX_DISTANCE_ARG_NAME),
         min_lifetime_fraction=getattr(
             INPUT_ARG_OBJECT, MIN_LIFETIME_FRACTION_ARG_NAME
+        ),
+        output_warning_file_name=getattr(
+            INPUT_ARG_OBJECT, OUTPUT_WARNING_FILE_ARG_NAME
         )
     )
---
+++ gewittergefahr/scripts/link_warnings_to_storms_test.py
@@ -0,0 +1,152 @@
+"""Unit tests for link_warnings_to_storms.py."""
+
+import copy
+import unittest
+import numpy
+import pandas
+from gewittergefahr.gg_utils import polygons
+from gewittergefahr.gg_utils import linkage_test
+from gewittergefahr.scripts import link_warnings_to_storms as link_warnings
+
+TOLERANCE = 1e-6
+STORM_OBJECT_TABLE = linkage_test.create_storm_objects()
+
+# The following constants are used to test _find_one_distance.
+RELATIVE_X_VERTICES_METRES = numpy.array([-0.5, 0, 0.5, 0, -0.5])
+RELATIVE_Y_VERTICES_METRES = numpy.array([0, 0.5, 0, -0.5, 0])
+
+STORM_TO_X_VERTICES_METRES = {
+    0: RELATIVE_X_VERTICES_METRES - 10,
+    1: RELATIVE_X_VERTICES_METRES - 15,
+    2: RELATIVE_X_VERTICES_METRES,
+    3: RELATIVE_X_VERTICES_METRES,
+    4: RELATIVE_X_VERTICES_METRES + 10,
+    5: RELATIVE_X_VERTICES_METRES + 15,
+    6: RELATIVE_X_VERTICES_METRES,
+    7: RELATIVE_X_VERTICES_METRES,
+    8: RELATIVE_X_VERTICES_METRES - 15,
+}
+
+STORM_TO_Y_VERTICES_METRES = {
+    0: RELATIVE_Y_VERTICES_METRES,
+    1: RELATIVE_Y_VERTICES_METRES,
+    2: RELATIVE_Y_VERTICES_METRES + 3,
+    3: RELATIVE_Y_VERTICES_METRES + 5,
+    4: RELATIVE_Y_VERTICES_METRES,
+    5: RELATIVE_Y_VERTICES_METRES,
+    6: RELATIVE_Y_VERTICES_METRES - 3,
+    7: RELATIVE_Y_VERTICES_METRES - 5,
+    8: RELATIVE_Y_VERTICES_METRES - 5,
+}
+
+STORM_TO_WARNING_DIST_METRES = {
+    0: 0.,
+    1: 4.5,
+    2: 0.,
+    3: 1.5,
+    4: 0.,
+    5: 4.5,
+    6: 0.,
+    7: 1.5,
+    8: numpy.sqrt(24.25),
+}
+
+THESE_X_METRES = numpy.array([-10, -10, 10, 10, -10], dtype=float)
+THESE_Y_METRES = numpy.array([-3, 3, 3, -3, -3], dtype=float)
+WARNING_POLYGON_OBJECT_XY = polygons.vertex_arrays_to_polygon_object(
+    exterior_x_coords=THESE_X_METRES, exterior_y_coords=THESE_Y_METRES
+)
+
+# The following constants are used to test _link_one_warning.
+THESE_START_TIMES_UNIX_SEC = numpy.array([4, 3], dtype=int)
+THESE_END_TIMES_UNIX_SEC = numpy.array([6, 5], dtype=int)
+
+THESE_X_METRES = numpy.array([274, 274, 276, 276, 274], dtype=float)
+THESE_Y_METRES = numpy.array([59.5, 60.5, 60.5, 59.5, 59.5])
+FIRST_POLYGON_OBJECT = polygons.vertex_arrays_to_polygon_object(
+    exterior_x_coords=THESE_X_METRES, exterior_y_coords=THESE_Y_METRES
+)
+
+THESE_X_METRES = numpy.array([273, 273, 275, 275, 273], dtype=float)
+SECOND_POLYGON_OBJECT = polygons.vertex_arrays_to_polygon_object(
+    exterior_x_coords=THESE_X_METRES, exterior_y_coords=THESE_Y_METRES
+)
+
+THIS_DICT = {
+    link_warnings.WARNING_START_TIME_KEY: THESE_START_TIMES_UNIX_SEC,
+    link_warnings.WARNING_END_TIME_KEY: THESE_END_TIMES_UNIX_SEC,
+    link_warnings.WARNING_LATLNG_POLYGON_KEY:
+        [FIRST_POLYGON_OBJECT, SECOND_POLYGON_OBJECT],
+    link_warnings.WARNING_XY_POLYGON_KEY:
+        [FIRST_POLYGON_OBJECT, SECOND_POLYGON_OBJECT]
+}
+
+WARNING_TABLE = pandas.DataFrame.from_dict(THIS_DICT)
+
+MAX_DISTANCE_METRES = 1.
+MIN_LIFETIME_FRACTION = 0.5
+WARNING_TO_SEC_ID_STRINGS = {
+    0: ['B4', 'B5'],
+    1: ['B4']
+}
+
+
+class LinkWarningsToStormsTests(unittest.TestCase):
+    """Each method is a unit test for link_warnings_to_storms.py."""
+
+    def test_find_one_distance(self):
+        """Ensures correct output from _find_one_distance."""
+
+        num_storms = len(STORM_TO_X_VERTICES_METRES)
+
+        for i in range(num_storms):
+            this_distance_metres = link_warnings._find_one_distance(
+                storm_x_vertices_metres=STORM_TO_X_VERTICES_METRES[i],
+                storm_y_vertices_metres=STORM_TO_Y_VERTICES_METRES[i],
+                warning_polygon_object_xy=WARNING_POLYGON_OBJECT_XY
+            )
+
+            self.assertTrue(numpy.isclose(
+                this_distance_metres, STORM_TO_WARNING_DIST_METRES[i],
+                atol=TOLERANCE
+            ))
+
+    def test_link_one_warning_first(self):
+        """Ensures correct output from _link_one_warning.
+
+        In this case, trying to link first warning.
+        """
+
+        actual_sec_id_strings = link_warnings._link_one_warning(
+            warning_table=WARNING_TABLE.iloc[[0]],
+            storm_object_table=copy.deepcopy(STORM_OBJECT_TABLE),
+            max_distance_metres=MAX_DISTANCE_METRES,
+            min_lifetime_fraction=MIN_LIFETIME_FRACTION, test_mode=True
+        )
+
+        expected_sec_id_strings = WARNING_TO_SEC_ID_STRINGS[0]
+        self.assertTrue(
+            set(expected_sec_id_strings) == set(actual_sec_id_strings)
+        )
+
+    def test_link_one_warning_second(self):
+        """Ensures correct output from _link_one_warning.
+
+        In this case, trying to link second warning.
+        """
+
+        actual_sec_id_strings = link_warnings._link_one_warning(
+            warning_table=WARNING_TABLE.iloc[[1]],
+            storm_object_table=copy.deepcopy(STORM_OBJECT_TABLE),
+            max_distance_metres=MAX_DISTANCE_METRES,
+            min_lifetime_fraction=MIN_LIFETIME_FRACTION, test_mode=True
+        )
+
+        expected_sec_id_strings = WARNING_TO_SEC_ID_STRINGS[1]
+        self.assertTrue(
+            set(expected_sec_id_strings) == set(actual_sec_id_strings)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
