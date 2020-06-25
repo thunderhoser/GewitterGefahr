07f486fac4df288803f425fcfd7f008eb751ebe1
thunderhoser <lagerqui@ualberta.ca>
2020-06-24 19:38:53 -0600

    Tweaked input-checking.

--- gewittergefahr/deep_learning/architecture_utils.py
+++ gewittergefahr/deep_learning/architecture_utils.py
@@ -62,7 +62,8 @@ def _check_convolution_options(
     """
 
     error_checking.assert_is_integer(num_kernel_rows)
-    error_checking.assert_is_geq(num_kernel_rows, 3)
+    # error_checking.assert_is_geq(num_kernel_rows, 3)
+    error_checking.assert_is_geq(num_kernel_rows, 1)
     error_checking.assert_is_integer(num_rows_per_stride)
     error_checking.assert_is_geq(num_rows_per_stride, 1)
     error_checking.assert_is_leq(num_rows_per_stride, num_kernel_rows)
@@ -84,14 +85,16 @@ def _check_convolution_options(
 
     if num_kernel_dimensions >= 2:
         error_checking.assert_is_integer(num_kernel_columns)
-        error_checking.assert_is_geq(num_kernel_columns, 3)
+        # error_checking.assert_is_geq(num_kernel_columns, 3)
+        error_checking.assert_is_geq(num_kernel_columns, 1)
         error_checking.assert_is_integer(num_columns_per_stride)
         error_checking.assert_is_geq(num_columns_per_stride, 1)
         error_checking.assert_is_leq(num_columns_per_stride, num_kernel_columns)
 
     if num_kernel_dimensions == 3:
         error_checking.assert_is_integer(num_kernel_heights)
-        error_checking.assert_is_geq(num_kernel_heights, 3)
+        # error_checking.assert_is_geq(num_kernel_heights, 3)
+        error_checking.assert_is_geq(num_kernel_heights, 1)
         error_checking.assert_is_integer(num_heights_per_stride)
         error_checking.assert_is_geq(num_heights_per_stride, 1)
         error_checking.assert_is_leq(num_heights_per_stride, num_kernel_heights)
