b9c7ee9decf2801e44bb85fede9e04cfcf2bd0a6
thunderhoser <lagerqui@ualberta.ca>
2020-12-24 14:41:36 -0700

    Added print statement.

--- gewittergefahr/deep_learning/testing_io.py
+++ gewittergefahr/deep_learning/testing_io.py
@@ -307,6 +307,9 @@ def generator_2d_or_3d(
             desired_full_id_strings=desired_full_id_strings,
             desired_times_unix_sec=desired_times_unix_sec)
     )
+    print('\n\n\n\n\n')
+    print(full_storm_id_strings)
+    print('\n\n\n\n\n')
     print('\n')
 
     example_file_names = option_dict[trainval_io.EXAMPLE_FILES_KEY]
